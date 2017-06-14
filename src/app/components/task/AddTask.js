832c7848d21dd53a6cc61b48656b98b0c216186d
Lisa Jeon <jeon.lisa@gmail.com>
2017-06-14 08:44:17 -0400

    allows user to create task with description and taskList

--- src/app/components/task/AddTask.js
+++ src/app/components/task/AddTask.js
@@ -7,15 +7,17 @@ import TaskListMembersDropdownListContainer from './TaskListMembersDropdownListC
 import * as TaskApi from '../../api/task-api'
 
 class AddTask extends React.Component {
-	constructor(props) {
+	constructor(props, container) {
 		super(props)
+		this.container = container
 		this.state = {
 				value: '',
 				assignedToId: ''
-		};
+		}
     this.handleSubmit = this.handleSubmit.bind(this)
 		this.handleTaskDetailsChange = this.handleTaskDetailsChange.bind(this)
 		this.handleAddMemberToTask = this.handleAddMemberToTask.bind(this)
+		this.unmount = this.unmount.bind(this)
 	}
 
   	componentDidMount () {
@@ -23,9 +25,17 @@ class AddTask extends React.Component {
 		//this.state.text = ""
   	}
 
+		unmount() {
+			ReactDOM.unmountComponentAtNode(this.container);
+		}
+
   	handleTaskDetailsChange(event) {
     	this.setState({value: event.target.value});
   	}
+
+		componentWillUnmount(){
+			alert("unmounting")
+		}
 
   	handleSubmit () {
 		if(this.props.taskListId != "inbox"){
@@ -43,14 +53,16 @@ class AddTask extends React.Component {
   	}
 
 		submit = (values) => {
+			this.props.addTask(values)
 			alert("working")
 			// print the form values to the console
 			console.log(values)
+			this.unmount()
 		}
 
     render() {
     return (
-			<div className="add-form-wrapper">
+			<div id="addTaskFormWrapper" className="add-form-wrapper">
 				<div className="task-item add-form row expanded">
 					<AddTaskForm onSubmit={this.submit} taskLists={this.props.taskLists} patients={this.props.patients}/>
 				</div>
