82fc959468f5789f3f9cf0e96d0f4400d79057bd
Nitin Gujral <nitin.gujral@childrens.harvard.edu>
2017-08-10 11:47:22 -0400

    added null check

--- src/app/components/common/NavBar.js
+++ src/app/components/common/NavBar.js
@@ -102,7 +102,7 @@ class NavBar extends React.Component {
               <NavLink activeClassName="active" to={"/tasks/Assigned to me"}>Assigned to me</NavLink>
               <NavLink activeClassName="active" to={"/tasks/Assigned by me"}>Assigned by me</NavLink>
 
-              {this.props.taskLists.map(taskList => {
+              {this.props.taskLists && this.props.taskLists.map(taskList => {
                 return(
                   <NavLink  to={"/tasks/" + taskList.listName + "/" + taskList.taskListId} activeClassName="active" title={taskList.listName} key={taskList.taskListId}><span className="list-logo small"></span>{taskList.listName}</NavLink>
                 )
