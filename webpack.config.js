4c052881f8c44736c18aa9a26eb4b95b2d4b4ee5
Mateusz Pokora <vrael560@gmail.com>
2018-11-27 13:37:48 +0100

    update packages to newer versions

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,7 @@
 var webpack = require('webpack');
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin')
-//var BowerWebpackPlugin = require("bower-webpack-plugin");
-var dotenv = require('dotenv'); //pm2 environment files
-// var Promise = require('es6-promise').Promise;
+var dotenv = require('dotenv');
 
 var BUILD_DIR = path.resolve(__dirname, 'dist');
 var APP_DIR = path.resolve(__dirname, 'src/app');
@@ -26,54 +24,36 @@ const exposed = [
 const exposedEnvironment = {}
 exposed.forEach(i => { exposedEnvironment[i] = JSON.stringify(process.env[i]) })
 
-var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: __dirname + '/src/index.html',
-  filename: 'index.html',
-  inject: 'body',
-  hash: true
-});
-
 var config = {
   entry: ['babel-polyfill', APP_DIR + '/index.js'],
   devtool: "source-map",
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
-    // filename: '[name].[chunkhash].js'
   },
   module : {
-    loaders : [
+    rules: [
       {
         test : /\.jsx?/,
         include : APP_DIR,
         exclude: /node_modules/,
         loader : ["babel-loader"]
       }
-/*    ,
-      {
-        test:   /\.css$/,
-        loader: "style!css"
-      }
-*/
     ]
   },
-  //plugins: [HTMLWebpackPluginConfig, new BowerWebpackPlugin()]
-  plugins: [HTMLWebpackPluginConfig,
+  plugins: [
     new webpack.LoaderOptionsPlugin({
       debug: true
     }),
     new webpack.DefinePlugin({
       'process.env': exposedEnvironment
     }),
-    // new webpack.optimize.CommonsChunkPlugin({
-    //   name: 'vendor'
-    // }),
-    // new webpack.optimize.CommonsChunkPlugin({
-    //   name: 'manifest'
-    // }),
-    // new webpack.ProvidePlugin({
-    //   Promise: 'es6-promise-promise',
-    // })
+    new HtmlWebpackPlugin({
+      template: __dirname + '/src/index.html',
+      filename: 'index.html',
+      inject: 'body',
+      hash: true
+    })
   ],
   devServer: {
     compress: false,
