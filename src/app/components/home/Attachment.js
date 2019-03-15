24ac35651374bc702adff96c295c235c48eb0173
Adam Dąbrowski <adam.dabrowski@htdevelopers.com>
2019-03-15 18:37:27 +0100

    Make attachments clickable

--- src/app/components/home/Attachment.js
+++ src/app/components/home/Attachment.js
@@ -1,15 +1,18 @@
 import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import ButtonBase from '@material-ui/core/ButtonBase';
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  padding: 0 10px;
-  background: #ededf0;
-  height: 24px;
-  margin-top: 2px;
+  && {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    background: #ededf0;
+    height: 24px;
+    margin-top: 2px;
+  }
 `;
 
 const Name = styled.div`
@@ -24,24 +27,46 @@ const Size = styled.div`
   margin-left: 20px;
 `;
 
-const Remove = styled.div`
+const Open = styled(ButtonBase)`
+  && {
+    height: 100%;
+    flex: 1;
+    padding: 0 10px;
+    justify-content: flex-start;
+  }
+`;
+
+const Remove = styled(ButtonBase)`
+&& {
+  height: 100%;
+  width: 26px;
   color: #aab8c4;
   font-size: 12px;
   font-weight: 500;
   margin-left: auto;
+}
 `;
 
 const Attachment = ({
   id, name, size, remove,
 }) => {
-  const handleRemove = useCallback(() => {
-    remove(id);
+  const handleOpen = useCallback((e) => {
+    e.stopPropagation();
+    console.log('DOWNLOAD');
+  });
+
+  const handleRemove = useCallback((e) => {
+    e.stopPropagation();
+    console.log('REMOVE');
+    // remove(id);
   }, [id]);
 
   return (
     <Container>
-      <Name>{name}</Name>
-      <Size>{`(${size}K)`}</Size>
+      <Open onClick={handleOpen}>
+        <Name>{name}</Name>
+        <Size>{`(${size}K)`}</Size>
+      </Open>
       <Remove onClick={handleRemove}>✕</Remove>
     </Container>);
 };
