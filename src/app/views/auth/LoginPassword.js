f91a700eb4bd77d16a35fb272d772cac4d8c35aa
Aleksander Krawiel <43279345+akrawiel@users.noreply.github.com>
2019-10-16 14:00:55 +0200

    Fix loading of login password initial values (#67)
    * Refactor of login templates
    
    * Change login screen container
    
    * Initial work on email screen
    
    * Change username login screen
    
    * Change password login view
    
    * Minor typography break fix
    
    * Add responsive login container
    
    * Button pointer fix
    
    * Fix loading of login password initial values

--- src/app/views/auth/LoginPassword.js
+++ src/app/views/auth/LoginPassword.js
@@ -6,31 +6,35 @@ import { mobileAnalyticsClient } from '../../api/analytics-api';
 import * as userApi from '../../api/user-api';
 import LoginFormPassword from '../../components/auth/LoginFormPassword';
 
-const onSubmit = (form) => {
-  const username = window.sessionStorage.getItem('username');
-  return userApi
-    .login(username, form.password)
-    .then((data) => {
-      mobileAnalyticsClient.recordEvent('AUTH_EVENTS', {
-        LOGIN_SUCCESS: 'YES',
-      });
-      if (data === 'SMS_MFA') {
-        hashHistory.push(`confirmMFACode?uname=${form.username}`);
-      } else {
-        sessionStorage.setItem('sessionStartTime', new Date().getTime());
-
-        hashHistory.push('/');
-        success('Logged in.');
-      }
-    })
-    .catch((e) => {
-      error(e && e.message ? e.message : 'Could not login.');
-      mobileAnalyticsClient.recordEvent('AUTH_EVENTS', {
-        LOGIN_SUCCESS: 'NO',
-      });
+const onSubmit = form => userApi
+  .login(form.username, form.password)
+  .then((data) => {
+    mobileAnalyticsClient.recordEvent('AUTH_EVENTS', {
+      LOGIN_SUCCESS: 'YES',
     });
-};
+    if (data === 'SMS_MFA') {
+      hashHistory.push(`confirmMFACode?uname=${form.username}`);
+    } else {
+      sessionStorage.setItem('sessionStartTime', new Date().getTime());
 
-const LoginPassword = () => <LoginFormPassword onSubmit={onSubmit} />;
+      hashHistory.push('/');
+      success('Logged in.');
+    }
+  })
+  .catch((e) => {
+    error(e && e.message ? e.message : 'Could not login.');
+    mobileAnalyticsClient.recordEvent('AUTH_EVENTS', {
+      LOGIN_SUCCESS: 'NO',
+    });
+  });
+
+const LoginPassword = () => (
+  <LoginFormPassword
+    initialValues={{
+      username: window.sessionStorage.getItem('username'),
+    }}
+    onSubmit={onSubmit}
+  />
+);
 
 export default LoginPassword;
