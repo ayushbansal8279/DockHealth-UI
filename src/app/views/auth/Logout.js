2c618068cf601e730cd3ed37b72b36e164a24556
Aleksander Krawiel <43279345+akrawiel@users.noreply.github.com>
2019-10-17 10:56:50 +0200

    New login scren - logout page (#75)
    * Change log out page
    
    * Adjust logout margin

--- src/app/views/auth/Logout.js
+++ src/app/views/auth/Logout.js
@@ -1,77 +1,74 @@
-import React from 'react'
-import { Link, browserHistory, hashHistory } from 'react-router'
-import * as userApi from '../../api/user-api'
-import { error, success } from '../../actions/notification-actions'
-import {mobileAnalyticsClient} from '../../api/analytics-api'
+import Grid from '@material-ui/core/Grid';
+import isNil from 'ramda/es/isNil';
+import React, { PureComponent } from 'react';
+import { hashHistory } from 'react-router';
 
-export default class Logout extends React.Component {
+import { error } from '../../actions/notification-actions';
+import { mobileAnalyticsClient } from '../../api/analytics-api';
+import * as userApi from '../../api/user-api';
+import { NextButton, TitleTypography } from '../../components/auth/AuthComponents.styled';
 
-   componentDidMount(){
-    var durationOfTimeSpentOnApp = this.getDurationOfTimeSpentOnApp();
-    return userApi.logout()
-      .then(data => {
+export default class Logout extends PureComponent {
+  componentDidMount = () => {
+    const durationOfTimeSpentOnApp = this.getDurationOfTimeSpentOnApp();
+
+    return userApi
+      .logout()
+      .then(() => {
         mobileAnalyticsClient.recordEvent('AUTH_EVENTS', {
-            'LOGOUT_SUCCESS': 'YES'
+          LOGOUT_SUCCESS: 'YES',
         });
-        //console.log(data);
+        // console.log(data);
         mobileAnalyticsClient.recordEvent('DURATION_INAPP', {
-            'TIME_DURATION': durationOfTimeSpentOnApp
+          TIME_DURATION: durationOfTimeSpentOnApp,
         });
       })
-      .catch(e => {
-        error(e && e.message ? e.message : 'Could not logout.')
+      .catch((e) => {
+        error(e && e.message ? e.message : 'Could not logout.');
         mobileAnalyticsClient.recordEvent('AUTH_EVENTS', {
-            'LOGOUT_SUCCESS': 'NO'
+          LOGOUT_SUCCESS: 'NO',
         });
-      })
-    }
+      });
+  };
 
-    getDurationOfTimeSpentOnApp(){
-      var readableDifference;
-      try {
-        if(sessionStorage.sessionStartTime == undefined || sessionStorage.sessionStartTime == null){
-          return null;
-        }
-
-        var sessionEndTime= new Date().getTime();
-        var timeDifference=sessionEndTime-sessionStorage.sessionStartTime;
-        var differenceDate = new Date(timeDifference);
-        readableDifference = differenceDate.getUTCHours() + ':' + differenceDate.getUTCMinutes() + ':' + differenceDate.getUTCSeconds();
-        return readableDifference;
-      }
-      catch(e){
-        console.log("Error in getDurationOfTimeSpentOnApp");
+  getDurationOfTimeSpentOnApp = () => {
+    try {
+      if (isNil(sessionStorage.sessionStartTime)) {
         return null;
       }
+
+      const sessionEndTime = new Date().getTime();
+      const timeDifference = sessionEndTime - sessionStorage.sessionStartTime;
+      const differenceDate = new Date(timeDifference);
+      return `${differenceDate.getUTCHours()}:${differenceDate.getUTCMinutes()}:${differenceDate.getUTCSeconds()}`;
+    } catch (e) {
+      return null;
     }
+  };
 
-  // onSubmit (form) {
-  //   return userApi.login(form.username, form.password)
-  //     .then(data => {
-  //       if(data == "SMS_MFA"){
-  //         hashHistory.push('confirmMFACode?uname='+form.username)
-  //       }else{
-  //         //browserHistory.push('/resetPassword')
-  //         hashHistory.push('/')
-  //         success('Logged in.')
-  //       }
-  //     })
-  //     .catch(e => {
-  //       error(e && e.message ? e.message : 'Could not login.')
-  //     })
-  // }
+  redirectToLogin = () => {
+    hashHistory.push('login');
+  };
 
-  render () {
-    return (
-			<div className="row expanded">
-				<div className="columns large-12 text-center top-buffer">
-					<h3>You have been logged out</h3>
-				</div>
-				<div className="columns large-12 top-buffer text-right details">
-          <h4 className='subtitle'><Link to="/login">Login here</Link></h4>
-				</div>
-
-			</div>
-    )
-  }
+  render = () => (
+    <Grid container>
+      <TitleTypography variant="h2" style={{ marginTop: '6em' }}>
+        You have been logged out
+      </TitleTypography>
+      <Grid item sm={12} md={6}>
+        <NextButton
+          active
+          id="loginButton"
+          variant="contained"
+          color="primary"
+          style={{
+            marginTop: '5rem',
+          }}
+          onClick={this.redirectToLogin}
+        >
+          Sign In
+        </NextButton>
+      </Grid>
+    </Grid>
+  );
 }
