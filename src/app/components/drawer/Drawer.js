b95068bc26bbbf074bbac9b518f51aa7ae1e961b
Nitin Gujral <nitin.gujral@childrens.harvard.edu>
2019-11-05 16:49:11 -0500

    enabled intercom integration

--- src/app/components/drawer/Drawer.js
+++ src/app/components/drawer/Drawer.js
@@ -6,6 +6,7 @@ import MaterialDrawer from '@material-ui/core/Drawer';
 import useBoolean from '../../hooks/useBoolean';
 import DrawerList from './DrawerList';
 import DrawerTitle from './DrawerTitle';
+import Intercom from 'react-intercom';
 
 const StyledDrawer = styled(MaterialDrawer).attrs({
   variant: 'permanent',
@@ -37,6 +38,12 @@ const ContentContainer = styled.div`
 const Drawer = ({ header, user, lists, children }) => {
   const [isOpen, open, close] = useBoolean(false);
 
+  const intercomUser = {
+    email: user.email,
+    name: `${user.firstName} ${user.lastName}`
+  };
+  console.log(intercomUser)
+
   return (
     <div style={{ display: 'flex', height: '100%' }}>
       <StyledDrawer open={isOpen}>
@@ -48,6 +55,7 @@ const Drawer = ({ header, user, lists, children }) => {
           lists={lists}
         />
         <DrawerTitle header={header} />
+        <Intercom appID="q7dotpic" { ...intercomUser } />
       </StyledDrawer>
       <ContentContainer topPadded={header.show} open={isOpen}>
         {children}
