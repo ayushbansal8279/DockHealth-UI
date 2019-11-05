7b7f631dc5dcf84e92a277ccbc060f814ebf0199
Aleksander Krawiel <43279345+akrawiel@users.noreply.github.com>
2019-11-05 19:54:19 +0100

    Add styled input custom sizing and textarea prop (#115)

--- src/app/components/userProfileView/StyledInput.js
+++ src/app/components/userProfileView/StyledInput.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useFormContext } from 'react-hook-form';
 import MaskedInput from 'react-text-mask';
 import styled from 'styled-components';
+import mergeDeepRight from 'ramda/es/mergeDeepRight';
 
 import { mergeRefs } from '../../helpers/utilityFunctions';
 import {
@@ -11,6 +12,7 @@ import {
 
 const EMPTY_CLASS_NAME = 'empty';
 const ERROR_CLASS_NAME = 'error';
+const FOCUS_CLASS_NAME = 'focus';
 
 const StyledLabel = styled.div`
   && {
@@ -20,7 +22,7 @@ const StyledLabel = styled.div`
     font-weight: normal;
     pointer-events: none;
     position: absolute;
-    top: 50%;
+    top: 2.34375rem;
     transform: translateY(-50%);
     transition: font-size 0.2s ease-out, top 0.2s ease-out;
     z-index: 1;
@@ -43,6 +45,8 @@ const StyledInputContainer = styled.div`
     background-color: ${props => props.backgroundColor ?? '#f3f5f6'};
     border-radius: 0;
     box-sizing: border-box;
+    height: ${props => (props.isTextarea ? 9.375 : 4.6875)}rem;
+    margin-top: 0.5rem;
     position: relative;
 
     ${props => {
@@ -69,54 +73,86 @@ const StyledErrorLabel = styled.div`
   }
 `;
 
-const StyledInput = styled.input`
-  && {
-    background-color: transparent;
+const inputStyle = styleExtension => props =>
+  mergeDeepRight(
+    {
+      '&&': {
+        backgroundColor: 'transparent',
+        border: '1.5px solid transparent',
+        boxShadow: 'none',
+        color: '#000',
+        fontSize: `${props.fontSize ?? 14}px`,
+        fontWeight: 'normal',
+        height: '100%',
+        outline: 'none',
+        padding: '1rem 2.5rem 0 1rem',
+
+        cursor: props.controlled && 'pointer',
+        pointerEvents: props.controlled && 'none',
+
+        width: props.fullWidth && '100%',
+
+        [`&:focus ~ ${StyledLabel}, &:not(.${EMPTY_CLASS_NAME}) ~ ${StyledLabel}`]: {
+          fontSize: `${props.labelFontSize ?? 12}px`,
+          top: `${props.isTextarea ? 1.25 : 1}rem`,
+
+          '& > .input-label': {
+            color: '#ababb2',
+          },
+        },
+
+        '&:focus': {
+          border: '1.5px solid #dedee2',
+        },
+
+        [`&:${ERROR_CLASS_NAME}`]: {
+          border: '1.5px solid #e40909',
+        },
+
+        [`&:-webkit-autofill, &:-webkit-autofill:active, &:-webkit-autofill:hover, &:-webkit-autofill:focus`]: {
+          transition: 'all 0.25s ease-out, -webkit-box-shadow 0s',
+          '-webkit-box-shadow':
+            '0 0 0 40px rgba(243, 245, 246) inset !important',
+        },
+      },
+    },
+    styleExtension,
+  );
+
+const StyledInput = styled.input(inputStyle({}));
+const StyledTextarea = styled.textarea(
+  inputStyle({
+    '&&': {
+      resize: 'none',
+      padding: 0,
+    },
+    '&&&': {
+      border: 'none',
+    },
+  }),
+);
+
+const TextareaWrapper = styled.div`
+  height: 100%;
+  transition: all 0.25s ease-out;
+
+  ${props =>
+    props.isTextarea &&
+    `
     border: 1.5px solid transparent;
-    box-shadow: none;
-    color: #000;
-    font-size: ${props => props.fontSize ?? 14}px;
-    font-family: 'Open Sans', sans-serif;
-    font-weight: normal;
-    height: 75px;
-    margin-top: 8px;
-    outline: none;
-    padding: 1rem 2.5rem 0 1rem;
-    ${props =>
-      props.controlled &&
-      `
-      cursor: pointer;
-      pointer-events: none;
-    `}
-    ${props => props.fullWidth && 'width: 100%;'}
-    ${props => props.gutterBottom && 'margin-bottom: 1rem;'}
+    box-sizing: border-box;
+    cursor: text;
+    overflow: hidden;
+    padding: 2rem 1rem 1rem 1rem;
 
-    &:focus ~ ${StyledLabel}, &:not(.${EMPTY_CLASS_NAME}) ~ ${StyledLabel} {
-      font-size: 12px;
-      top: 25%;
-
-      & > .input-label {
-        color: #ababb2;
-      }
-    }
-
-    &:focus {
+    &.${FOCUS_CLASS_NAME} {
       border: 1.5px solid #dedee2;
     }
 
     &.${ERROR_CLASS_NAME} {
       border: 1.5px solid #e40909;
     }
-
-    &:-webkit-autofill,
-    &:-webkit-autofill:active,
-    &:-webkit-autofill:hover,
-    &:-webkit-autofill:focus {
-      transition: all 0.25s ease-out, -webkit-box-shadow 0s;
-      -webkit-box-shadow: 0 0 0 40px rgba(243, 245, 246) inset !important;
-    }
-
-  }
+  `}
 `;
 
 const renderPhoneNumberField = ({ inputProps, props, register }) => (
@@ -144,6 +180,8 @@ export default React.forwardRef(
       controlled = false,
       onContainerClick = () => {},
       fontSize,
+      labelFontSize,
+      isTextarea = false,
       backgroundColor,
       ...props
     },
@@ -154,6 +192,8 @@ export default React.forwardRef(
     const error = (errors[name] || {}).message;
     const currentValue = watch(name);
     const hasError = Boolean(error);
+
+    const [inputState, setInputState] = useState('');
 
     let inputClassName = '';
 
@@ -167,19 +207,35 @@ export default React.forwardRef(
 
     inputClassName = inputClassName.trim();
 
+    const wrapperClassName = `${inputClassName} ${inputState}`.trim();
+
+    const onTextareaWrapperClicked = event => {
+      if (isTextarea) {
+        const [textarea] = event.target.querySelectorAll('textarea');
+
+        return textarea?.focus();
+      }
+
+      return null;
+    };
+
     const inputProps = {
       className: inputClassName,
       name,
       controlled,
       fontSize,
+      labelFontSize,
+      isTextarea,
       fullWidth: true,
-      gutterBottom: true,
     };
+
+    const InputComponent = isTextarea ? StyledTextarea : StyledInput;
 
     return (
       <StyledInputContainer
         controlled={controlled}
         containerDisabled={containerDisabled}
+        isTextarea={isTextarea}
         onClick={e => {
           if (!containerDisabled) {
             onContainerClick(e);
@@ -195,12 +251,24 @@ export default React.forwardRef(
             render={renderPhoneNumberField({ inputProps, props, register })}
           />
         ) : (
-          <StyledInput ref={register} {...inputProps} {...props} />
+          <TextareaWrapper
+            className={isTextarea ? wrapperClassName : ''}
+            isTextarea={isTextarea}
+            onClick={onTextareaWrapperClicked}
+          >
+            <InputComponent
+              ref={register}
+              {...inputProps}
+              {...props}
+              onFocus={() => setInputState(FOCUS_CLASS_NAME)}
+              onBlur={() => setInputState('')}
+            />
+            <StyledLabel fontSize={fontSize}>
+              <span className="input-label">{label}</span>
+              {required && <span className="required">*</span>}
+            </StyledLabel>
+          </TextareaWrapper>
         )}
-        <StyledLabel fontSize={fontSize}>
-          <span className="input-label">{label}</span>
-          {required && <span className="required">*</span>}
-        </StyledLabel>
       </StyledInputContainer>
     );
   },
