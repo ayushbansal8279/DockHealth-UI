945aa5dae406ed18a0ecadd66e2deff90aa8e08e
Nitin Gujral <nitin.gujral@childrens.harvard.edu>
2019-11-15 10:29:41 -0500

    enabled pusher for realtime updates

--- src/app/views/TaskView.js
+++ src/app/views/TaskView.js
@@ -28,6 +28,11 @@ import {
   ToolbarContainer,
 } from './TaskView.styled';
 
+import Pusher from 'pusher-js';
+
+const APP_KEY = process.env.PUSHER_APP_KEY;
+const APP_CLUSTER = process.env.PUSHER_CLUSTER_NAME;
+
 const groupBy = (list, keyGetter) => {
   const map = new Map();
   list.forEach(item => {
@@ -117,6 +122,20 @@ class TaskView extends Component {
 
   componentDidMount = () => {
     this.resetHeader();
+    const socket = new Pusher(APP_KEY, {
+      cluster: APP_CLUSTER,
+    });
+    const channel = socket.subscribe('dock-task-channel');
+    // Listen to the channel for new entries.
+    // The server publishes to this channel whenever a entry is updated
+    channel.bind('task-update', data => {
+      // Since the app is going to be realtime, we don't want the same item to
+      // be shown twice. Device A publishes an entry, all other devices including itself
+      // receives the entry, so act like a basic filter
+      console.log("received data")
+      console.log(data)
+      this.props.refreshTask(data.task)
+    });
   };
 
   componentDidUpdate = ({
