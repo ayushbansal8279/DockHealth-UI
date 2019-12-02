575be9c6161f11900e0b279ff6c946a2c65d7dc9
Aleksander Krawiel <43279345+akrawiel@users.noreply.github.com>
2019-12-02 14:53:25 +0100

    Filters not working on inbox (#191)
    * TaskView refactor
    
    * Add filters for inbox
    
    * Fix toolbar container inbox display

--- src/app/views/TaskView.js
+++ src/app/views/TaskView.js
@@ -4,6 +4,7 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import Popover from '@material-ui/core/Popover';
 import { AnimatePresence } from 'framer-motion';
+import Pusher from 'pusher-js';
 import equals from 'ramda/es/equals';
 import filter from 'ramda/es/filter';
 import map from 'ramda/es/map';
@@ -46,8 +47,6 @@ import {
   TaskViewGrid,
   ToolbarContainer,
 } from './TaskView.styled';
-
-import Pusher from 'pusher-js';
 
 const APP_KEY = process.env.PUSHER_APP_KEY;
 const APP_CLUSTER = process.env.PUSHER_CLUSTER_NAME;
@@ -117,33 +116,37 @@ class TaskView extends Component {
 
   filterButton = React.createRef();
 
-  refresh = () => {
-    const { actions, patientActions } = this.props;
-
-    actions.loading();
-    this.closeAuditHistory();
-
-    actions.getInboxTasks('INCOMPLETE');
-
-    patientActions.getAllPatients();
-  };
-
   componentDidMount = () => {
     this.resetHeader();
   };
 
-  componentWillMount = () => {
-  };
+  componentWillUpdate(nextProps) {
+    const { refreshTask, taskList } = this.props;
 
-  componentWillUnmount = () => {
-    const { taskTimeouts } = this.state;
-
-    Object.values(taskTimeouts)
-      .flat()
-      .forEach(({ taskTimeoutId }) => {
-        clearTimeout(taskTimeoutId);
+    if (
+      (!taskList && nextProps && nextProps.taskList) ||
+      (nextProps &&
+        nextProps.taskList &&
+        taskList.taskListId !== nextProps.taskList.taskListId)
+    ) {
+      const socket = new Pusher(APP_KEY, {
+        cluster: APP_CLUSTER,
       });
-  };
+      const currentTaskListId = nextProps.taskList.taskListId;
+      const channelName = `dock-task-channel-${currentTaskListId}`;
+      const channel = socket.subscribe(channelName);
+
+      // Listen to the channel for new entries.
+      // The server publishes to this channel whenever a entry is updated
+      channel.bind('task-update', data => {
+        // Since the app is going to be realtime, we don't want the same item to
+        // be shown twice. Device A publishes an entry, all other devices including itself
+        // receives the entry, so act like a basic filter
+        refreshTask(data.task);
+        getTaskListStats(nextProps.taskList);
+      });
+    }
+  }
 
   componentDidUpdate = ({
     isFetching: prevIsFetching,
@@ -155,31 +158,26 @@ class TaskView extends Component {
     }
   };
 
-  componentWillUpdate(nextProps) {
-    if((!this.props.taskList && nextProps && nextProps.taskList)
-      || ( nextProps && nextProps.taskList
-        && this.props.taskList.taskListId != nextProps.taskList.taskListId)){
-      const socket = new Pusher(APP_KEY, {
-        cluster: APP_CLUSTER,
+  refresh = () => {
+    const { actions, patientActions } = this.props;
+
+    actions.loading();
+    this.closeAuditHistory();
+
+    actions.getInboxTasks('INCOMPLETE');
+
+    patientActions.getAllPatients();
+  };
+
+  componentWillUnmount = () => {
+    const { taskTimeouts } = this.state;
+
+    Object.values(taskTimeouts)
+      .flat()
+      .forEach(({ taskTimeoutId }) => {
+        clearTimeout(taskTimeoutId);
       });
-      const currentTaskListId = nextProps.taskList.taskListId;
-      const channelName = 'dock-task-channel-'+currentTaskListId;
-      const channel = socket.subscribe(channelName);
-      console.log('subscribed to channel: '+channelName)
-      // Listen to the channel for new entries.
-      // The server publishes to this channel whenever a entry is updated
-      channel.bind('task-update', data => {
-        // Since the app is going to be realtime, we don't want the same item to
-        // be shown twice. Device A publishes an entry, all other devices including itself
-        // receives the entry, so act like a basic filter
-        console.log("received data")
-        console.log(data)
-        this.props.refreshTask(data.task)
-        this.props.refreshTask(data.task)
-        getTaskListStats(nextProps.taskList);
-      });
-    }
-  }
+  };
 
   clearTaskTimeouts = (taskTimeoutId, callback = () => {}) => {
     this.setState(prevState => {
@@ -509,8 +507,7 @@ class TaskView extends Component {
       return <TaskList {...tasklistProps} />;
     }
 
-    var listMap = Array.from(groupedTasks.keys())
-    return listMap.map(listName => (
+    return Array.from(groupedTasks.keys()).map(listName => (
       <React.Fragment key={listName}>
         <h5>{listName}</h5>
         <TaskList listTasks={groupedTasks.get(listName)} {...tasklistProps} />
@@ -604,7 +601,8 @@ class TaskView extends Component {
     const currentFilterDescription =
       filterOptions.find(({ value }) => value === filterBy)?.description ?? '';
 
-    const toolbarContainerVisible = (isInbox && tasks.length > 0) || !isInbox;
+    const toolbarContainerVisible =
+      (isInbox && (tasks.length > 0 || isFetching)) || !isInbox;
 
     return (
       <div
