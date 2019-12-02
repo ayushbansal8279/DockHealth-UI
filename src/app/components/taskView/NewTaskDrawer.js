b877c869faa476d5e17edd43a0fd72677cb0da4e
Aleksander Krawiel <43279345+akrawiel@users.noreply.github.com>
2019-12-02 15:33:09 +0100

    Task drawer scrollbar fix (#193)
    * Fix task flag displaying (#190)
    
    * Fix development env file base urls
    
    * Fix comment group padding
    
    * Fix priority flag displaying
    
    * Remove console.log
    
    * Fix auto save indicator displaying
    
    * Filters not working on inbox (#191)
    
    * TaskView refactor
    
    * Add filters for inbox
    
    * Fix toolbar container inbox display
    
    * Remove intersection observer from task drawer
    
    * Add top margin to empty task lists
    
    * Add ellipsis overflow to task descriptions
    
    * Fix task description overflow popover handling

--- src/app/components/taskView/NewTaskDrawer.js
+++ src/app/components/taskView/NewTaskDrawer.js
@@ -141,35 +141,6 @@ for (let threshold = 0; threshold <= 1; threshold += 0.02) {
   thresholds.push(threshold);
 }
 
-const intersectionCallback = entries => {
-  entries.forEach(entry => {
-    const {
-      intersectionRect: { height },
-      target,
-    } = entry;
-    const form = target.querySelector('form');
-
-    if (form) {
-      form.style.height = height;
-      form.style.minHeight = height;
-    }
-  });
-};
-
-let observer;
-try {
-  observer = new IntersectionObserver(intersectionCallback, {
-    root: null,
-    rootMargin: '0px',
-    threshold: thresholds,
-  });
-} catch {
-  observer = {
-    observe: () => {},
-    unobserve: () => {},
-  };
-}
-
 export default ({
   closeDrawer,
   headsUpAreaRef,
@@ -194,7 +165,6 @@ export default ({
   const userProfile = useSelector(store => store.userState.userProfile);
   const dispatch = useDispatch();
   const taskContainerRef = useRef(null);
-  const [cachedTaskContainerRef, setCachedTaskContainerRef] = useState(null);
   const [
     autoSaveVisible,
     setAutoSaveVisible,
@@ -323,22 +293,6 @@ export default ({
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [headsUpAreaRef?.scrollHeight],
   );
-
-  useEffect(() => {
-    setCachedTaskContainerRef(taskContainerRef.current);
-  }, []);
-
-  useEffect(() => {
-    if (cachedTaskContainerRef) {
-      observer.observe(taskContainerRef.current);
-    }
-
-    return () => {
-      if (cachedTaskContainerRef) {
-        observer.unobserve(cachedTaskContainerRef);
-      }
-    };
-  }, [cachedTaskContainerRef]);
 
   let defaultValues = {};
 
