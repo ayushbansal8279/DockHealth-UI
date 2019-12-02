27a9f3f49bca368e000ae56a61a3d252726ef1b8
Nitin Gujral <nitin.gujral@childrens.harvard.edu>
2019-12-02 17:02:28 -0500

    fixed link for Assigned To Me lists

--- src/app/views/ListDetailsView.js
+++ src/app/views/ListDetailsView.js
@@ -28,13 +28,27 @@ class Home extends PureComponent {
       sortBy = 'CREATED_DT';
     }
 
-    if (listName === 'assigned_by_me' || listName === 'assigned_to_me') {
+    if (listName === 'assigned_by_me') {
       actions
         .getTasksAssignedByMe(undefined, sortBy, filterBy, taskStatus)
         .then(() => {
           actions.loadingCompletedTasks();
           if (taskStatus === 'INCOMPLETE') {
             actions.getTasksAssignedByMe(
+              undefined,
+              sortBy,
+              filterBy,
+              'COMPLETE',
+            );
+          }
+        });
+    }else if (listName === 'assigned_to_me') {
+      actions
+        .getTasksAssignedToMe(undefined, sortBy, filterBy, taskStatus)
+        .then(() => {
+          actions.loadingCompletedTasks();
+          if (taskStatus === 'INCOMPLETE') {
+            actions.getTasksAssignedToMe(
               undefined,
               sortBy,
               filterBy,
