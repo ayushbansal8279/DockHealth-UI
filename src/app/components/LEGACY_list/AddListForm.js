6f151ef9cfa30051d6a809ba87d798898589a2b2
Aleksander Krawiel <43279345+akrawiel@users.noreply.github.com>
2019-12-03 15:02:37 +0100

    Fix blanking of task assignments number (#197)
    * Add list form refactor
    
    * Fix tasklist properties overwriting

--- src/app/components/LEGACY_list/AddListForm.js
+++ src/app/components/LEGACY_list/AddListForm.js
@@ -4,42 +4,31 @@ import Autosuggest from 'react-autosuggest';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { arrayPush, change, Field, reduxForm, reset } from 'redux-form';
-import Grid from '@material-ui/core/Grid';
+import prop from 'ramda/es/prop';
 
 import * as PeopleActions from '../../actions/people-actions';
 import * as TaskListActions from '../../actions/tasklist-actions';
-import BaseComponentWithAutoComplete from '../LEGACY_base/BaseComponentWithAutoComplete';
 import BasicField from '../common/BasicField';
-import MemberInitials from '../members/MemberInitials';
+import BaseComponentWithAutoComplete from '../LEGACY_base/BaseComponentWithAutoComplete';
 import Member from '../members/Member';
 
 class AddListForm extends BaseComponentWithAutoComplete {
-  constructor(props) {
-    super(props);
-    this.state = {
-      saveResultMessage: '',
-      selectedMembers: [],
-      selectedAdmins: [],
-      selectedSuggestion: '',
-      selectedSuggestionForAdmin: '',
-      selectedSuggestionForMember: '',
-      suggestions: [],
-    };
-    this.getSuggestions = this.getSuggestions.bind(this);
-    this.getSuggestionValue = this.getSuggestionValue.bind(this);
-    this.renderSuggestion = this.renderSuggestion.bind(this);
-    this.renderSuggestionsContainer = this.renderSuggestionsContainer.bind(
-      this,
-    );
-    this.renderInputComponent = this.renderInputComponent.bind(this);
-  }
+  state = {
+    saveResultMessage: '',
+    selectedMembers: [],
+    selectedAdmins: [],
+    selectedSuggestion: '',
+    selectedSuggestionForAdmin: '',
+    selectedSuggestionForMember: '',
+    suggestions: [],
+  };
 
   componentDidMount() {
     this.props.peopleActions.findAllUsersByOrganizationId();
   }
 
   componentWillReceiveProps(nextProps) {
-    if (this.props.taskListId != nextProps.taskListId) {
+    if (this.props.taskListId !== nextProps.taskListId) {
       this.setState({
         selectedAdmins: [],
         selectedMembers: [],
@@ -63,7 +52,7 @@ class AddListForm extends BaseComponentWithAutoComplete {
     enableFoundationAccordionComponent('.add-form-wrapper');
   }
 
-  //TODO Make sure not to suggest added users.
+  // TODO Make sure not to suggest added users.
   getSuggestions = value => {
     const inputValue = value.trim().toLowerCase();
     const inputLength = inputValue.length;
@@ -76,33 +65,21 @@ class AddListForm extends BaseComponentWithAutoComplete {
               inputValue ||
               person.lastName.toLowerCase().slice(0, inputLength) ===
                 inputValue) &&
-                //TODO Literally right here. Replace this check with a check against every first name or last name in the list.
-            (person.firstName != this.props.currentUserProfile.firstName ||
-              person.lastName != this.props.currentUserProfile.lastName)
-              && !this.checkPersonAlreadySelected(person),
+            // TODO Literally right here. Replace this check with a check against every first name or last name in the list.
+            (person.firstName !== this.props.currentUserProfile.firstName ||
+              person.lastName !== this.props.currentUserProfile.lastName) &&
+            !this.checkPersonAlreadySelected(person),
         );
   };
 
-  //Checks if the user has already been put into the list. 
-  checkPersonAlreadySelected = (person) => {
-    if (this.state.selectedAdmins.length > 0) {
-      var k = 0;
-      for (k in this.state.selectedAdmins) {
-        if(this.state.selectedAdmins[k].userId == person.userId){
-          return true
-        }
-      }
-    }
-    if (this.state.selectedMembers.length > 0) {
-      var k = 0;
-      for (k in this.state.selectedMembers) {
-        if(this.state.selectedMembers[k].userId == person.userId){
-          return true
-        }
-      }
-    }
-    return false;
-  }
+  checkPersonAlreadySelected = ({ userId: personUserId }) => {
+    const { selectedAdmins, selectedMembers } = this.state;
+
+    return Boolean(
+      selectedAdmins.find(({ userId }) => userId === personUserId) ||
+        selectedMembers.find(({ userId }) => userId === personUserId),
+    );
+  };
 
   getSuggestionValue = suggestion =>
     `${suggestion.firstName} ${suggestion.lastName}`;
@@ -111,14 +88,13 @@ class AddListForm extends BaseComponentWithAutoComplete {
   renderSuggestion = suggestion => (
     <div className="row condense expanded border-bottom align-middle">
       <div className="columns shrink">
-        <Member member={suggestion}/>
+        <Member member={suggestion} />
       </div>
       <div className="columns">
         <span className="item-title">
-          {suggestion.firstName} {suggestion.lastName}
-{' '}
+          {`${suggestion.firstName} ${suggestion.lastName}`}
         </span>
-        {suggestion.userInviteStatus == 'PENDING' && (
+        {suggestion.userInviteStatus === 'PENDING' && (
           <span className="item-details highlight">Invited</span>
         )}
       </div>
@@ -132,83 +108,60 @@ class AddListForm extends BaseComponentWithAutoComplete {
   };
 
   onChangeSuggestionSearchForMembers = (event, { newValue }) => {
-
     this.setState({
       selectedSuggestionForMember: newValue,
     });
   };
 
-  onSuggestionSelectedForAdmins = (
-    event,
-    { suggestion, suggestionValue, suggestionIndex, sectionIndex, method },
-  ) => {
-    this.setState({
-      selectedAdmins: [suggestion].concat(this.state.selectedAdmins),
+  onSuggestionSelectedForAdmins = (event, { suggestion }) => {
+    this.setState(prevState => ({
+      selectedAdmins: [suggestion].concat(prevState.selectedAdmins),
       suggestions: [],
       selectedSuggestionForAdmin: '',
-    });
+    }));
     $('.addAdminForList').click();
   };
 
-  onSuggestionSelectedForMembers = (
-    event,
-    { suggestion, suggestionValue, suggestionIndex, sectionIndex, method },
-  ) => {
-    this.setState({
-      selectedMembers: [suggestion].concat(this.state.selectedMembers),
+  onSuggestionSelectedForMembers = (event, { suggestion }) => {
+    this.setState(prevState => ({
+      selectedMembers: [suggestion].concat(prevState.selectedMembers),
       suggestions: [],
       selectedSuggestionForMember: '',
-    });
+    }));
     $('.addMemberForList').click();
   };
 
-  onSubmit(formProps) {
-    // this.sendInvitations (selectedMembers)
-    // this.sendInvitations (selectedAdmins)
+  onSubmit = formProps => {
+    const { selectedAdmins, selectedMembers } = this.state;
+    const { currentList } = this.props;
 
-    const taskList = {};
+    const taskList = { ...currentList };
     taskList.taskListId = this.props.taskListId;
     taskList.listName = formProps.listName;
     taskList.notifications = formProps.notifications;
-    taskList.admins = [];
-    if (this.state.selectedAdmins.length > 0) {
-      var k = 0;
-      for (k in this.state.selectedAdmins) {
-        taskList.admins.push(this.state.selectedAdmins[k].userId);
-      }
-    }
-    taskList.members = [];
-    if (this.state.selectedMembers.length > 0) {
-      var k = 0;
-      for (k in this.state.selectedMembers) {
-        taskList.members.push(this.state.selectedMembers[k].userId);
-      }
-    }
+    taskList.admins = selectedAdmins.map(prop('userId'));
+    taskList.members = selectedMembers.map(prop('userId'));
 
-    const component = this;
     this.props.taskListActions
       .saveTaskList(taskList)
-      .then(res => {
-        component.setState({ saveResultMessage: 'List saved successfully!!' });
-        component.props.formActions.reset('addListForm');
-        // hide the form
+      .then(() => {
+        this.setState({ saveResultMessage: 'List saved successfully!!' });
+        this.props.formActions.reset('addListForm');
         $('.add').click();
       })
       .catch(error => {
-        component.setState({
-          saveResultMessage: `${error.message}: ${
-            error.response.data.errorMessage
-          }`,
+        this.setState({
+          saveResultMessage: `${error.message}: ${error.response.data.errorMessage}`,
         });
       });
-  }
+  };
 
-  cancelEdit = (event) => {
+  cancelEdit = event => {
     toggleTaskForm();
     event.preventDefault();
   };
 
-  openMemberList = (e, triggerClass) => {
+  openMemberList = () => {
     $('.adminsList')
       .first()
       .trigger('click');
@@ -216,7 +169,6 @@ class AddListForm extends BaseComponentWithAutoComplete {
 
   render() {
     const {
-      selectedSuggestion,
       selectedSuggestionForAdmin,
       selectedSuggestionForMember,
       suggestions,
@@ -236,7 +188,10 @@ class AddListForm extends BaseComponentWithAutoComplete {
     };
 
     return (
-        <div className="add-form-wrapper" style={{marginTop: "-50px", marginBottom: "20px", width: "80%"}}>
+      <div
+        className="add-form-wrapper"
+        style={{ marginTop: '-50px', marginBottom: '20px', width: '80%' }}
+      >
         <div className="task-item add-form row expanded">
           <form
             className="inline-label"
@@ -261,42 +216,11 @@ class AddListForm extends BaseComponentWithAutoComplete {
                 <label>Owner</label>
                 <ul className="menu member-photo-list">
                   <li>
-                    <Member
-                      member={this.props.listOwner}
-                    />
+                    <Member member={this.props.listOwner} />
                   </li>
                 </ul>
               </div>
             </div>
-
-            {/* <!-- Admins --> */}
-            {/* <div className="column large-12 input-group no-icon static-label accordion" data-accordion data-allow-all-closed="true">
-              <div className="input-wrapper accordion-item" data-accordion-item>
-                <label>Admins</label>
-                <ul className="menu member-photo-list" onClick={(e) => this.openMemberList(e, 'adminsList')}>
-                  <li><span className="add-member circle medium addAdminForList">+</span></li>
-                  {this.state.selectedAdmins && this.state.selectedAdmins.map(member => {
-                    return(
-                      <li key={"list_admin_"+member.userId}><Member member={member}/></li>
-                    )
-                  })}
-                </ul>
-                <a href="#" className="adminsList accordion-title">Open Autosuggest</a>
-                <div className="accordion-content" data-tab-content>
-                    <Autosuggest
-                      suggestions={suggestions}
-                      getSuggestionValue={this.getSuggestionValue}
-                      renderSuggestion={this.renderSuggestion}
-                      onSuggestionSelected={this.onSuggestionSelectedForAdmins}
-                      inputProps={inputPropsForAdmins}
-                      onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-                      onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-                      renderSuggestionsContainer={this.renderSuggestionsContainer}
-                      renderInputComponent={this.renderInputComponent}
-                    />
-                </div>
-              </div>
-            </div> */}
 
             {/* <!-- Admins --> */}
             <div
@@ -305,7 +229,7 @@ class AddListForm extends BaseComponentWithAutoComplete {
               data-allow-all-closed="true"
             >
               <div className="input-wrapper accordion-item" data-accordion-item>
-                <a href="#" className="accordion-title">
+                <div className="accordion-title">
                   <label>Admins</label>
                   <ul className="menu member-photo-list">
                     <li>
@@ -313,18 +237,15 @@ class AddListForm extends BaseComponentWithAutoComplete {
                         +
                       </span>
                     </li>
-                    {this.state.selectedAdmins &&
-                      this.state.selectedAdmins.map(member => {
-                        return (
-                          <li key={`list_admin_${member.userId}`}>
-                            <Member
-                              member={member}
-                            />
-                          </li>
-                        );
-                      })}
+                    {this.state.selectedAdmins?.map(member => {
+                      return (
+                        <li key={`list_admin_${member.userId}`}>
+                          <Member member={member} />
+                        </li>
+                      );
+                    })}
                   </ul>
-                </a>
+                </div>
                 <div className="accordion-content" data-tab-content>
                   <Autosuggest
                     suggestions={suggestions}
@@ -352,7 +273,7 @@ class AddListForm extends BaseComponentWithAutoComplete {
               data-allow-all-closed="true"
             >
               <div className="input-wrapper accordion-item" data-accordion-item>
-                <a href="#" className="accordion-title">
+                <div className="accordion-title">
                   <label>Members</label>
                   <ul className="menu member-photo-list">
                     <li>
@@ -360,18 +281,15 @@ class AddListForm extends BaseComponentWithAutoComplete {
                         +
                       </span>
                     </li>
-                    {this.state.selectedMembers &&
-                      this.state.selectedMembers.map(member => {
-                        return (
-                          <li key={`list_member_${member.userId}`}>
-                            <Member
-                              member={member}
-                            />
-                          </li>
-                        );
-                      })}
+                    {this.state.selectedMembers?.map(member => {
+                      return (
+                        <li key={`list_member_${member.userId}`}>
+                          <Member member={member} />
+                        </li>
+                      );
+                    })}
                   </ul>
-                </a>
+                </div>
                 <div className="accordion-content" data-tab-content>
                   <Autosuggest
                     suggestions={suggestions}
@@ -451,18 +369,18 @@ class AddListForm extends BaseComponentWithAutoComplete {
   }
 }
 
-AddListForm = reduxForm({
-  // a unique name for the form
+const ConnectedAddListForm = reduxForm({
   form: 'addListForm',
   enableReinitialize: true,
 })(AddListForm);
 
-const mapStateToProps = function(store) {
+const mapStateToProps = store => {
   let initialTaskListValues = {};
   let taskListId = 0;
   let listOwner = {};
   if (store.taskListState.currentList) {
     initialTaskListValues = store.taskListState.currentList;
+    // eslint-disable-next-line prefer-destructuring
     taskListId = store.taskListState.currentList.taskListId;
     listOwner = store.taskListState.currentList.creator;
   } else {
@@ -476,16 +394,9 @@ const mapStateToProps = function(store) {
     currentList: store.taskListState.currentList,
     listOwner,
   };
-  // var initialValues = {}
-  // if(this.props.taskList){
-  //   initialValues = this.props.taskList
-  // }
-  // return{
-  //   initialValues: initialValues
-  // }
 };
 
-const mapDispatchToProps = function(dispatch) {
+const mapDispatchToProps = dispatch => {
   return {
     taskListActions: bindActionCreators(TaskListActions, dispatch),
     peopleActions: bindActionCreators(PeopleActions, dispatch),
@@ -496,4 +407,4 @@ const mapDispatchToProps = function(dispatch) {
 export default connect(
   mapStateToProps,
   mapDispatchToProps,
-)(AddListForm);
+)(ConnectedAddListForm);
