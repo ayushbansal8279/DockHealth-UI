a67f9a12f3a930f7d9dc080ed8eb3f657536ef64
Aleksander Krawiel <43279345+akrawiel@users.noreply.github.com>
2019-12-04 18:36:18 +0100

    Fix grid sizing of patient sidebar (#204)

--- src/app/components/patients/Patients.js
+++ src/app/components/patients/Patients.js
@@ -106,7 +106,11 @@ const PatientsLayout = () => {
         {isFetching ? (
           <PatientsListSpinner isFetching={isFetching} />
         ) : (
-          <Grid xs item>
+          <Grid
+            md={12}
+            lg={highlightedPatient || isCreatingPatient ? 6 : 12}
+            item
+          >
             <PatientsList
               patients={filteredPatients}
               isFiltered={searchTerm !== ''}
@@ -116,12 +120,12 @@ const PatientsLayout = () => {
           </Grid>
         )}
         {highlightedPatient && (
-          <Grid xs item>
+          <Grid md={12} lg={6} item>
             <PatientsSidebar patient={highlightedPatient} />
           </Grid>
         )}
         {isCreatingPatient && (
-          <Grid xs item>
+          <Grid md={12} lg={6} item>
             <PatientsCreation />
           </Grid>
         )}
