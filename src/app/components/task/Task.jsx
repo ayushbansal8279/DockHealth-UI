f05d193047296ad5291e43f8f095e8f3e8613469
Aleksander Krawiel <43279345+akrawiel@users.noreply.github.com>
2019-12-12 14:24:23 +0100

    Subtasks missing order fix (#222)

--- src/app/components/task/Task.jsx
+++ src/app/components/task/Task.jsx
@@ -1,15 +1,9 @@
-import { Sortable } from '@shopify/draggable';
-import insert from 'ramda/es/insert';
-import pick from 'ramda/es/pick';
-import React, { useEffect, useRef, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
 
-import { sortSubtasks } from '../../actions/task-actions';
-import { onSubtaskOrderChanged } from '../../helpers/ga-event-helper';
-import useConfirmation from '../../hooks/useConfirmation';
 import Flag from '../common/Flag';
 import AddSubtask from './AddSubtask';
 import ConfirmationDialog from './ConfirmationDialog';
+import initializeTaskHooks from './Task.Hooks';
 import {
   TaskAnimationContainer,
   TaskContainer,
@@ -23,103 +17,27 @@ const Task = props => {
   const {
     task,
     isSubtask,
-    markComplete,
     selectedTaskId,
-    storeAsCurrentTask,
     slimView,
+    subtaskIndex,
     taskDrawerOpen,
-  } = props;
-  const { subtasks, priority, taskList, isNewSubtask } = task;
+    priority,
+    taskList,
+    isNewSubtask,
+    animationContainer,
+    sortableContainer,
+    hasSubtasks,
+    renderedSubtasks,
+    isOpen,
+    close,
+    handleStatusChange,
+    confirm,
+    isSelfOrSubtaskActive,
+  } = initializeTaskHooks({ props, DRAGGABLE_ITEM_CLASS });
 
-  const {
-    addingNewSubtask,
-    addingNewSubtaskParentId,
-    subtaskShape,
-  } = useSelector(state =>
-    pick(['addingNewSubtask', 'addingNewSubtaskParentId', 'subtaskShape'])(
-      state.taskState,
-    ),
-  );
-  const animationContainer = useRef(null);
-  const sortableContainer = useRef(null);
-  const [sortable, setSortable] = useState(null);
-  const dispatch = useDispatch();
-
-  const hasSubtasks = subtasks?.length > 0;
-
-  const onSortableStop = event => {
-    const movedSubtask = subtasks[event.data.oldIndex];
-    const otherSubtasks = subtasks.filter(
-      ({ taskId }) => taskId !== movedSubtask?.taskId,
-    );
-
-    onSubtaskOrderChanged();
-
-    const newSubtasks = insert(
-      event.data.newIndex,
-      movedSubtask,
-      otherSubtasks,
-    );
-    sortSubtasks({ task, subtasks: newSubtasks }, dispatch);
-  };
-
-  useEffect(() => {
-    if (hasSubtasks) {
-      const newSortable = new Sortable(sortableContainer.current, {
-        draggable: `.${DRAGGABLE_ITEM_CLASS}`,
-      });
-
-      // eslint-disable-next-line no-unused-expressions
-      sortable?.off('sortable:stop', onSortableStop);
-
-      setSortable(newSortable);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [hasSubtasks]);
-
-  useEffect(() => {
-    // eslint-disable-next-line no-unused-expressions
-    sortable?.on('sortable:stop', onSortableStop);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [sortable]);
-
-  const isSelfOrSubtaskActive =
-    selectedTaskId === task.taskId ||
-    Boolean(
-      !isSubtask && subtasks.find(({ taskId }) => taskId === selectedTaskId),
-    );
-
-  useEffect(() => {
-    if (isNewSubtask) {
-      animationContainer.current.style.height =
-        animationContainer.current.scrollHeight;
-    }
-  }, [isNewSubtask]);
-
-  useEffect(
-    () => {
-      if (
-        slimView &&
-        subtasks.find(({ taskId }) => taskId === selectedTaskId) &&
-        !isSelfOrSubtaskActive
-      ) {
-        storeAsCurrentTask(null);
-      }
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [slimView],
-  );
-
-  const renderedSubtasks =
-    addingNewSubtask && addingNewSubtaskParentId === task?.taskId
-      ? [...subtasks, subtaskShape]
-      : subtasks;
-
-  // Check all subtasks confirmation dialog
-  const { isOpen, close, handleStatusChange, confirm } = useConfirmation(
-    task,
-    markComplete,
-  );
+  if (isSubtask && subtaskIndex == null) {
+    return null;
+  }
 
   return (
     <>
