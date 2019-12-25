9f9e3efeedfbc764890c5a0db527e1d94c17cedb
Nitin Gujral <nitin.gujral@childrens.harvard.edu>
2019-12-25 02:39:38 -0500

    default to server side sorting (unread/new + flagged first)

--- src/app/components/task/TaskList.js
+++ src/app/components/task/TaskList.js
@@ -131,6 +131,9 @@ const DEFAULT_SORTING = [
   })),
 ];
 
+const NO_SORTING = [
+];
+
 const TaskList = ({
   tasks = [],
   taskDrawerOpen,
@@ -156,7 +159,7 @@ const TaskList = ({
 
   const [taskListShowMoreIndex, setTaskListShowMoreIndex] = useState(1);
   const [isShowMoreLocked, setShowMoreLocked] = useState(false);
-  const [currentSorting, setCurrentSorting] = useState(DEFAULT_SORTING);
+  const [currentSorting, setCurrentSorting] = useState(NO_SORTING);
 
   const [newlyAddedTasks, tasksToShow] = partition(
     ({ taskId }) => newlyAddedTaskIds.includes(taskId),
@@ -193,9 +196,14 @@ const TaskList = ({
   }, [showMoreButtonVisible, isShowMoreLocked, taskListShowMoreIndex]);
 
   const onSortingChanged = useCallback(({ key }) => () => {
+    var currentSortingColumns = currentSorting;
+    if(currentSortingColumns.length == 0){
+      currentSortingColumns = DEFAULT_SORTING;
+    }
+
     const [[currentSortingColumn], otherSortingColumns] = partition(
       propEq('key', key),
-      currentSorting,
+      currentSortingColumns,
     );
 
     const order = currentSortingColumn.order === 'asc' ? 'desc' : 'asc';
@@ -214,9 +222,10 @@ const TaskList = ({
   );
 
   const sortedTasksToShow = [
-    ...newlyAddedTasks,
-    ...sortWith(reverse(sortingMethods), tasksToShow),
-  ];
+      ...newlyAddedTasks,
+      ...sortWith(reverse(sortingMethods), tasksToShow),
+    ];
+  
 
   return (
     <TaskListOuterContainer>
