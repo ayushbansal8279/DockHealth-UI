7935ef4b4ee21ae1ddf69c89f2a9f156c5ad72ec
Nitin Gujral <nitin.gujral@childrens.harvard.edu>
2020-01-10 09:32:55 -0500

    formating changes

--- src/app/components/members/MemberInvitationPopup.js
+++ src/app/components/members/MemberInvitationPopup.js
@@ -3,8 +3,10 @@ import React, { useCallback, useState } from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 
-import { inviteMultipleUsersToTaskList, 
-  getMembersByTaskListId } from '../../actions/tasklist-actions';
+import {
+  inviteMultipleUsersToTaskList,
+  getMembersByTaskListId,
+} from '../../actions/tasklist-actions';
 import ListItem from '../common/ListItem';
 import PickerHeader from '../common/PickerHeader';
 import SearchHeader from '../common/SearchHeader';
@@ -59,13 +61,10 @@ const MemberInvitationPopup = ({ members, taskList, back, close, invite }) => {
   });
 
   const [isSearching, setIsSearching] = useState(false);
-  const toggleSearch = useCallback(
-    () => {
-      setIsSearching(!isSearching);
-      setSearchTerm('');
-    },
-    [isSearching],
-  );
+  const toggleSearch = useCallback(() => {
+    setIsSearching(!isSearching);
+    setSearchTerm('');
+  }, [isSearching]);
 
   const handleClose = useCallback(() => {
     setIsSearching(false);
@@ -79,15 +78,14 @@ const MemberInvitationPopup = ({ members, taskList, back, close, invite }) => {
 
   // member search
   const matchListing = (term, { userName }) => {
-    const userNameMatches = userName && userName.toLowerCase().includes(term);
-    return userNameMatches;
+    return userName && userName.toLowerCase().includes(term);
   };
 
   const orderListings = (p1, p2) =>
     p1.lastName ? p1.lastName.localeCompare(p2.lastName) : -1;
 
   // Search
-  const searchTerms = searchTerm.toLowerCase().match(/[\S]+/g) || [];
+  const searchTerms = searchTerm.toLowerCase().match(/\S+/g) || [];
   const isMatch = listing =>
     searchTerms.every(term => matchListing(term, listing));
 
@@ -98,7 +96,7 @@ const MemberInvitationPopup = ({ members, taskList, back, close, invite }) => {
   const sortedListings = filteredListings;
 
   return (
-    <React.Fragment>
+    <>
       {isSearching ? (
         <SearchHeader handleSearch={search} handleSearchToggle={toggleSearch} />
       ) : (
@@ -120,7 +118,7 @@ const MemberInvitationPopup = ({ members, taskList, back, close, invite }) => {
           </ListItem>
         ))}
       </List>
-    </React.Fragment>
+    </>
   );
 };
 
@@ -151,9 +149,8 @@ const mapStateToProps = store => ({
 
 const mapDispatchToProps = (dispatch, { taskList }) => ({
   invite: userId => {
-    inviteMultipleUsersToTaskList(taskList.taskListId, [userId])(dispatch)
-    .then(() => 
-      getMembersByTaskListId(taskList.taskListId, 'ALL')(dispatch)
+    inviteMultipleUsersToTaskList(taskList.taskListId, [userId])(dispatch).then(
+      () => getMembersByTaskListId(taskList.taskListId, 'ALL')(dispatch),
     );
   },
 });
