d8a18a555c4af68d2fc7d1d795d7b872d16c2294
Nitin Gujral <nitin.gujral@childrens.harvard.edu>
2020-01-12 10:30:39 -0500

    disabled "edited" label pending requirement change

--- src/app/components/task/TaskBody.TaskDescriptionContainer.jsx
+++ src/app/components/task/TaskBody.TaskDescriptionContainer.jsx
@@ -1,5 +1,5 @@
 import { AnimatePresence } from 'framer-motion';
-import moment from 'moment';
+// import moment from 'moment';
 import React from 'react';
 import ReactHtmlParser from 'react-html-parser';
 
@@ -8,7 +8,7 @@ import EnvelopeIcon from '../../img/envelope.svg';
 import UpdateIndicatorIcon from '../../img/update-indicator.svg';
 import {
   CompletedBy,
-  EditedTaskDescriptionLabel,
+  // EditedTaskDescriptionLabel,
   PatientsTasklistDescription,
   PatientsTasklistInfo,
   PatientsTaskListInnerDescription,
@@ -39,8 +39,8 @@ export default ({
   description,
   taskInnerDescriptionReference,
   status,
-  createdDateTime,
-  updatedDateTime,
+  // createdDateTime,
+  // updatedDateTime,
   updated,
   formattedCreationDate,
   formattedUserName,
@@ -48,7 +48,7 @@ export default ({
   countInfoContent,
   members,
 }) => {
-  const edited = moment(updatedDateTime).isAfter(moment(createdDateTime));
+  // const edited = moment(updatedDateTime).isAfter(moment(createdDateTime));
 
   return (
     <PatientTasklistContainer isSubtask={isSubtask}>
@@ -91,11 +91,11 @@ export default ({
                   value: description || 'Unnamed task',
                 }),
               )}
-              {edited && (
+              {/* {edited && (
                 <EditedTaskDescriptionLabel>
                   (edited)
                 </EditedTaskDescriptionLabel>
-              )}
+              )} */}
               <PatientsTasklistStrikeThrough
                 hasDescription={Boolean(description)}
                 active={status === 'COMPLETE'}
