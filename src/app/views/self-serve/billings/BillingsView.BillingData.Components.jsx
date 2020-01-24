c3c596bb4d35c9209d778061cfbc6320deda7d9d
Aleksander Krawiel <43279345+akrawiel@users.noreply.github.com>
2020-01-24 19:24:24 +0100

    Change styling of billing view payment information (#281)

--- src/app/views/self-serve/billings/BillingsView.BillingData.Components.jsx
+++ src/app/views/self-serve/billings/BillingsView.BillingData.Components.jsx
@@ -1,6 +1,8 @@
 import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 import InputBase from '@material-ui/core/InputBase';
 import InputLabel from '@material-ui/core/InputLabel';
+import Radio from '@material-ui/core/Radio';
 import withStyles from '@material-ui/core/styles/withStyles';
 import styled from 'styled-components';
 import { ButtonBase } from '@material-ui/core';
@@ -9,8 +11,7 @@ import React from 'react';
 export const StyledFormControl = withStyles({
   root: {
     backgroundColor: '#f3f5f6',
-    height: '3rem',
-    marginBottom: '0.5rem',
+    height: '4rem',
   },
 })(FormControl);
 
@@ -19,7 +20,7 @@ export const StyledInputLabel = withStyles({
     color: '#2e3a43',
     pointerEvents: 'none',
     top: '50%',
-    transform: 'translate(0.5rem, -50%) scale(1)',
+    transform: 'translate(1rem, -50%) scale(1)',
     transition: 'all 200ms cubic-bezier(0.0, 0, 0.2, 1)',
     zIndex: 2,
   },
@@ -31,7 +32,7 @@ export const StyledInputLabel = withStyles({
   shrink: {
     color: '#ababb2',
     top: '5%',
-    transform: 'translate(0.5rem, 0) scale(0.75)',
+    transform: 'translate(1rem, 0) scale(0.75)',
     transformOrigin: 'center left',
     transition: 'all 200ms cubic-bezier(0.0, 0, 0.2, 1)',
   },
@@ -50,7 +51,10 @@ export const StyledInputBase = withStyles({
     boxShadow: 'none',
     fontFamily: '"Open Sans", sans-serif',
     paddingBottom: 0,
-    padding: '0.6rem 0.5rem',
+    padding: '0.5rem 1rem',
+    '&[disabled]': {
+      backgroundColor: '#f3f5f6',
+    },
     '&:focus': {
       backgroundColor: '#f3f5f6',
       border: 0,
@@ -64,7 +68,7 @@ export const BillingElementContainer = styled.div`
   display: flex;
   cursor: text;
   height: 4rem;
-  padding: 0.5rem;
+  padding: 0.5rem 1rem;
   padding-bottom: 0;
   justify-content: flex-start;
   width: 100%;
@@ -79,7 +83,7 @@ export const BillingButton = withStyles({
   root: {
     borderRadius: '0.25rem',
     height: '3rem',
-    width: '100%',
+    padding: '0.25rem 0.5rem',
   },
   contained: {
     backgroundColor: '#074a86',
@@ -87,9 +91,35 @@ export const BillingButton = withStyles({
   },
   outlined: {
     color: '#074a86',
+    height: '2rem',
   },
-})(({ classes, variant, ...props }) => {
-  const className = `${classes.root} ${classes[variant]}`.trim();
+  outlinedHigh: {
+    color: '#074a86',
+  },
+  fullWidth: {
+    width: '100%',
+  },
+})(({ classes, variant, fullWidth, ...props }) => {
+  const className = `${classes.root} ${classes[variant]} ${
+    fullWidth ? classes.fullWidth : ''
+  }`.trim();
 
   return <ButtonBase className={className} {...props} />;
 });
+
+export const BillingControlLabel = withStyles({
+  label: {
+    color: '#000',
+    fontSize: '1rem',
+    opacity: 1,
+  },
+})(FormControlLabel);
+
+export const BillingRadio = withStyles({
+  checked: {
+    color: '#125375',
+  },
+  disabled: {
+    color: '#12537560',
+  },
+})(Radio);
--- src/app/views/self-serve/billings/BillingsView.BillingData.jsx
+++ src/app/views/self-serve/billings/BillingsView.BillingData.jsx
@@ -1,12 +1,8 @@
-import {
-  FormControlLabel,
-  MenuItem,
-  Radio,
-  RadioGroup,
-  Select,
-} from '@material-ui/core';
+import MenuItem from '@material-ui/core/MenuItem';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Select from '@material-ui/core/Select';
 import Grid from '@material-ui/core/Grid';
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import {
   CardCVCElement,
   CardExpiryElement,
@@ -15,6 +11,7 @@ import {
 } from 'react-stripe-elements';
 import { useToggle } from 'react-use';
 
+import useForm from 'react-hook-form';
 import useBoolean from '../../../hooks/useBoolean';
 import {
   BillingElementContainer,
@@ -22,11 +19,14 @@ import {
   StyledInputBase,
   StyledInputLabel,
   BillingButton,
+  BillingControlLabel,
+  BillingRadio,
 } from './BillingsView.BillingData.Components';
+import { H2 } from './BillingsView.Styled';
 
 const PAYMENT_METHODS = {
-  CREDIT: Symbol('CREDIT'),
-  ACH: Symbol('ACH'),
+  CREDIT: 'CREDIT',
+  ACH: 'ACH',
 };
 
 const billingElementStyling = {
@@ -36,26 +36,43 @@ const billingElementStyling = {
   },
 };
 
-const BillingElement = ({ Component, label, placeholder, alwaysShrink }) => {
+/**
+ * @todo Add save billing information + Stripe handling
+ *
+ * @param stripe - Stripe instance
+ * @param unsetUpdatingBilling - method to finish updating billing
+ */
+const onSubmit = ({ stripe, unsetUpdatingBilling }) => data => {
+  alert(JSON.stringify(data, null, 2));
+  console.log(stripe);
+  unsetUpdatingBilling();
+};
+
+const BillingElement = ({
+  Component,
+  disabled,
+  label,
+  placeholder,
+  alwaysShrink,
+}) => {
   const [isFocused, setFocused, unsetFocused] = useBoolean(false);
   const [isEmpty, setEmpty] = useToggle(true);
-  const componentReference = useRef(null);
+  const [componentReference, setComponentReference] = useState(null);
 
   return (
-    <StyledFormControl fullWidth>
+    <StyledFormControl fullWidth onClick={() => componentReference?.focus()}>
       <StyledInputLabel shrink={isFocused || !isEmpty || alwaysShrink} required>
         {label}
       </StyledInputLabel>
-      <BillingElementContainer
-        onClick={() => componentReference.current?._ref.click()}
-      >
+      <BillingElementContainer>
         <Component
           onChange={({ empty }) => setEmpty(empty)}
           placeholder={isFocused ? placeholder : ''}
           onFocus={setFocused}
           onBlur={unsetFocused}
           style={billingElementStyling}
-          ref={componentReference}
+          onReady={reference => setComponentReference(reference)}
+          disabled={disabled}
         />
       </BillingElementContainer>
     </StyledFormControl>
@@ -64,72 +81,196 @@ const BillingElement = ({ Component, label, placeholder, alwaysShrink }) => {
 
 const SaveBillingElement = ({ isUpdatingBilling, unsetUpdatingBilling }) =>
   isUpdatingBilling && (
-    <>
-      <Grid item sm={8} md={4}>
-        <BillingButton onClick={unsetUpdatingBilling} variant="contained">
-          Save billing
-        </BillingButton>
-      </Grid>
-      <Grid item sm={4} md={2}>
-        <BillingButton onClick={unsetUpdatingBilling} variant="outlined">
+    <Grid item sm={12} container justify="flex-end">
+      <Grid item sm={6} md={2}>
+        <BillingButton
+          fullWidth
+          onClick={unsetUpdatingBilling}
+          variant="outlinedHigh"
+        >
           Cancel
         </BillingButton>
       </Grid>
-    </>
+      <Grid item sm={6} md={4}>
+        <BillingButton fullWidth type="submit" variant="contained">
+          Save billing information
+        </BillingButton>
+      </Grid>
+    </Grid>
   );
 
-const CreditPaymentForm = ({ isUpdatingBilling, unsetUpdatingBilling }) => (
-  <>
-    <Grid item sm={12} md={6}>
-      <StyledFormControl fullWidth>
-        <StyledInputLabel required>Name on card</StyledInputLabel>
-        <StyledInputBase name="nameOnCard" placeholder="Name" />
-      </StyledFormControl>
-    </Grid>
-    <Grid item sm={12} md={6}>
-      <BillingElement
-        Component={CardNumberElement}
-        label="Card number"
-        placeholder="1234 1234 1234 1234"
+const BillingRadioGroup = ({
+  isUpdatingBilling,
+  setSelectedPaymentMethod,
+  register,
+}) => (
+  <Grid item md={6}>
+    <RadioGroup name="paymentMethod" row defaultValue={PAYMENT_METHODS.CREDIT}>
+      <BillingControlLabel
+        value={PAYMENT_METHODS.CREDIT}
+        control={
+          <BillingRadio
+            color="default"
+            disabled={!isUpdatingBilling}
+            onClick={() => setSelectedPaymentMethod(PAYMENT_METHODS.CREDIT)}
+            inputRef={register}
+          />
+        }
+        label="Credit Card / ATM Card"
+        labelPlacement="right"
       />
-    </Grid>
-    <Grid item sm={12} md={6}>
-      <BillingElement
-        Component={CardExpiryElement}
-        label="Expiration date"
-        placeholder="MM/YY"
+      <BillingControlLabel
+        value={PAYMENT_METHODS.ACH}
+        control={
+          <BillingRadio
+            color="default"
+            disabled
+            onClick={() => setSelectedPaymentMethod(PAYMENT_METHODS.ACH)}
+            inputRef={register}
+          />
+        }
+        label="ACH"
+        labelPlacement="right"
       />
-    </Grid>
-    <Grid item sm={12} md={6}>
-      <BillingElement
-        Component={CardCVCElement}
-        label="CVC"
-        placeholder="CVC Code"
-      />
-    </Grid>
-    {isUpdatingBilling && (
-      <>
-        <Grid item sm={12}>
-          <StyledFormControl fullWidth>
-            <StyledInputLabel required>Billing address</StyledInputLabel>
-            <StyledInputBase name="billingAddress" placeholder="Address" />
-          </StyledFormControl>
-        </Grid>
-        <Grid item sm={12} md={6}>
-          <StyledFormControl fullWidth>
-            <StyledInputLabel required>Postal Code</StyledInputLabel>
-            <StyledInputBase name="postalCode" placeholder="Postal code" />
-          </StyledFormControl>
-        </Grid>
-      </>
-    )}
-    <SaveBillingElement
-      isUpdatingBilling={isUpdatingBilling}
-      unsetUpdatingBilling={unsetUpdatingBilling}
-    />
-  </>
+    </RadioGroup>
+  </Grid>
 );
 
+const PaymentInformationLabel = () => (
+  <Grid item xs={12}>
+    <H2>Payment information</H2>
+  </Grid>
+);
+
+const CreditPaymentForm = ({
+  isUpdatingBilling,
+  unsetUpdatingBilling,
+  setSelectedPaymentMethod,
+  register,
+}) => {
+  return (
+    <>
+      {isUpdatingBilling && (
+        <>
+          <Grid item sm={12}>
+            <H2>Billing information</H2>
+          </Grid>
+          <Grid item sm={12} container justify="space-between">
+            <BillingRadioGroup
+              isUpdatingBilling={isUpdatingBilling}
+              setSelectedPaymentMethod={setSelectedPaymentMethod}
+              register={register}
+            />
+          </Grid>
+          <Grid item sm={12}>
+            <StyledFormControl fullWidth>
+              <StyledInputLabel required>Name</StyledInputLabel>
+              <StyledInputBase
+                inputRef={register}
+                name="name"
+                placeholder="Name"
+              />
+            </StyledFormControl>
+          </Grid>
+          <Grid item sm={12}>
+            <StyledFormControl fullWidth>
+              <StyledInputLabel required>Email</StyledInputLabel>
+              <StyledInputBase
+                inputRef={register}
+                name="email"
+                placeholder="Email"
+              />
+            </StyledFormControl>
+          </Grid>
+          <Grid item sm={12}>
+            <StyledFormControl fullWidth>
+              <StyledInputLabel required>Address</StyledInputLabel>
+              <StyledInputBase
+                inputRef={register}
+                name="address"
+                placeholder="Address"
+              />
+            </StyledFormControl>
+          </Grid>
+          <Grid item sm={12}>
+            <StyledFormControl fullWidth>
+              <StyledInputLabel required>City</StyledInputLabel>
+              <StyledInputBase
+                inputRef={register}
+                name="city"
+                placeholder="city"
+              />
+            </StyledFormControl>
+          </Grid>
+          <Grid item sm={12} md={6}>
+            <StyledFormControl fullWidth>
+              <StyledInputLabel required>State</StyledInputLabel>
+              <StyledInputBase
+                inputRef={register}
+                name="state"
+                placeholder="State"
+              />
+            </StyledFormControl>
+          </Grid>
+          <Grid item sm={12} md={6}>
+            <StyledFormControl fullWidth>
+              <StyledInputLabel required>Zip</StyledInputLabel>
+              <StyledInputBase
+                inputRef={register}
+                name="zip"
+                placeholder="Zip"
+              />
+            </StyledFormControl>
+          </Grid>
+          <PaymentInformationLabel />
+        </>
+      )}
+      <Grid item sm={12} md={isUpdatingBilling ? 12 : 6}>
+        <BillingElement
+          Component={CardNumberElement}
+          label="Card number"
+          placeholder="1234 1234 1234 1234"
+          disabled={!isUpdatingBilling}
+        />
+      </Grid>
+      <Grid item sm={12} md={isUpdatingBilling ? 12 : 6}>
+        <StyledFormControl fullWidth>
+          <StyledInputLabel required>Name on card</StyledInputLabel>
+          <StyledInputBase
+            name="nameOnCard"
+            inputRef={register}
+            placeholder="Name"
+            disabled={!isUpdatingBilling}
+          />
+        </StyledFormControl>
+      </Grid>
+      <Grid item sm={12} md={6}>
+        <BillingElement
+          Component={CardExpiryElement}
+          label="Expiration date"
+          placeholder="MM/YY"
+          disabled={!isUpdatingBilling}
+        />
+      </Grid>
+      <Grid item sm={12} md={6}>
+        <BillingElement
+          Component={CardCVCElement}
+          label="CVC"
+          placeholder="CVC Code"
+          disabled={!isUpdatingBilling}
+        />
+      </Grid>
+      <SaveBillingElement
+        isUpdatingBilling={isUpdatingBilling}
+        unsetUpdatingBilling={unsetUpdatingBilling}
+      />
+    </>
+  );
+};
+
+/**
+ * @deprecated
+ */
 const ACHPaymentForm = ({ isUpdatingBilling, unsetUpdatingBilling }) => {
   const [accountType, setAccountType] = useState('');
 
@@ -189,7 +330,7 @@ const ACHPaymentForm = ({ isUpdatingBilling, unsetUpdatingBilling }) => {
   );
 };
 
-const BillingData = () => {
+const BillingData = ({ stripe }) => {
   const [selectedPaymentMethod, setSelectedPaymentMethod] = useState(
     PAYMENT_METHODS.CREDIT,
   );
@@ -200,52 +341,33 @@ const BillingData = () => {
     unsetUpdatingBilling,
   ] = useBoolean(false);
 
+  const { handleSubmit, register } = useForm();
+
   return (
-    <>
-      <Grid container spacing={8} justify="space-between">
-        <RadioGroup
-          name="paymentMethod"
-          row
-          defaultValue={PAYMENT_METHODS.CREDIT.toString()}
-        >
-          <FormControlLabel
-            value={PAYMENT_METHODS.CREDIT.toString()}
-            control={
-              <Radio
-                disabled={!isUpdatingBilling}
-                onClick={() => setSelectedPaymentMethod(PAYMENT_METHODS.CREDIT)}
-                style={{ color: '#125375' }}
-              />
-            }
-            label="Credit Card / ATM Card"
-            labelPlacement="top"
+    <form onSubmit={handleSubmit(onSubmit({ stripe, unsetUpdatingBilling }))}>
+      {!isUpdatingBilling && (
+        <Grid container spacing={16} justify="space-between">
+          <Grid item sm={12}>
+            <PaymentInformationLabel />
+          </Grid>
+          <BillingRadioGroup
+            isUpdatingBilling={isUpdatingBilling}
+            setSelectedPaymentMethod={setSelectedPaymentMethod}
           />
-          <FormControlLabel
-            value={PAYMENT_METHODS.ACH.toString()}
-            control={
-              <Radio
-                disabled={!isUpdatingBilling}
-                onClick={() => setSelectedPaymentMethod(PAYMENT_METHODS.ACH)}
-                style={{ color: '#125375' }}
-              />
-            }
-            label="ACH"
-            labelPlacement="top"
-          />
-        </RadioGroup>
-        {!isUpdatingBilling && (
-          <Grid item md={2} sm={12} container justify="flex-end">
+          <Grid item md={6} sm={12} container justify="flex-end">
             <BillingButton onClick={setUpdatingBilling} variant="outlined">
-              Update billing
+              Update billing information
             </BillingButton>
           </Grid>
-        )}
-      </Grid>
-      <Grid container spacing={8}>
+        </Grid>
+      )}
+      <Grid container spacing={16}>
         {selectedPaymentMethod === PAYMENT_METHODS.CREDIT && (
           <CreditPaymentForm
             isUpdatingBilling={isUpdatingBilling}
             unsetUpdatingBilling={unsetUpdatingBilling}
+            setSelectedPaymentMethod={selectedPaymentMethod}
+            register={register}
           />
         )}
         {selectedPaymentMethod === PAYMENT_METHODS.ACH && (
@@ -255,7 +377,7 @@ const BillingData = () => {
           />
         )}
       </Grid>
-    </>
+    </form>
   );
 };
 
