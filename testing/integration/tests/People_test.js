10e492ac9c6d70359330d4ad3964028b3635234b
GardenGorgon <gbousvaros2015@gmail.com>
2020-01-29 12:05:04 -0500

    People_test works also.

--- testing/integration/tests/People_test.js
+++ testing/integration/tests/People_test.js
@@ -7,7 +7,8 @@ Scenario('Try to invite a user, get shut out', (I, lgnPg, tskLstPg, peoplePage) 
     peoplePage.disarmNavigationSidebar();
     I.wait();
     peoplePage.clickAddPersonButton();
-    I.wait();
+    I.wait(4);
+    //pause();
     I.see('Because you are not an administrator');
 });
 
@@ -18,7 +19,7 @@ Scenario('Invite a user succesfully', (I, lgnPg, tskLstPg, peoplePage) => {
     peoplePage.clickAddPersonButton();
     I.wait(3);
     //pause();
-    peoplePage.fillInvite('Gorgeous', 'Greg', 'george+a7@dock.health');
+    peoplePage.fillInvite('Heroic', 'Helen', 'george+a8@dock.health');
     peoplePage.sendInvite();
     //I.amOnPage('https://mail.google.com/mail/u/0/#inbox');
 
@@ -38,6 +39,8 @@ Scenario('Search for a user', (I, lgnPg, tskLstPg, peoplePage) => {
     lgnPg.fullLogin(2);
     tskLstPg.enterPeople();
     peoplePage.disarmNavigationSidebar();
+    I.wait(4);
+    //pause();
     peoplePage.makeSearch("Dig");
     I.wait(3);
     peoplePage.clickPerson(1);
@@ -45,6 +48,8 @@ Scenario('Search for a user', (I, lgnPg, tskLstPg, peoplePage) => {
     I.see('Dig Dug');
 });
 
+
+//Broken due to Un-Admin not being finished.
 Scenario('Give Bart admin, then take it away.', (I, lgnPg, tskLstPg, peoplePage) => {
     lgnPg.fullLogin(2);
     I.wait();
@@ -55,9 +60,10 @@ Scenario('Give Bart admin, then take it away.', (I, lgnPg, tskLstPg, peoplePage)
     peoplePage.makeSearch("Bart");
     I.wait();
     peoplePage.switchAdminStatus(1);
-    I.wait(2);
+    I.wait(3);
     I.see('Admin');
     peoplePage.switchAdminStatus(1);
+    I.wait(3);
     //pause();
     //It's broke
     I.dontSee('Admin')
