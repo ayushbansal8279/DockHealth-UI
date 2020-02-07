b442b9114551878ff6745c8bf4800ee8d0f8abb6
Nitin Gujral <nitin.gujral@childrens.harvard.edu>
2020-02-07 11:54:29 -0500

    disabled invoices for now

--- src/app/views/self-serve/billings/BillingsView.jsx
+++ src/app/views/self-serve/billings/BillingsView.jsx
@@ -120,7 +120,7 @@ const BillingsView = () => {
           SaveBillingElement={SaveBillingElement}
         />
       </Elements>
-      <InvoicesList />
+      {/* <InvoicesList /> */}
     </BillingsViewContainer>
   );
 };
