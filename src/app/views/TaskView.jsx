ef1c29619ca766ae499a50a66eaf2e588825640a
Nitin Gujral <nitin.gujral@childrens.harvard.edu>
2020-03-08 13:09:32 -0400

    updated pusher handling to only accept specific tasklist events

--- src/app/views/TaskView.jsx
+++ src/app/views/TaskView.jsx
@@ -59,6 +59,10 @@ const APP_CLUSTER = process.env.PUSHER_CLUSTER_NAME;
 
 const SHOW_MORE_STEP_COUNT = 100;
 
+const pusher = new Pusher(APP_KEY, {
+  cluster: APP_CLUSTER,
+});
+
 const groupBy = (list, keyGetter) => {
   const checkMap = new Map();
 
@@ -240,7 +244,7 @@ class TaskView extends Component {
   };
 
   componentWillUpdate(nextProps) {
-    const { taskList } = this.props;
+    const { taskList, currentUser } = this.props;
 
     if (
       (!taskList && nextProps && nextProps.taskList) ||
@@ -248,7 +252,7 @@ class TaskView extends Component {
         nextProps.taskList &&
         taskList.taskListIdentifier !== nextProps.taskList.taskListIdentifier)
     ) {
-      // this.listenForRealTimeEvents(nextProps.taskList);
+      this.listenForRealTimeEvents(nextProps.taskList, currentUser);
     }
   }
 
@@ -326,33 +330,47 @@ class TaskView extends Component {
       return;
     }
     const { refreshTask, taskActions } = this.props;
-    const socket = new Pusher(APP_KEY, {
-      cluster: APP_CLUSTER,
-    });
-
     const currentUserIdentifier = currentUser.userIdentifier;
     const channelName = `dock-user-channel-${currentUserIdentifier}`;
-    const channel = socket.subscribe(channelName);
+
+    var channel = pusher.channel(channelName);
+    if(!channel){
+      channel = pusher.subscribe(channelName);
+      console.log('subscribed to channel')
+    }
+    channel.bind('pusher:subscription_succeeded', function() {
+      console.log('subscription_succeeded')
+    });
+    channel.bind('pusher:subscription_error', function(status) {
+      console.log('subscription_error',status)
+    });
+    console.log(channel)
     // Listen to the channel for new entries.
     // The server publishes to this channel whenever a entry is updated
+
     channel.bind('task-update', data => {
       // Since the app is going to be realtime, we don't want the same item to
       // be shown twice. Device A publishes an entry, all other devices including itself
       // receives the entry, so act like a basic filter
-      if (
-        (data.eventType?.startsWith('CREATE_TASK') ||
-          data.eventType?.startsWith('DUPLICATE_TASK')) &&
-        data.task?.taskList
-      ) {
-        taskActions.getListTasks(
-          data.task.taskList.taskListIdentifier,
-          null,
-          null,
-          'INCOMPLETE',
-        );
+      console.log(data)
+      const currentTaskListIdentifier = taskList?.taskListIdentifier;
+      if(data.task?.taskList 
+        && data.task?.taskList.taskListIdentifier === currentTaskListIdentifier){
+        if (
+          (data.eventType?.startsWith('CREATE_TASK') ||
+            data.eventType?.startsWith('DUPLICATE_TASK')) &&
+          data.task?.taskList
+        ) {
+          taskActions.getListTasks(
+            data.task.taskList.taskListIdentifier,
+            null,
+            null,
+            'INCOMPLETE',
+          );
+        }
+        // eslint-disable-next-line no-unused-expressions
+        refreshTask?.(data.task);
       }
-      // eslint-disable-next-line no-unused-expressions
-      refreshTask?.(data.task);
     });
   };
 
@@ -373,6 +391,19 @@ class TaskView extends Component {
       .forEach(({ taskTimeoutId }) => {
         clearTimeout(taskTimeoutId);
       });
+
+    console.log('unsubscribing from channel')
+    const { currentUser } = this.props;
+    const currentUserIdentifier = currentUser?.userIdentifier;
+    if(currentUserIdentifier){
+      const channelName = `dock-user-channel-${currentUserIdentifier}`;
+      var channel = pusher.channel(channelName);
+      if(channel){
+        channel = pusher.unsubscribe(channelName);
+        console.log('unsubscribed from channel')
+      }
+    }
+  
   };
 
   clearTaskTimeouts = (taskTimeoutId, callback = () => {}) => {
