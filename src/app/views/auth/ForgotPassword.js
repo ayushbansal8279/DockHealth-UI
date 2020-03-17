7c5c23815864cd9aefeebb803ebaa2606fcd3d63
GardenGorgon <gbousvaros2015@gmail.com>
2020-03-17 16:18:15 -0400

    Forgot password page cna handle non-confirmed user.

--- src/app/views/auth/ForgotPassword.js
+++ src/app/views/auth/ForgotPassword.js
@@ -1,13 +1,16 @@
-import React, { PureComponent } from 'react';
+import React, { useCallback, useState } from 'react';
 import { hashHistory } from 'react-router';
 
 import { error, success } from '../../actions/notification-actions';
 import { mobileAnalyticsClient } from '../../api/analytics-api';
 import * as userApi from '../../api/user-api';
 import ForgotPasswordForm from '../../components/auth/ForgotPasswordForm';
+import { showAlert, showToast } from '../../helpers/utility-functions';
 
-export default class ForgotPassword extends PureComponent {
-  onSubmit = form =>
+const ForgotPassword = () => {
+  const [unconfirmedUserFlag, setUnconfirmedUserFlag] = useState(false);
+
+  const onSubmit = useCallback(({ setError }) => form => {
     userApi
       .forgotPassword({
         username: form.username,
@@ -19,24 +22,78 @@ export default class ForgotPassword extends PureComponent {
         success(
           `Sent verification code to: ${resp.CodeDeliveryDetails.Destination}`,
         );
-        window.sessionStorage.setItem('username', form.username)
-        if(resp.CodeDeliveryDetails){
-          const deliveryMedium = resp.CodeDeliveryDetails.DeliveryMedium
-          if(deliveryMedium == "EMAIL"){
+        window.sessionStorage.setItem('username', form.username);
+        if (resp.CodeDeliveryDetails) {
+          const deliveryMedium = resp.CodeDeliveryDetails.DeliveryMedium;
+          if (deliveryMedium === 'EMAIL') {
             hashHistory.push('resetPasswordEmailSent');
-          }else{
+          } else {
             hashHistory.push('resetPassword');
           }
         }
       })
-      .catch(e => {
+      .catch(error_ => {
+        let message = error_?.message;
+        const errorCode = error_?.code;
+        if (errorCode === 'InvalidParameterException') {
+          message =
+            'Email is not confirmed. Please check your email or click below to resend.'; // User is not confirmed.
+          setUnconfirmedUserFlag(true);
+        }
         mobileAnalyticsClient.recordEvent('AUTH_EVENTS', {
           FORGOT_PASSWORD_SUCCESS: 'NO',
         });
-        const msg = e.message || 'An error occurred.';
 
-        error(msg);
+        setError(
+          'username',
+          'invalid',
+          message ?? 'Unknown Error. Please try again.',
+
+          // (error?.message!='User is not confirmed.') ?? 'Incorrect email or password. Please try again.',
+        );
       });
+  });
 
-  render = () => <ForgotPasswordForm type="Confirm" onSubmit={this.onSubmit} />;
-}
+  const onResendCode = useCallback(
+    ({ setError }) => form => {
+      try {
+        userApi
+          .resendConfirmationCode({
+            username: form.username,
+          })
+          .then(() => {
+            showToast({
+              status: 'success',
+              title: 'Account confirmation email resent',
+            });
+            setUnconfirmedUserFlag(false);
+            form.password = ''; // Erroring.
+          });
+      } catch (error_) {
+        showAlert({
+          icon: 'error',
+          title: 'Error',
+          text:
+            error_?.message ?? 'Could not resend email, please try again later',
+        });
+      }
+    },
+    [],
+  );
+
+  const onChange = () => {
+    setUnconfirmedUserFlag(false);
+  };
+
+  return (
+    <ForgotPasswordForm
+      type="Confirm"
+      onSubmit={onSubmit}
+      onResendCode={onResendCode}
+      onChange={onChange}
+      unconfirmedUserFlag={unconfirmedUserFlag}
+    />
+  );
+};
+
+export default ForgotPassword;
