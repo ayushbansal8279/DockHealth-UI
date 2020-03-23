e0a5fde1d5eb2e8bc056fb2dd4e03794ca49db4f
Nitin Gujral <nitin.gujral@childrens.harvard.edu>
2020-03-23 14:49:18 -0400

    disabled network error check for now since it causes issues on login pages as well

--- src/app/api/axios-heydoc.js
+++ src/app/api/axios-heydoc.js
@@ -8,22 +8,22 @@ const axiosInstance = axios.create({
   baseURL: process.env.HEYDOC_SERVICES_BASE_URL,
 });
 
-axiosInstance.interceptors.response.use(identity, error => {
-  if (error.response) {
-    localStorage.setItem(NETWORK_ERROR, 'false');
-  } else if (localStorage.getItem(NETWORK_ERROR) === 'false') {
-    localStorage.setItem(NETWORK_ERROR, 'true');
-    window.location.href = '/';
-  } else {
-    showAlert({
-      status: 'error',
-      title: 'Connection error',
-      text: 'Check your Internet connection and refresh the page',
-      confirmButtonText: 'Refresh page',
-    }).then(() => {
-      window.location.href = '/';
-    });
-  }
-});
+// axiosInstance.interceptors.response.use(identity, error => {
+//   if (error.response) {
+//     localStorage.setItem(NETWORK_ERROR, 'false');
+//   } else if (localStorage.getItem(NETWORK_ERROR) === 'false') {
+//     localStorage.setItem(NETWORK_ERROR, 'true');
+//     window.location.href = '/';
+//   } else {
+//     showAlert({
+//       status: 'error',
+//       title: 'Connection error',
+//       text: 'Check your Internet connection and refresh the page',
+//       confirmButtonText: 'Refresh page',
+//     }).then(() => {
+//       window.location.href = '/';
+//     });
+//   }
+// });
 
 export default axiosInstance;
