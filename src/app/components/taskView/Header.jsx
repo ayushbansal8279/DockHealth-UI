576dbaea8bde4d3ba4ce82b110aaccb7def3cadd
Aleksander Krawiel <a.krawiel@gmail.com>
2020-04-02 17:38:39 +0200

    Update list header caret color

--- src/app/components/taskView/Header.jsx
+++ src/app/components/taskView/Header.jsx
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 import { hashHistory } from 'react-router';
 import styled from 'styled-components';
 import useBoolean from '../../hooks/useBoolean';
+import palette from '../../palette';
 import GenericHeader from '../common/GenericHeader';
 import ListPopover from '../common/ListPopover';
 import { RotatableHeaderChevron } from '../common/RotatableChevron';
@@ -82,6 +83,7 @@ const Header = ({ hasTitle, title, isFetching, taskList }) => {
           <Spacing horizontal={3} />
           <RotatableHeaderChevron
             rotated={isListPopoverOpen}
+            color={palette.oPlusRed}
             ref={listPopoverReference}
           />
         </StyledTitle>
