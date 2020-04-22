59ab571b51fd39e67d0b8a3ba956cf95c0d31336
GardenGorgon <gbousvaros2015@gmail.com>
2020-04-22 15:15:31 -0400

    Invited designation is recolored to white on grey.

--- src/app/views/self-serve/subscriptions/SubscriptionsView.MemberTypeLabel.Components.js
+++ src/app/views/self-serve/subscriptions/SubscriptionsView.MemberTypeLabel.Components.js
@@ -16,7 +16,7 @@ export const MemberTypeLabelButton = styled.div`
 
   ${props =>
     props.invited
-      ? `background-color: ${palette.lightGrey}; color: ${palette.coolGrey2};`
+      ? `background-color: ${palette.scrollbarGrey}; color: ${palette.white};`
       : `background-color: ${palette.coolGrey2}; color: ${palette.white};`}
 
   &:hover {
