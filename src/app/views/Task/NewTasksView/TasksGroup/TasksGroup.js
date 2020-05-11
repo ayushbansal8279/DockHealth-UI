d557f0fc31c2a781f6c7069eb5a319de4f9dd601
dwojcik96 <dwojcik@classpass.com>
2020-05-11 15:41:52 +0200

    Add slim and full view icons

--- src/app/views/Task/NewTasksView/TasksGroup/TasksGroup.js
+++ src/app/views/Task/NewTasksView/TasksGroup/TasksGroup.js
@@ -1,7 +1,22 @@
 import React, { useState } from 'react';
 import ArrowIcon from 'img/arrow';
-import { Arrow, TasksGroupContainer, TasksGroupLabel, Tasks } from './styled';
+import FullViewIcon from 'img/full-view';
+import FullViewActiveIcon from 'img/full-view-active';
+import SlimViewIcon from 'img/slim-view';
+import SlimViewActiveIcon from 'img/slim-view-active';
 import Task from '../TaskItem/TaskItem';
+
+import {
+  Arrow,
+  TasksGroupContainer,
+  TasksGroupHeader,
+  TasksGroupLabel,
+  Tasks,
+  ViewIcon,
+} from './styled';
+
+const FULL_VIEW = 'FULL_VIEW';
+const SLIM_VIEW = 'SLIM_VIEW';
 
 const TasksGroup = ({
   currentUser,
@@ -12,24 +27,40 @@ const TasksGroup = ({
   tasks,
 }) => {
   const [isOpen, switchOpen] = useState(false);
+  const [viewType, setViewType] = useState(FULL_VIEW);
+  const isFullView = viewType === FULL_VIEW;
 
   return (
     <TasksGroupContainer>
-      <TasksGroupLabel>
-        <Arrow
-          alt="arrow"
-          isOpen={isOpen}
-          onClick={() => switchOpen(!isOpen)}
-          src={ArrowIcon}
-        />
-        {groupName} ({tasks?.length || 0})
-      </TasksGroupLabel>
-
+      <TasksGroupHeader>
+        <TasksGroupLabel>
+          <Arrow
+            alt="arrow"
+            isOpen={isOpen}
+            onClick={() => switchOpen(!isOpen)}
+            src={ArrowIcon}
+          />
+          {groupName} ({tasks?.length || 0})
+        </TasksGroupLabel>
+        <div>
+          <ViewIcon
+            alt="slim-view"
+            src={isFullView ? SlimViewIcon : SlimViewActiveIcon}
+            onClick={() => setViewType(SLIM_VIEW)}
+          />
+          <ViewIcon
+            alt="full-view"
+            src={isFullView ? FullViewActiveIcon : FullViewIcon}
+            onClick={() => setViewType(FULL_VIEW)}
+          />
+        </div>
+      </TasksGroupHeader>
       <Tasks timeout={150} in={isOpen}>
         {tasks?.map(task => (
           <Task
             key={task.taskId}
             currentUser={currentUser}
+            isFullView={isFullView}
             markComplete={markComplete}
             openDrawer={openDrawer}
             storeAsCurrentTask={storeAsCurrentTask}
