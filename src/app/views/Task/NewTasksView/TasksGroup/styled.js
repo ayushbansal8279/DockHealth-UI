913d92a9ad8bf6d2a9bd523a752c4b7250aeb625
Maciej Laufer <maciek.laufer@gmail.com>
2020-05-12 17:11:46 +0200

    Add empty list view

--- src/app/views/Task/NewTasksView/TasksGroup/styled.js
+++ src/app/views/Task/NewTasksView/TasksGroup/styled.js
@@ -2,35 +2,6 @@ import { Collapse } from '@material-ui/core';
 import styled from 'styled-components';
 import spacing from 'styles/spacing';
 import { fontWeights, fontSizes } from 'styles/font';
-import palette from 'styles/palette';
-
-export const AddTaskInputWrapper = styled.div`
-  position: relative;
-  font-size: ${fontSizes.smallPlus};
-
-  & > input {
-    height: auto;
-    margin-bottom: ${spacing.small};
-    padding: ${spacing.regular} ${spacing.huge};
-    border-color: ${palette.coolGrey3};
-    font-size: 1em;
-
-    &:focus {
-      box-shadow: none;
-    }
-  }
-
-  &:before {
-    position: absolute;
-    top: 50%;
-    left: 20px;
-    display: block;
-    content: '+';
-    transform: translateY(-50%);
-    color: ${palette.orange};
-    font-size: 1em;
-  }
-`;
 
 export const Arrow = styled.img`
   transform: ${props => props.isOpen && 'rotateX(180deg)'};
