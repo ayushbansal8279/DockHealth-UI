ec9888e43078ed49f434f8bc49d2aed8d65d331f
Maciej Laufer <maciek.laufer@gmail.com>
2020-05-14 12:14:34 +0200

    Styling fixes

--- src/app/views/Task/NewTasksView/TasksViewLoader/TasksViewLoader.js
+++ src/app/views/Task/NewTasksView/TasksViewLoader/TasksViewLoader.js
@@ -2,18 +2,15 @@ import React from 'react';
 import CubesLoader from 'components/common/CubesLoader';
 import { FadeContainer, StyledFade } from './styled';
 
-const TasksViewLoader = ({ children, isFetchingData }) => (
-  <>
-    {isFetchingData ? (
-      <FadeContainer>
-        <StyledFade in unmountOnExit>
-          <CubesLoader size={40} />
-        </StyledFade>
-      </FadeContainer>
-    ) : (
-      children
-    )}
-  </>
-);
+const TasksViewLoader = ({ children, isFetchingData }) =>
+  isFetchingData ? (
+    <FadeContainer>
+      <StyledFade in unmountOnExit>
+        <CubesLoader size={40} />
+      </StyledFade>
+    </FadeContainer>
+  ) : (
+    children
+  );
 
 export default TasksViewLoader;
