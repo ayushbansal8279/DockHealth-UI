f6c84accadfe8adf3897c4eb5a801452bcb9dfb2
Maciej Laufer <maciek.laufer@gmail.com>
2020-05-14 11:28:47 +0200

    PR comments fixes

--- src/app/views/Task/NewTasksView/TasksViewLoader/styled.js
+++ src/app/views/Task/NewTasksView/TasksViewLoader/styled.js
@@ -1,9 +1,17 @@
+import React from 'react';
 import styled from 'styled-components';
+import { Fade } from '@material-ui/core';
 
 export const FadeContainer = styled.div`
   display: flex;
   justify-content: center;
   padding-top: 100px;
+`;
+
+export const StyledFade = styled(({ children, ...props }) => (
+  <Fade {...props}>{children}</Fade>
+))`
+  transition-delay: 800ms;
 `;
 
 export default FadeContainer;
