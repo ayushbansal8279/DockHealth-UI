8051c747df66f4c99b2ab271d5a40f1fb1bfdf92
Maciej Laufer <maciek.laufer@gmail.com>
2020-05-19 15:01:18 +0200

    Remove unnecessary props

--- src/app/views/Task/NewTasksView/CompletedTasksView.jsx
+++ src/app/views/Task/NewTasksView/CompletedTasksView.jsx
@@ -9,34 +9,19 @@ const CompletedTasksView = ({
   currentUser,
   markComplete,
   storeAsCurrentTask,
-  // groupedTasks,
-  // groupList,
   toggleSingleTaskPriority,
-  // editGroupName,
-  // quickAddTask,
-  // openDeleteConfirmationModal,
-  // changeGroupsOrder,
 }) => {
   return (
     <>
       {tasks.length > 0 ? (
         <TaskGroupsContainer>
           <TasksGroup
-            // isDefaultGroup={groupType === TASKGROUP_DEFAULT_TYPE}
-            // groupId={taskGroupIdentifier}
             groupName="Completed"
             currentUser={currentUser}
             markComplete={markComplete}
             openDrawer={openDrawer}
             storeAsCurrentTask={storeAsCurrentTask}
             toggleTaskPriority={toggleSingleTaskPriority}
-            // editGroupName={editGroupName}
-            // quickAddTask={quickAddTask}
-            // deleteGroup={openDeleteConfirmationModal}
-            // moveGroupUp={() => changeGroupsOrder(i, i - 1)}
-            // moveGroupDown={() => changeGroupsOrder(i, i + 1)}
-            // isFirstGroup={i === 0}
-            // isLastGroup={i === groupList?.length - 1}
             tasks={tasks}
             isCompletedGroup
           />
