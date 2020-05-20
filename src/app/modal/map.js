25e86a1eb74189b5a6b1c3d0afeb89bd02e667a6
Maciej Laufer <maciek.laufer@gmail.com>
2020-05-20 14:40:24 +0200

    Add confirmation modal

--- src/app/modal/map.js
+++ src/app/modal/map.js
@@ -1,7 +1,9 @@
 import DeleteGroupModal from './components/DeleteGroupModal/DeleteGroupModal';
+import CompleteAllTasksModal from './components/CompleteAllTasksModal/CompleteAllTasksModal';
 
 const MODAL_MAP = {
   DeleteGroup: DeleteGroupModal,
+  CompleteAllTasks: CompleteAllTasksModal,
 };
 
 export default MODAL_MAP;
