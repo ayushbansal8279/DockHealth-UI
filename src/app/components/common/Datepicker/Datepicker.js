0f62f0070c3c1d557c0a6e7cf08e0a5019b744c5
Maciej Laufer <maciek.laufer@gmail.com>
2020-05-26 11:21:19 +0200

    Move calendar to common components

---
+++ src/app/components/common/Datepicker/Datepicker.js
@@ -0,0 +1,92 @@
+import React, { useState, useMemo, useCallback } from 'react';
+import { useMount, useUpdate } from 'react-use';
+import moment from 'moment';
+import { Grid, IconButton } from '@material-ui/core';
+import { KeyboardArrowLeft, KeyboardArrowRight } from '@material-ui/icons';
+import { MontserratTypography } from 'styles/theme-montserrat';
+import Spacing from 'components/common/Spacing';
+import {
+  CalendarContainer,
+  CalendarGridContainer,
+  CurrentMonthLabel,
+} from './styled';
+
+import {
+  getCalendarFormattedMonth,
+  renderDayOfWeekHeaderLabel,
+  DAYS_OF_WEEK,
+  renderDayLabels,
+} from './helpers';
+
+const Datepicker = ({ selectedDate, onDateChange }) => {
+  const [currentMonthMoment, setCurrentMonthMoment] = useState(null);
+
+  const forceUpdate = useUpdate();
+
+  const momentSelectedDate = selectedDate ? moment(selectedDate) : null;
+
+  useMount(() => {
+    setCurrentMonthMoment((momentSelectedDate || moment()).startOf('month'));
+  });
+
+  const formattedCurrentMonth = getCalendarFormattedMonth(currentMonthMoment);
+
+  const dayLabelsToRender = useMemo(
+    () =>
+      renderDayLabels({ currentMonthMoment, momentSelectedDate, onDateChange }),
+    // useEffect work better for primitive values (like strings in here)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [formattedCurrentMonth],
+  );
+
+  const changeMonth = useCallback(
+    value => {
+      setCurrentMonthMoment(
+        currentMonthMoment?.add(value, 'month')?.startOf('month'),
+      );
+
+      // this update is used to due current month label not rerendering on month change
+      forceUpdate();
+    },
+    [currentMonthMoment, forceUpdate],
+  );
+  return (
+    <>
+      <CalendarContainer>
+        <Grid container justify="space-between" alignItems="center">
+          <IconButton
+            size="small"
+            color="inherit"
+            onClick={() => changeMonth(-1)}
+          >
+            <KeyboardArrowLeft />
+          </IconButton>
+          <CurrentMonthLabel>
+            <MontserratTypography
+              variant="h4"
+              color="inherit"
+              weight="500"
+              noWrap
+            >
+              {formattedCurrentMonth}
+            </MontserratTypography>
+          </CurrentMonthLabel>
+          <IconButton
+            size="small"
+            color="inherit"
+            onClick={() => changeMonth(1)}
+          >
+            <KeyboardArrowRight />
+          </IconButton>
+        </Grid>
+        <Spacing vertical={5} />
+        <CalendarGridContainer>
+          {DAYS_OF_WEEK.map(renderDayOfWeekHeaderLabel)}
+          {dayLabelsToRender}
+        </CalendarGridContainer>
+      </CalendarContainer>
+    </>
+  );
+};
+
+export default Datepicker;
--- src/app/views/Task/NewTasksView/TaskItem/PopoverDatepicker/PopoverDatepicker.js
+++ src/app/views/Task/NewTasksView/TaskItem/PopoverDatepicker/PopoverDatepicker.js
@@ -1,65 +1,22 @@
-import React, { useState, useRef, useMemo, useCallback } from 'react';
-import moment from 'moment';
-import { useMount, useUpdate } from 'react-use';
-import { Grid, IconButton, Popover } from '@material-ui/core';
-import { KeyboardArrowLeft, KeyboardArrowRight } from '@material-ui/icons';
-import { MontserratTypography } from 'styles/theme-montserrat';
-import Spacing from 'components/common/Spacing';
+import React, { useState, useRef } from 'react';
+import { Popover } from '@material-ui/core';
 
-import {
-  CalendarContainer,
-  CalendarGridContainer,
-  CurrentMonthLabel,
-} from './styled';
+import Datepicker from 'components/common/Datepicker/Datepicker';
 
-import {
-  getCalendarFormattedMonth,
-  renderDayOfWeekHeaderLabel,
-  DAYS_OF_WEEK,
-  renderDayLabels,
-} from './helpers';
-
-const PopoverDatepicker = ({ currentDueDate, setDate, children }) => {
+const PopoverDatepicker = ({ selectedDate, children }) => {
   const buttonReference = useRef(null);
-  const [isOpen, openPopover] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-  const [currentMonthMoment, setCurrentMonthMoment] = useState(null);
-
-  const selectedTaskDueDate = currentDueDate || moment();
-
-  const forceUpdate = useUpdate();
-
-  useMount(() => {
-    setCurrentMonthMoment(moment(selectedTaskDueDate).startOf('month'));
-    // setCurrentDueDate(dateFieldValue ? moment(dateFieldValue) : null);
-  });
-
-  const formattedCurrentMonth = getCalendarFormattedMonth(currentMonthMoment);
-
-  const dayLabelsToRender = useMemo(
-    () => renderDayLabels({ currentMonthMoment, currentDueDate, setDate }),
-    // useEffect work better for primitive values (like strings in here)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [formattedCurrentMonth],
-  );
-
-  const changeMonth = useCallback(
-    value => {
-      setCurrentMonthMoment(
-        currentMonthMoment?.add(value, 'month')?.startOf('month'),
-      );
-
-      // this update is used to due current month label not rerendering on month change
-      forceUpdate();
-    },
-    [currentMonthMoment, forceUpdate],
-  );
+  const handleDatePick = pickedDate => {
+    setIsOpen(false);
+    console.log('date change', pickedDate);
+  };
 
   return (
     <>
       <button
         type="button"
-        onClick={() => openPopover(!isOpen)}
+        onClick={() => setIsOpen(!isOpen)}
         ref={buttonReference}
       >
         {children}
@@ -75,42 +32,9 @@ const PopoverDatepicker = ({ currentDueDate, setDate, children }) => {
           horizontal: 'right',
         }}
         open={isOpen}
-        onClose={() => openPopover(false)}
+        onClose={() => setIsOpen(false)}
       >
-        {/* currentMonthMoment && */}
-        <CalendarContainer>
-          <Grid container justify="space-between" alignItems="center">
-            <IconButton
-              size="small"
-              color="inherit"
-              onClick={() => changeMonth(-1)}
-            >
-              <KeyboardArrowLeft />
-            </IconButton>
-            <CurrentMonthLabel>
-              <MontserratTypography
-                variant="h4"
-                color="inherit"
-                weight="500"
-                noWrap
-              >
-                {formattedCurrentMonth}
-              </MontserratTypography>
-            </CurrentMonthLabel>
-            <IconButton
-              size="small"
-              color="inherit"
-              onClick={() => changeMonth(1)}
-            >
-              <KeyboardArrowRight />
-            </IconButton>
-          </Grid>
-          <Spacing vertical={5} />
-          <CalendarGridContainer>
-            {DAYS_OF_WEEK.map(renderDayOfWeekHeaderLabel)}
-            {dayLabelsToRender}
-          </CalendarGridContainer>
-        </CalendarContainer>
+        <Datepicker selectedDate={selectedDate} onDateChange={handleDatePick} />
       </Popover>
     </>
   );
