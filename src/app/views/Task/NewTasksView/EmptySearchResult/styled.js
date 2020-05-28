850e4080edca8c5e74716bbf002546d542917eed
Maciej Laufer <maciek.laufer@gmail.com>
2020-05-28 11:04:24 +0200

    Add empty result component for search

---
+++ src/app/views/Task/NewTasksView/EmptySearchResult/styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import spacing from 'styles/spacing';
+
+export const EmptySearchResultContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const EmptySearchResultImage = styled.img`
+  display: block;
+  padding: ${spacing.huge} 0;
+`;
