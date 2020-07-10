655995abeea42da34ce8e64ee9e1253f9c3c824b
Maciej Laufer <maciek.laufer@gmail.com>
2020-07-10 13:15:41 +0200

    Add click away listener

--- src/app/components/taskView/newTaskDrawer/TaskDrawerTourContent/TaskDrawerTourContent.js
+++ src/app/components/taskView/newTaskDrawer/TaskDrawerTourContent/TaskDrawerTourContent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ClickAwayListener } from '@material-ui/core';
 import Spacing from 'components/common/Spacing';
 import { TASK_DRAWER_TOUR_STEPS } from './task-drawer-tour-steps';
 import {
@@ -16,35 +17,39 @@ import {
 
 const TaskDrawerTourContent = ({ setStep, stepIndex, onClose }) => {
   return (
-    <TourContent>
-      <Title>{TASK_DRAWER_TOUR_STEPS[stepIndex].title}</Title>
-      <Description>{TASK_DRAWER_TOUR_STEPS[stepIndex].description}</Description>
-      {TASK_DRAWER_TOUR_STEPS[stepIndex].description2 && (
-        <>
-          <Spacing vertical={4} />
-          <Description>
-            {TASK_DRAWER_TOUR_STEPS[stepIndex].description2}
-          </Description>
-        </>
-      )}
-      <NavigationContainer>
-        <NavigationDotsContainer>
-          {TASK_DRAWER_TOUR_STEPS.map(({ key }, index) => (
-            <DotNavigationButton key={key} onClick={() => setStep(index)}>
-              <Dot isNext={index > stepIndex} />
-            </DotNavigationButton>
-          ))}
-        </NavigationDotsContainer>
-        {stepIndex < TASK_DRAWER_TOUR_STEPS.length - 1 ? (
-          <TourButton onClick={() => setStep(stepIndex + 1)}>Next</TourButton>
-        ) : (
-          <TourButton onClick={onClose}>Got it</TourButton>
+    <ClickAwayListener onClickAway={onClose}>
+      <TourContent>
+        <Title>{TASK_DRAWER_TOUR_STEPS[stepIndex].title}</Title>
+        <Description>
+          {TASK_DRAWER_TOUR_STEPS[stepIndex].description}
+        </Description>
+        {TASK_DRAWER_TOUR_STEPS[stepIndex].description2 && (
+          <>
+            <Spacing vertical={4} />
+            <Description>
+              {TASK_DRAWER_TOUR_STEPS[stepIndex].description2}
+            </Description>
+          </>
         )}
-      </NavigationContainer>
-      <CloseIconButton onClick={onClose} size="small">
-        <CloseIcon />
-      </CloseIconButton>
-    </TourContent>
+        <NavigationContainer>
+          <NavigationDotsContainer>
+            {TASK_DRAWER_TOUR_STEPS.map(({ key }, index) => (
+              <DotNavigationButton key={key} onClick={() => setStep(index)}>
+                <Dot isNext={index > stepIndex} />
+              </DotNavigationButton>
+            ))}
+          </NavigationDotsContainer>
+          {stepIndex < TASK_DRAWER_TOUR_STEPS.length - 1 ? (
+            <TourButton onClick={() => setStep(stepIndex + 1)}>Next</TourButton>
+          ) : (
+            <TourButton onClick={onClose}>Got it</TourButton>
+          )}
+        </NavigationContainer>
+        <CloseIconButton onClick={onClose} size="small">
+          <CloseIcon />
+        </CloseIconButton>
+      </TourContent>
+    </ClickAwayListener>
   );
 };
 
