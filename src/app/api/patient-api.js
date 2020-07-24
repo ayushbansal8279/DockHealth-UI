43327e76da7cbf9acfd6a1bf92b7fa8644fe0cdf
Nitin Gujral <nitin.gujral@childrens.harvard.edu>
2020-07-24 19:05:00 -0400

    fix patient update and show message when it fails

--- src/app/api/patient-api.js
+++ src/app/api/patient-api.js
@@ -1,6 +1,6 @@
 import { fixList } from 'helpers/inbox-fix';
-import axios from './axios-heydoc';
 import { showAlert } from 'helpers/utility-functions';
+import axios from './axios-heydoc';
 
 export function getAllPatients() {
   return axios
@@ -70,13 +70,17 @@ export function addPatient(patient) {
     })
     .catch(error => {
       console.log(error);
-      showAlert({status: 'error', title: 'Error', text: 'Error in adding patient. Please try again.'});
+      showAlert({
+        status: 'error',
+        title: 'Error',
+        text: 'Error in adding patient. Please try again.',
+      });
       throw new Error(error?.response?.data);
     });
 }
 
 export function updatePatient(patient) {
-  if(patient.dob === ""){
+  if (patient.dob === '') {
     patient.dob = null;
   }
   return axios
@@ -86,7 +90,11 @@ export function updatePatient(patient) {
     })
     .catch(error => {
       console.log(error);
-      showAlert({status: 'error', title: 'Error', text: 'Error in updating patient. Please try again.'});
+      showAlert({
+        status: 'error',
+        title: 'Error',
+        text: 'Error in updating patient. Please try again.',
+      });
       throw new Error(error?.response?.data);
     });
 }
@@ -153,7 +161,11 @@ export const updatePatientNote = note =>
     .put('/patient/note', note)
     .then(response => response.data)
     .catch(error => {
-      showAlert({status: 'error', title: 'Error', text: 'Error in updating patient note. Please try again.'});
+      showAlert({
+        status: 'error',
+        title: 'Error',
+        text: 'Error in updating patient note. Please try again.',
+      });
       throw new Error(error?.response?.data);
     });
 
@@ -162,7 +174,11 @@ export const deletePatientNote = patientNoteIdentifier =>
     .delete(`/patient/note/deletePatientNoteById/${patientNoteIdentifier}`)
     .then(response => response.data)
     .catch(error => {
-      showAlert({status: 'error', title: 'Error', text: 'Error in deleting patient note. Please try again.'});
+      showAlert({
+        status: 'error',
+        title: 'Error',
+        text: 'Error in deleting patient note. Please try again.',
+      });
       throw new Error(error?.response?.data);
     });
 
@@ -171,6 +187,10 @@ export const archivePatient = patientNoteIdentifier =>
     .delete(`/patient/archivePatient/${patientNoteIdentifier}`)
     .then(response => response.data)
     .catch(error => {
-      showAlert({status: 'error', title: 'Error', text: 'Error in archiving patient. Please try again.'});
+      showAlert({
+        status: 'error',
+        title: 'Error',
+        text: 'Error in archiving patient. Please try again.',
+      });
       throw new Error(error?.response?.data);
     });
