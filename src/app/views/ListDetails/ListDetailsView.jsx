4ee5991bb6248d82baedfc0c2b03e73322a28822
Nitin Gujral <nitin.gujral@childrens.harvard.edu>
2020-07-28 14:19:46 -0400

    hot fix for realtime updates switch the task on the other user

--- src/app/views/ListDetails/ListDetailsView.jsx
+++ src/app/views/ListDetails/ListDetailsView.jsx
@@ -207,7 +207,7 @@ class Home extends Component {
           );
         }
         // eslint-disable-next-line no-unused-expressions
-        actions.refreshTask(data.task);
+        actions.refreshAnotherTask(data.task);
       }
     });
   };
