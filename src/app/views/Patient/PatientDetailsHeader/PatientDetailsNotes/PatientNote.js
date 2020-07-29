b1a8aa168766858c6ad4cc9f1362e20017c19de8
dwojcik96 <dwojcik@classpass.com>
2020-07-29 11:40:44 +0200

    Fix note wrapping, replace input by textarea

--- src/app/views/Patient/PatientDetailsHeader/PatientDetailsNotes/PatientNote.js
+++ src/app/views/Patient/PatientDetailsHeader/PatientDetailsNotes/PatientNote.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import moment from 'moment';
 import {
   PatientNote as StyledPatientNote,
   PatientNoteAuthor,
   PatientNoteDescription,
+  PatientNoteTextarea,
   PatientNoteOption,
   PatientNoteOptions,
   PatientNoteInformation,
@@ -21,6 +22,12 @@ const PatientNote = ({
   deletePatientNote,
 }) => {
   const [updatedDescription, setUpdatedDescription] = useState(description);
+  const [textareaHeight, setTextareaHeight] = useState(0);
+  const descritpionElement = useRef(null);
+
+  useEffect(() => {
+    setTextareaHeight(descritpionElement?.current?.offsetHeight);
+  }, [descritpionElement, description, updatedDescription]);
 
   useEffect(() => {
     setUpdatedDescription(description);
@@ -30,21 +37,28 @@ const PatientNote = ({
   return (
     <StyledPatientNote isEditable={isEditable}>
       <PatientNoteInformation>
-        <PatientNoteDescription
-          onChange={event => setUpdatedDescription(event.target.value)}
-          onKeyDown={async event => {
-            if (event.keyCode === 13 && updatedDescription?.length > 0) {
-              await editPatientNote({
-                note: updatedDescription,
-                patientNoteIdentifier,
-              });
-              setEditableNote(null);
-            }
-          }}
-          onBlur={() => setEditableNote(null)}
-          value={isEditable ? updatedDescription : description}
-          disabled={!isEditable}
-        />
+        {!isEditable && (
+          <PatientNoteDescription ref={descritpionElement}>
+            {description}
+          </PatientNoteDescription>
+        )}
+        {isEditable && (
+          <PatientNoteTextarea
+            onChange={event => setUpdatedDescription(event.target.value)}
+            onKeyDown={async event => {
+              if (event.keyCode === 13 && updatedDescription?.length > 0) {
+                await editPatientNote({
+                  note: updatedDescription,
+                  patientNoteIdentifier,
+                });
+                setEditableNote(null);
+              }
+            }}
+            onBlur={() => setEditableNote(null)}
+            value={isEditable ? updatedDescription : description}
+            textareaHeight={textareaHeight}
+          />
+        )}
         <PatientNoteAuthor>
           {firstName} {lastName} {moment(dateUpdated).format('h:mma M/DD/YY')}
         </PatientNoteAuthor>
