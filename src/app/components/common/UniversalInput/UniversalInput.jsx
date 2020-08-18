51359ac7ba1b874e534327c4e366f3e673714116
Maciej Laufer <maciek.laufer@gmail.com>
2020-08-18 16:03:11 +0200

    Reorganize profile settings input section

--- src/app/components/common/UniversalInput/UniversalInput.jsx
+++ src/app/components/common/UniversalInput/UniversalInput.jsx
@@ -220,7 +220,7 @@ export const UniversalInput = ({
           placeholder={placeholder}
           inputRef={register}
           inputProps={{
-            // value,
+            value,
             setValue,
             name,
           }}
--- src/app/views/UserProfile/UserProfileForm/form-definitions.jsx
+++ src/app/views/UserProfile/UserProfileForm/form-definitions.jsx
@@ -1,36 +1,38 @@
 import { head } from 'ramda';
-import React from 'react';
-import styled from 'styled-components';
-
-const PrePasswordLabel = styled.div`
-  margin-bottom: 1rem;
-  text-align: left;
-  width: 80%;
-`;
 
 export const formFieldDefinitions = [
+  {
+    key: 'firstName',
+    label: 'First Name',
+    required: true,
+  },
+  {
+    key: 'lastName',
+    label: 'Last Name',
+    required: true,
+  },
   {
     key: 'title',
     label: 'Title',
     required: true,
     defaultValueGetter: ({ titles }) => head(titles || [])?.name,
   },
-  {
-    key: 'specialty',
-    label: 'My Specialty',
-    defaultValueGetter: ({ specialties }) => head(specialties || [])?.name,
-  },
-  {
-    key: 'subspecialty',
-    label: 'My Subspecialty',
-    defaultValueGetter: ({ specialties }) =>
-      head(head(specialties || [])?.subSpecialties || [])?.subSpecialtyName,
-  },
-  {
-    key: 'organizationName',
-    label: 'Organization',
-    readOnly: true,
-  },
+  // {
+  //   key: 'specialty',
+  //   label: 'My Specialty',
+  //   defaultValueGetter: ({ specialties }) => head(specialties || [])?.name,
+  // },
+  // {
+  //   key: 'subspecialty',
+  //   label: 'My Subspecialty',
+  //   defaultValueGetter: ({ specialties }) =>
+  //     head(head(specialties || [])?.subSpecialties || [])?.subSpecialtyName,
+  // },
+  // {
+  //   key: 'organizationName',
+  //   label: 'Organization',
+  //   readOnly: true,
+  // },
   {
     key: 'department',
     label: 'Department',
@@ -46,12 +48,6 @@ export const formFieldDefinitions = [
     label: 'Your Mobile Phone Number',
     isPhoneNumber: true,
     readOnly: true,
-    PreFieldComponent: () => (
-      <PrePasswordLabel>
-        A valid mobile phone number is required to send an authentication code
-        for HIPAA compliance
-      </PrePasswordLabel>
-    ),
     defaultValueGetter: ({ accountPhoneNumber }) =>
       accountPhoneNumber
         ? accountPhoneNumber.replace(/^\+1/, '')
