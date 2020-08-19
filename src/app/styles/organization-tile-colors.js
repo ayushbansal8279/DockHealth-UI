e9694e5c9ea6e4bd85017e0c1b92eb9dceb3f8ff
Maciej Laufer <maciek.laufer@gmail.com>
2020-08-19 14:53:30 +0200

    Code review code fixes

---
+++ src/app/styles/organization-tile-colors.js
@@ -0,0 +1,32 @@
+export const ORGANIZATION_TILE_COLORS = [
+  {
+    uniqueName: 'yellow',
+    hex: '#fdcd38',
+  },
+  {
+    uniqueName: 'orange',
+    hex: '#ee8b31',
+  },
+  {
+    uniqueName: 'lightBlue',
+    hex: '#1ba5e3',
+  },
+  {
+    uniqueName: 'blue',
+    hex: '#094a85',
+  },
+  {
+    uniqueName: 'purple',
+    hex: '#553ebd',
+  },
+  {
+    uniqueName: 'red',
+    hex: '#e7473d',
+  },
+  {
+    uniqueName: 'green',
+    hex: '#bcd44c',
+  },
+];
+
+export default ORGANIZATION_TILE_COLORS;
--- src/app/views/UserProfile/styled.js
+++ src/app/views/UserProfile/styled.js
@@ -12,7 +12,7 @@ export const ProfileSettingsWrapper = styled.div`
   max-width: 798px;
   width: 100%;
   margin: 0 auto;
-  padding: 140px 20px;
+  padding: 140px ${spacing.regularPlus};
   color: ${palette.mediumGrey};
   font-family: 'Montserrat', sans-serif;
 `;
