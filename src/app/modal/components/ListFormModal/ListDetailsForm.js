447163237ed1d707d22244f7ac20ba32a972fba9
Maciej Laufer <maciek.laufer@gmail.com>
2020-08-21 09:31:56 +0200

    Change create list modal

--- src/app/modal/components/ListFormModal/ListDetailsForm.js
+++ src/app/modal/components/ListFormModal/ListDetailsForm.js
@@ -1,37 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
 import { showAlert } from 'helpers/utility-functions';
 import * as TaskListActions from 'actions/tasklist-actions';
 import { Grid } from '@material-ui/core';
 import Input from 'components/common/Input/Input';
 import Spacing from 'components/common/Spacing';
 import messages from 'components/ListForm/messages';
-import initializeListFormHooks from 'components/ListForm/hooks';
 import Button from 'components/common/Button/Button';
-import { Title, ButtonsWrapper, FormWrapper, Header } from './styled';
+import { Title, ButtonWrapper, FormWrapper, Header } from './styled';
 
 const onSubmit = ({
   event,
   dispatch,
   nextStep,
   closeModal,
-  isSavingList,
   setIsSavingList,
   onListCreationSuccess,
+  setList,
+  taskListIdentifier,
 }) => data => {
-  const taskList = { ...data };
-  event.preventDefault();
   event.stopPropagation();
-
-  if (isSavingList) return;
+  event.preventDefault();
 
   setIsSavingList(true);
-  dispatch(TaskListActions.saveTaskList(taskList))
+  dispatch(TaskListActions.saveTaskList({ ...data, taskListIdentifier }))
     .then(updatedList => {
       if (updatedList) {
+        setList(updatedList);
         nextStep();
-        dispatch(TaskListActions.setTaskListAsCurrentList(updatedList));
-        setIsSavingList(false);
-        onListCreationSuccess();
+
+        if (typeof onListCreationSuccess === 'function')
+          onListCreationSuccess();
       }
     })
     .catch(error => {
@@ -45,30 +45,66 @@ const onSubmit = ({
     });
 };
 
-const ListDetailsForm = ({ closeModal, onListCreationSuccess, nextStep }) => {
+const ListDetailsForm = ({
+  closeModal,
+  onListCreationSuccess,
+  nextStep,
+  list,
+  setList,
+}) => {
+  const dispatch = useDispatch();
   const [isSavingList, setIsSavingList] = useState(false);
-  const {
-    register,
-    errors,
-    dispatch,
-    handleSubmit,
-    listNameValue,
-    listDescriptionValue,
-    taskListIdentifier,
-  } = initializeListFormHooks();
+
+  const formContext = useForm({
+    defaultValues: {
+      listName: list?.listName,
+      listDescription: list?.listDescription,
+    },
+    reValidateMode: 'onSubmit',
+  });
+
+  const { register, errors, unregister, handleSubmit, watch } = formContext;
+
+  const listNameValue = watch('listName');
+  const listDescriptionValue = watch('listDescription');
+
+  useEffect(() => {
+    register(
+      {
+        name: 'listName',
+      },
+      {
+        validate: value => {
+          if (![...value]?.filter(char => char !== ' ').length > 0) {
+            return 'This field is required';
+          }
+
+          return true;
+        },
+      },
+    );
+
+    register({ name: 'listDescription' });
+    return () => {
+      unregister('listName');
+      unregister('listDescription');
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <FormWrapper
       onSubmit={event =>
         handleSubmit(
           onSubmit({
-            dispatch,
             event,
+            dispatch,
             nextStep,
             closeModal,
-            isSavingList,
             setIsSavingList,
             onListCreationSuccess,
+            setList,
+            taskListIdentifier: list?.taskListIdentifier,
           }),
         )(event)
       }
@@ -76,7 +112,7 @@ const ListDetailsForm = ({ closeModal, onListCreationSuccess, nextStep }) => {
       <Grid container direction="column" justify="space-between">
         <Grid item>
           <Header>
-            <Title>Create a list</Title>
+            <Title>{list ? 'Edit a list' : 'Create a list'}</Title>
           </Header>
           <Input
             ref={register}
@@ -103,16 +139,28 @@ const ListDetailsForm = ({ closeModal, onListCreationSuccess, nextStep }) => {
           />
           <Spacing vertical={4} />
         </Grid>
-        <Grid>
-          <ButtonsWrapper>
-            <Button variant="outlined" onClick={closeModal}>
+        <Grid container direction="row" justify="center">
+          <ButtonWrapper>
+            <Button
+              fullWidth
+              variant="outlined"
+              onClick={closeModal}
+              size="small"
+            >
               Cancel
             </Button>
-            <Spacing horizontal={4} />
-            <Button type="submit">
-              {taskListIdentifier ? 'Update' : 'Create'}
+          </ButtonWrapper>
+          <Spacing horizontal={3} />
+          <ButtonWrapper>
+            <Button
+              fullWidth
+              type="submit"
+              size="small"
+              disabled={isSavingList}
+            >
+              Next
             </Button>
-          </ButtonsWrapper>
+          </ButtonWrapper>
         </Grid>
       </Grid>
     </FormWrapper>
