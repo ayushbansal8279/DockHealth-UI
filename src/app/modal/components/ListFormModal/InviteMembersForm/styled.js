5b1f66959bee0d7023837dbbce2b76ea628f12bf
Maciej Laufer <maciek.laufer@gmail.com>
2020-08-25 13:16:29 +0200

    Add context menu with change role action

--- src/app/modal/components/ListFormModal/InviteMembersForm/styled.js
+++ src/app/modal/components/ListFormModal/InviteMembersForm/styled.js
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 import palette from 'styles/palette';
 import { fontSizes, fontWeights } from 'styles/font';
 import spacing from 'styles/spacing';
+import { Popover } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 
 export const InviteInitialViewWrapper = styled.div`
   display: flex;
@@ -108,3 +110,53 @@ export const MemberStatusLabel = styled.p`
   font-size: ${fontSizes.regular};
   font-weight: ${fontWeights.regular};
 `;
+
+export const MemberMenuWrapper = styled.div`
+  width: 210px;
+  background: ${palette.white};
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+export const MemberMenuButton = styled.button`
+  width: 100%;
+  padding: ${spacing.smallPlus};
+  text-align: left;
+
+  &:not(:last-child) {
+    border-bottom: 1px solid ${palette.coolGrey3};
+  }
+
+  &:hover {
+    cursor: pointer;
+    background: ${palette.coolGrey4};
+  }
+`;
+
+export const MemberMenuButtonTitle = styled.p`
+  margin-bottom: 0;
+  font-family: 'Roboto', sans-serif;
+  font-size: ${fontSizes.regular};
+  font-weight: ${fontWeights.bold};
+  color: ${palette.mediumGrey};
+`;
+
+export const MemberMenuButtonDescription = styled.p`
+  margin-bottom: 0;
+  font-family: 'Roboto Condensed', sans-serif;
+  font-size: ${fontSizes.smallPlus};
+  font-weight: ${fontWeights.regular};
+  color: ${palette.coolGrey1};
+`;
+
+export const MenuPopover = withStyles({
+  root: {
+    zIndex: '5006 !important',
+  },
+  paper: {
+    border: 'none',
+    overflow: 'visible',
+  },
+  backdrop: {
+    zIndex: '5005 !important',
+  },
+})(Popover);
