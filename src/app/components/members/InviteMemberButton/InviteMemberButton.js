ca5d562462122b75a48c3d1da77af83637ce610d
Maciej Laufer <maciek.laufer@gmail.com>
2020-08-28 09:07:54 +0200

    Remove unnecessary html tags

--- src/app/components/members/InviteMemberButton/InviteMemberButton.js
+++ src/app/components/members/InviteMemberButton/InviteMemberButton.js
@@ -3,13 +3,9 @@ import { StyledIconButton } from './styled';
 
 const InviteMemberButton = ({ size = 54, onClick }) => {
   return (
-    <>
-      <div>
-        <StyledIconButton size={size} onClick={onClick}>
-          +
-        </StyledIconButton>
-      </div>
-    </>
+    <StyledIconButton size={size} onClick={onClick}>
+      +
+    </StyledIconButton>
   );
 };
 
