e59cc5748496be3d43f1f0dfe86ef330ce717a04
Maciej Laufer <maciek.laufer@gmail.com>
2020-09-03 13:45:27 +0200

    Add change mobile phone confirmation modal

--- src/app/modal/components/ChangeMobileNumberModal/ConfirmNumberStep.js
+++ src/app/modal/components/ChangeMobileNumberModal/ConfirmNumberStep.js
@@ -1,7 +1,10 @@
 import React, { useMemo } from 'react';
 import { Grid } from '@material-ui/core';
+import MobilePhoneIcon from 'img/modals/mobile-phone';
 import { FormContext, useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
 import { object, string } from 'yup';
+import { openModal } from 'modal/actions';
 import * as UserApi from 'api/user-api';
 import { UniversalInput } from 'components/common/UniversalInput/UniversalInput';
 import Spacing from 'components/common/Spacing';
@@ -28,13 +31,19 @@ const validationSchema = object({
     .required(REQUIRED_MESSAGE),
 });
 
-const onSubmit = ({ closeModal, onUpdateSuccess, setError }) => ({
+const onSubmit = ({ onUpdateSuccess, setError, dispatch }) => ({
   authorizationCode,
 }) => {
   UserApi.verifyNewPhoneNumber(authorizationCode)
     .then(() => {
-      closeModal();
       onUpdateSuccess();
+      dispatch(
+        openModal('Confirmation', {
+          description: 'Your mobile number has been verified and updated.',
+          icon: MobilePhoneIcon,
+          altIcon: 'Mobile phone',
+        }),
+      );
     })
     .catch(error => {
       setError(
@@ -46,11 +55,12 @@ const onSubmit = ({ closeModal, onUpdateSuccess, setError }) => ({
 };
 
 const ConfirmNumberStep = ({
-  closeModal,
   onUpdateSuccess,
   goToPreviousStep,
   newPhoneNumber = '',
 }) => {
+  const dispatch = useDispatch();
+
   const formMethods = useForm({
     validationSchema,
     reValidateMode: 'onSubmit',
@@ -71,7 +81,7 @@ const ConfirmNumberStep = ({
     <FormContext {...formMethods}>
       <StyledForm
         onSubmit={handleSubmit(
-          onSubmit({ closeModal, onUpdateSuccess, setError }),
+          onSubmit({ dispatch, onUpdateSuccess, setError }),
         )}
       >
         <GridMaxHeight container direction="column" justify="space-between">
