31d303221b5ac8ed166594054542c38634a6a29c
Dawid Wójcik <dawid.wojcik@htdevelopers.com>
2020-09-16 17:36:58 +0200

    Revert "Reassign owner to member"

--- src/app/api/people-api.js
+++ src/app/api/people-api.js
@@ -238,9 +238,3 @@ export function archiveUser(userIdentifier) {
     `user/archiveUserFromOrganization?userIdentifier=${userIdentifier}`,
   );
 }
-
-export function changeUserToOwner(userIdentifier) {
-  return axios.put(
-    `organization/reassignOwnerForOrg?assignedUserId=${userIdentifier}`,
-  );
-}
--- src/app/views/self-serve/subscriptions/SubscriptionsView.MemberTypeLabel.jsx
+++ src/app/views/self-serve/subscriptions/SubscriptionsView.MemberTypeLabel.jsx
@@ -1,9 +1,7 @@
-/* eslint-disable sonarjs/cognitive-complexity */
 import React, { useCallback, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { findAllUsers } from 'actions/people-actions';
 import Arrow from 'components/common/Arrow/Arrow';
-import { openModal } from 'modal/actions';
 import {
   MemberTypeButton,
   CurrentUserLabel,
@@ -40,16 +38,13 @@ const MemberTypeLabel = ({
   openPopover,
   closePopover,
   displayName,
-  ownersCount,
-  currentActiveUsers,
 }) => {
   const labelReference = useRef(null);
   const dispatch = useDispatch();
   const reloadUsers = useCallback(() => findAllUsers()(dispatch), [dispatch]);
-  const isCurrrentUser = userIdentifier === sessionStorage.userIdentifier;
 
   const PopoverComponent = (() => {
-    if (ownersCount < 2 && isCurrrentUser && orgUserRole === 'OWNER') {
+    if (userIdentifier === sessionStorage.userIdentifier) {
       return null;
     }
 
@@ -71,26 +66,13 @@ const MemberTypeLabel = ({
     return null;
   })();
 
-  let onClickAction = openPopover;
-
-  if (ownersCount < 2 && isCurrrentUser && orgUserRole === 'OWNER') {
-    onClickAction = () =>
-      dispatch(
-        openModal('SelectOwner', {
-          currentActiveUsers,
-        }),
-      );
-  } else if (isDisabledRemovingSubscription) {
-    onClickAction = () => {};
-  }
-
   return (
     <>
       <MemberTypeButton
         ref={labelReference}
         invited={invitationModifiable}
         clickable={!isDisabledRemovingSubscription}
-        onClick={onClickAction}
+        onClick={isDisabledRemovingSubscription ? () => {} : openPopover}
         isInvited={isInvited}
       >
         {!isDisabledRemovingSubscription && (
@@ -121,8 +103,6 @@ const MemberTypeLabel = ({
             labelReference,
             isPopoverOpen,
             displayName,
-            ownersCount,
-            currentActiveUsers,
           }}
         />
       )}
