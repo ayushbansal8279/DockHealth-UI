4f7af79ee1d3708a3d992508644856780be6b545
Nitin Gujral <nitin@dock.health>
2020-09-16 11:13:17 -0400

    updates for refer a colleague

--- src/app/components/drawer/ReferAColleagueModal.jsx
+++ src/app/components/drawer/ReferAColleagueModal.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
+import { Grid } from '@material-ui/core';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
@@ -14,6 +15,92 @@ import { object, string } from 'yup';
 import Spacing from 'components/common/Spacing';
 import { UniversalMontserratInput } from 'components/userProfileView/UniversalInput';
 import { showAlert } from 'helpers/utility-functions';
+import DockHeaderLogo from 'img/dock-header-logo.svg';
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 100%;
+  width: 100%;
+
+  @media screen and (max-width: ${mdBreakpoint}px) {
+    flex-direction: column;
+  }
+`;
+
+const LeftSideMainContainer = styled.div`
+  align-items: center;
+  background-color: ${palette.midnightBlue};
+  background-image: linear-gradient(
+      to bottom,
+      ${opacify(palette.midnightBlue, 0.5)},
+      ${opacify(palette.midnightBlue, 0.5)}
+    ),
+    url(${AuthTemplateTopBackgroundTop}),
+    url(${AuthTemplateTopBackgroundBottom});
+  background-repeat: repeat-x;
+  background-position: bottom, top;
+  display: flex-root;
+  max-width: 642px;
+  height: 100%;
+  justify-content: center;
+  min-height: 100%;
+  padding: 14rem 7rem 2rem 7rem;
+  width: 50%;
+
+  @media screen and (max-width: ${mdBreakpoint}px) {
+    height: min-content;
+    max-width: unset;
+    min-height: min-content;
+    padding: 1.5rem;
+    width: 100%;
+  }
+`;
+
+const LeftSideContentContainer = styled.div`
+  color: ${palette.white};
+  height: min-content;
+  min-height: min-content;
+  width: 100%;
+
+  @media screen and (min-width: ${mdBreakpoint}px) {
+    max-height: 721px;
+    max-width: 525px;
+  }
+`;
+
+const RightSideMainContainer = styled.div`
+  align-items: center;
+  background-color: ${palette.white};
+  display: flex;
+  flex: 1;
+  height: min-content;
+  justify-content: center;
+  min-height: min-content;
+
+  @media screen and (min-width: ${mdBreakpoint}px) {
+    height: 100%;
+    min-height: 100%;
+  }
+`;
+
+const RightSideContentContainer = styled.div`
+  align-items: flex-start;
+  display: flex;
+  height: min-content;
+  justify-content: center;
+  min-height: min-content;
+  padding: 1.5rem;
+  width: 100%;
+
+  @media screen and (min-width: ${mdBreakpoint}px) {
+    align-items: center;
+    height: 100%;
+    min-height: 100%;
+    padding: 2rem;
+    max-width: 495px;
+  }
+`;
 
 const StyledForm = styled.form`
   width: 100%;
@@ -64,53 +151,80 @@ export default function ReferAColleagueModal({ ...props }) {
     reValidateMode: 'onSubmit',
   });
 
+  const [maxWidth, setMaxWidth] = React.useState('xl');
+
   return (
     <div>
       <Dialog
         open={openReferral}
         onClose={handleReferralClose}
         aria-labelledby="form-dialog-title"
+        maxWidth={maxWidth}
       >
-        <DialogTitle id="form-dialog-title">Refer a colleague</DialogTitle>
 
-        <StyledForm
-          onSubmit={formMethods.handleSubmit(
-            onSubmit({
-              handleReferralClose,
-            }),
-          )}
-        >
-          <FormContext {...formMethods}>
-            <DialogContent>
-              <DialogContentText>
-                If you love dock health and want to refer a friend or colleague,
-                simply fill in their information below and we&apos;ll send an
-                invite to their inbox.
-              </DialogContentText>
-              <Spacing vertical={4} />
-              <MontserratTypography variant="h5">
-                <span style={{ color: palette.error }}>*</span>
-                <span> All fields required</span>
-              </MontserratTypography>
-              <Spacing vertical={3} />
-              <UniversalMontserratInput name="firstName" label="First Name" />
-              <Spacing vertical={3} />
-              <UniversalMontserratInput name="lastName" label="Last Name" />
-              <Spacing vertical={3} />
-              <UniversalMontserratInput name="email" label="Email" />
-              <Spacing vertical={3} />
-            </DialogContent>
+      <DialogContent>
+        <MainContainer>
+          <LeftSideMainContainer>
+            <LeftSideContentContainer>
 
-            <DialogActions>
-              <Button onClick={handleReferralClose} color="primary">
-                Cancel
-              </Button>
-              <Button type="submit" color="primary">
-                Send Invite
-              </Button>
-            </DialogActions>
-          </FormContext>
-        </StyledForm>
+              <StyledForm
+                onSubmit={formMethods.handleSubmit(
+                  onSubmit({
+                    handleReferralClose,
+                  }),
+                )}
+              >
+                <FormContext {...formMethods}>
+                    {/* <DialogContentText>
+                      If you love dock health and want to refer a friend or colleague,
+                      simply fill in their information below and we&apos;ll send an
+                      invite to their inbox.
+                    </DialogContentText> */}
+                    <Spacing vertical={4} />
+                    <MontserratTypography variant="h5">
+                      <span style={{ color: palette.error }}>*</span>
+                      <span> All fields required</span>
+                    </MontserratTypography>
+                    {/* <Spacing vertical={3} />
+                    <UniversalMontserratInput name="firstName" label="First Name" />
+                    <Spacing vertical={3} />
+                    <UniversalMontserratInput name="lastName" label="Last Name" />
+                    <Spacing vertical={3} />
+                    <UniversalMontserratInput name="email" label="Email" />
+                    <Spacing vertical={3} /> */}
+
+                  {/* <DialogActions>
+                    <Button onClick={handleReferralClose} color="primary">
+                      Cancel
+                    </Button>
+                    <Button type="submit" color="primary">
+                      Send Invite
+                    </Button>
+                  </DialogActions> */}
+                </FormContext>
+              </StyledForm>
+
+            </LeftSideContentContainer>
+          </LeftSideMainContainer>
+          <RightSideMainContainer>
+            <RightSideContentContainer>
+              <StyledGrid container justify="center" alignItems="left" direction="column">
+                <a href="/#/">
+                  <DockLogoImage />
+                </a>
+                <Spacing vertical={5} />
+                <MontserratTypography weight="normal" variant="h3" color="inherit">
+                  A simple, HIPAA compliant task management and collaboration platform
+                  built specifically for healthcare.
+                </MontserratTypography>
+              </StyledGrid>
+            </RightSideContentContainer>
+          </RightSideMainContainer>
+        </MainContainer>
+
+      </DialogContent>
+      {/* <DialogTitle id="form-dialog-title" style={{fontSize: '20px'}}>Refer a colleague</DialogTitle> */}
+
       </Dialog>
     </div>
   );
