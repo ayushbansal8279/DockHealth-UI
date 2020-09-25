8ad1380eba71017ee698a678350462fdddd342ca
Nitin Gujral <nitin@dock.health>
2020-09-24 20:22:16 -0400

    minor content fixes

--- src/app/modal/components/ListFormModal/InviteMembersForm/InviteMembersForm.js
+++ src/app/modal/components/ListFormModal/InviteMembersForm/InviteMembersForm.js
@@ -27,7 +27,7 @@ const InviteMembersForm = ({
     return (
       <InviteInitialViewWrapper>
         <Header>
-          <Title>Would you like to Invite people to this list</Title>
+          <Title>Would you like to Invite people to this list?</Title>
         </Header>
         <InviteInitialViewContent>
           <NavigationActionButton onClick={closeModal}>
