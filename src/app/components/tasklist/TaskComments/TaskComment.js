e6b62f62a1f3e99f0452671e3d71f6b56f61f0bc
Dawid Wójcik <dawid.wojcik@htdevelopers.com>
2020-10-16 18:09:17 +0200

    Revert "Group comments by date (#915)" (#917)
    This reverts commit 21aff422a6141bfbf9244b909423e87c9e57e614.

--- src/app/components/tasklist/TaskComments/TaskComment.js
+++ src/app/components/tasklist/TaskComments/TaskComment.js
@@ -6,7 +6,6 @@ import MentionsEditor from 'components/common/MentionsEditor/MentionsEditor';
 import { convertToEditorState } from 'components/common/MentionsEditor/helpers';
 import { useMentionsEditorState } from 'components/common/MentionsEditor/use-mentions-editor-state';
 import { createMentionEntities } from 'components/common/MentionsEditor/create-mention-entities';
-import { getTaskDateLabel } from './helpers';
 import {
   TaskCommentAvatarContainer,
   TaskCommentContainer,
@@ -47,9 +46,19 @@ const TaskComment = ({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [comment]);
 
+  let dateLabel = '';
+
+  if (moment(dateUpdated).isSame(new Date(), 'd')) {
+    dateLabel = 'Today';
+  } else if (moment(dateUpdated).isSame(moment().subtract(1, 'days'), 'd')) {
+    dateLabel = 'Yesterday';
+  } else {
+    dateLabel = moment(dateUpdated).format('MM/DD/YYYY');
+  }
+
   const commentDetails = `${creator.firstName} ${
     creator.lastName
-  }, ${getTaskDateLabel(dateUpdated)} @ ${moment(dateUpdated).format('h:mma')}`;
+  }, ${dateLabel} @ ${moment(dateUpdated).format('h:mma')}`;
 
   return (
     <TaskCommentContainer>
--- src/app/components/tasklist/TaskComments/TaskComments.js
+++ src/app/components/tasklist/TaskComments/TaskComments.js
@@ -1,12 +1,5 @@
 import React, { useState } from 'react';
-import moment from 'moment';
-import { groupBy } from 'ramda';
-import { getTaskDateLabel } from './helpers';
-import {
-  ShowMoreButton,
-  TaskCommentsContainer,
-  TaskGroupDateLabel,
-} from './styled';
+import { ShowMoreButton, TaskCommentsContainer } from './styled';
 import TaskComment from './TaskComment';
 
 const TaskComments = ({
@@ -19,26 +12,16 @@ const TaskComments = ({
     !!(comments !== undefined && comments?.length > 3),
   );
   const limitedComments = !showMore ? comments : comments?.slice(0, 3);
-  const groupedComments = groupBy(
-    ({ dateCreated }) => moment(dateCreated).format('M/DD/YYYY'),
-    limitedComments ?? [],
-  );
+
   return (
     <TaskCommentsContainer timeout={150} in={isOpen}>
-      {Object.keys(groupedComments).map(key => (
-        <div key={key}>
-          <TaskGroupDateLabel>{getTaskDateLabel(key)}</TaskGroupDateLabel>
-          <div onClick={onClickComment}>
-            {groupedComments[key]?.map(comment => (
-              <TaskComment
-                key={comment.commentId}
-                highlightedValue={highlightedValue}
-                onClickComment={onClickComment}
-                {...comment}
-              />
-            ))}
-          </div>
-        </div>
+      {limitedComments?.map((comment, index) => (
+        <TaskComment
+          {...comment}
+          key={index}
+          highlightedValue={highlightedValue}
+          onClickComment={onClickComment}
+        />
       ))}
       {showMore && (
         <ShowMoreButton onClick={() => setShowMore(false)}>
