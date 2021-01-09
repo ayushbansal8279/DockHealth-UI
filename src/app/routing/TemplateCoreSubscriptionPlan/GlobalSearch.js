52a7cd6ef85e6404bfdda2e8f8a5944892d331f3
Nitin Gujral <nitin@dock.health>
2021-01-08 20:33:05 -0500

    fixed header for Global Search and Patients views

--- src/app/routing/TemplateCoreSubscriptionPlan/GlobalSearch.js
+++ src/app/routing/TemplateCoreSubscriptionPlan/GlobalSearch.js
@@ -1,13 +1,15 @@
+import React from 'react';
 import { resetGlobalSearch } from 'actions/global-search-actions';
 import { closeDrawer } from 'actions/task-drawer-actions';
 import { setHeader } from 'actions/header-actions';
+import GenericHeader from 'components/common/GenericHeader';
 
 export const onEnterGlobalSearch = ({ dispatch }) => {
   setHeader(dispatch)({
     layout: [
       {
-        key: '',
-        component: null,
+        key: 'global-search-header',
+        component: <GenericHeader>Search</GenericHeader>,
       },
     ],
   });
