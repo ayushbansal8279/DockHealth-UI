ed22e10e65f3ac4f30cb17b39e14a7f001386d9e
Nitin Gujral <nitin@dock.health>
2021-01-23 23:03:37 -0500

    fixed setting the time to start of day

--- src/app/components/tasklist/BulkEditSection/BulkEditOptionsBar/BulkEditDueDateOption.jsx
+++ src/app/components/tasklist/BulkEditSection/BulkEditOptionsBar/BulkEditDueDateOption.jsx
@@ -4,40 +4,45 @@ import CalendarIcon from 'img/bulk-edit/CalendarIcon';
 import PopoverDatepicker from 'components/common/PopoverDatepicker/PopoverDatepicker';
 import { IconButton, IconBox } from './styled';
 
-const dueDateQuickSelectOptions = [
-  {
-    label: 'Today',
-    date: moment(),
-  },
-  {
-    label: 'Tomorrow',
-    date: moment().add(1, 'days'),
-  },
-];
+const BulkEditDueDateOption = ({ handleChangeDateTasks, isDisabled }) => {
+  const dueDateQuickSelectOptions = [
+    {
+      label: 'Today',
+      date: moment(),
+    },
+    {
+      label: 'Tomorrow',
+      date: moment().add(1, 'days'),
+    },
+  ];
 
-const BulkEditDueDateOption = ({ handleChangeDateTasks, isDisabled }) => (
-  <PopoverDatepicker
-    onDateChange={date => {
-      handleChangeDateTasks(date);
-    }}
-    quickSelectOptions={dueDateQuickSelectOptions}
-    openCalendarWithOptions
-    usePortal
-  >
-    {({ elementReference, setIsPopoverOpen, isPopoverOpen }) => (
-      <IconButton
-        type="button"
-        ref={elementReference}
-        onClick={() => setIsPopoverOpen(!isPopoverOpen)}
-        disabled={isDisabled}
-      >
-        <IconBox>
-          <CalendarIcon />
-        </IconBox>
-        <p>Date</p>
-      </IconButton>
-    )}
-  </PopoverDatepicker>
-);
+  return (
+    <PopoverDatepicker
+      onDateChange={date => {
+        const startTime = moment()
+          .startOf('day')
+          .format('HH:mm:ss.Z');
+        handleChangeDateTasks(`${date}T${startTime}`);
+      }}
+      quickSelectOptions={dueDateQuickSelectOptions}
+      openCalendarWithOptions
+      usePortal
+    >
+      {({ elementReference, setIsPopoverOpen, isPopoverOpen }) => (
+        <IconButton
+          type="button"
+          ref={elementReference}
+          onClick={() => setIsPopoverOpen(!isPopoverOpen)}
+          disabled={isDisabled}
+        >
+          <IconBox>
+            <CalendarIcon />
+          </IconBox>
+          <p>Date</p>
+        </IconButton>
+      )}
+    </PopoverDatepicker>
+  );
+};
 
 export default BulkEditDueDateOption;
