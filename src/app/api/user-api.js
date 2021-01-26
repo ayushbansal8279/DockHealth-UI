9e9d20a0d20b97d7ea395c0b59fe9e26cf0b1c26
Nitin Gujral <nitin@dock.health>
2021-01-25 22:11:14 -0500

    fixed error with calling Auth.currentAuthenticatedUser

--- src/app/api/user-api.js
+++ src/app/api/user-api.js
@@ -588,9 +588,9 @@ export function refreshAccessToken(email) {
   }
 
   return new Promise(async (resolve, reject) => {
-    const cognitoUser = await Auth.currentAuthenticatedUser(
+    const cognitoUser = await Auth.currentAuthenticatedUser({
       bypassCache: true, // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
-    );
+    });
     const currentSession = await Auth.currentSession();
     console.log('calling refreshSession');
     cognitoUser.refreshSession(
