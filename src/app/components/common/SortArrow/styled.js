0efaadd0d06921daeb5e29ae7b3a57c8ab4cc975
Dawid Wójcik <22615841+dwojcik96@users.noreply.github.com>
2021-03-04 16:24:13 +0100

    Revert "Restyle (#1122)" (#1135)
    This reverts commit 639df3ef9a1e89d0f0e7c683ff20003a0f0e315e.

--- src/app/components/common/SortArrow/styled.js
+++ src/app/components/common/SortArrow/styled.js
@@ -1,33 +1,14 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import palette from 'styles/palette';
 
 export const SortArrowContainer = styled.button`
   position: relative;
+  height: 13px;
+  width: 9px;
   cursor: pointer;
   transition: all 0.3s ease-in-out;
-
-  ${props => {
-    if (props.version === 'secondary') {
-      return css`
-        background-color: ${props.ordered ? palette.orange : 'transparent'};
-        border-radius: 50%;
-        padding: 1px;
-
-        * > path {
-          stroke: ${({ ordered }) => (ordered ? 'white' : palette.coolGrey1)};
-        }
-      `;
-    }
-
-    return css`
-      height: 13px;
-      width: 9px;
-      transform-origin: 50% 45%;
-      color: ${({ ordered }) =>
-        ordered ? palette.mediumGrey : palette.coolGrey1};
-    `;
-  }};
-
+  transform-origin: 50% 45%;
+  color: ${({ ordered }) => (ordered ? palette.mediumGrey : palette.coolGrey1)};
   ${({ isUp }) => isUp && `transform: rotate(180deg)`};
 
   ${({ hideIcon, ordered }) => !ordered && `opacity: ${hideIcon ? 0 : 1};`}
--- src/app/views/dashboard/DashboardList/styled.js
+++ src/app/views/dashboard/DashboardList/styled.js
@@ -69,7 +69,7 @@ export const DashboardTasksGroupLabel = styled.div`
 export const DashboardTasksGroupLabelName = styled.div`
   position: relative;
   flex: ${({ width }) => (width ? `${width} 0 0` : '1')};
-  padding: ${spacing.small} ${spacing.regular} ${spacing.small} 0;
+  padding: 8px 16px;
   height: 35px;
   overflow: visible;
 
