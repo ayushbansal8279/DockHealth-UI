865a740a1b326bea595bf2c1c6e3a67fc31a43d5
Dawid Wójcik <22615841+dwojcik96@users.noreply.github.com>
2021-03-11 10:56:09 +0100

    Add modal (#1157)

---
+++ src/app/modal/components/SendingInviteModal/styled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import palette from 'styles/palette';
+import spacing from 'styles/spacing';
+
+export const ModalWrapper = styled.div`
+  font-family: 'Roboto Condensed', sans-serif;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 340px;
+  height: 110px;
+  background-color: ${palette.white};
+`;
+
+export const ModalLabel = styled.div`
+  font-size: 18px;
+  text-transform: uppercase;
+  color: ${palette.mediumGrey};
+  margin-left: ${spacing.regular};
+`;
