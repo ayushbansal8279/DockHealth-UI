70b1ebbbfcad13d3bb97d2cf70c176e2bffcebc5
Maciej Laufer <maciek.laufer@gmail.com>
2021-03-12 17:55:51 +0100

    Remove console log

--- src/app/api/task-api.js
+++ src/app/api/task-api.js
@@ -124,7 +124,6 @@ export function deleteTask(taskIdentifier) {
   return axios
     .delete(`task/deleteTaskById/${taskIdentifier}`)
     .then(response => {
-      console.log('response', response);
       return response;
     })
     .catch(error => {
