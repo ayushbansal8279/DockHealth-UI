9e916b8b19c071ba568f2c338e19cad0c172d90d
Dawid Wójcik <22615841+dwojcik96@users.noreply.github.com>
2021-04-02 14:26:15 +0200

    Fix (#1179)

--- src/app/sagas/list-details-saga.js
+++ src/app/sagas/list-details-saga.js
@@ -501,7 +501,7 @@ function* doReassignTasksToAnotherGroup(payload) {
     const destinationTasks = insert(
       destinationIndex,
       sourceTask,
-      destinationGroup.tasks || [],
+      destinationGroup?.tasks || [],
     );
 
     yield put({
--- src/app/views/list-details/ListDetailsOpenedTasksContainer/ListDetailsOpenedTasks.jsx
+++ src/app/views/list-details/ListDetailsOpenedTasksContainer/ListDetailsOpenedTasks.jsx
@@ -99,7 +99,7 @@ const ListDetailsOpenedTasks = ({
       setDraggableId(null);
       onTaskOrderChanged();
 
-      if (source.droppableId === destination.droppableId) {
+      if (source?.droppableId === destination?.droppableId) {
         reorderTasksInGroup({ destination, source });
       } else {
         reassignTasksToAnotherGroup({ destination, source });
