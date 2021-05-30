802bdd5bc83c979c76eedb561a8040cde401d1f6
Nitin Gujral <nitin@dock.health>
2021-05-30 15:27:15 -0400

    fixed api parameter for bulk edit

--- src/app/components/tasklist/BulkEditSection/BulkEditOptionsBar/BulkEditOptionsBar.jsx
+++ src/app/components/tasklist/BulkEditSection/BulkEditOptionsBar/BulkEditOptionsBar.jsx
@@ -211,7 +211,7 @@ const BulkEditOptionsBar = ({
       bulkEditTasksApi({
         bulkEditType: 'STATUS',
         taskIdentifiers: allSelectedTasksIdentifiers,
-        workflowStatus: workflowStatus?.identifier || null,
+        workflowStatusIdentifier: workflowStatus?.identifier || null,
       })
         .then(({ transactionIdentifier }) => {
           dispatch(
