37869e14602659618dda7ac8eeb6951e918ce568
Nitin Gujral <nitin@dock.health>
2021-07-11 15:17:29 -0400

    updated display of select form elements with readonly state

--- src/app/components/common/Select/Select.jsx
+++ src/app/components/common/Select/Select.jsx
@@ -15,43 +15,69 @@ import {
   oneOfType,
   oneOf,
 } from 'prop-types';
+import { boolean } from 'yup';
+import Input from '../Input/Input';
 
-const Select = ({ options, label, name, error, variant, ...restProps }) => (
-  <FormControl error={error}>
-    <InputLabel variant={variant}>{label}</InputLabel>
-    <MuiSelect
-      MenuProps={{
-        anchorOrigin: {
-          vertical: 'bottom',
-          horizontal: 'left',
-        },
-        transformOrigin: {
-          vertical: 'top',
-          horizontal: 'left',
-        },
-        getContentAnchorEl: null,
-      }}
-      inputProps={{ name }}
-      variant={variant}
-      {...restProps}
-    >
-      {options.map(option => (
-        <MenuItem value={option.value}>{option.label}</MenuItem>
-      ))}
-    </MuiSelect>
-    {error && (
-      <FormHelperText variant={variant} error={error}>
-        {error}
-      </FormHelperText>
-    )}
-  </FormControl>
-);
+const Select = ({
+  options,
+  label,
+  name,
+  error,
+  variant,
+  readOnly,
+  value,
+  ...restProps
+}) => {
+  const selectedOption = options.find(element => element.value === value);
+  return (
+    <FormControl error={error}>
+      <InputLabel shrink={!!value} variant={variant}>
+        {label}
+      </InputLabel>
+      {readOnly ? (
+        <Input
+          name={name}
+          readOnly={readOnly}
+          value={selectedOption?.label}
+          {...restProps}
+        />
+      ) : (
+        <MuiSelect
+          MenuProps={{
+            anchorOrigin: {
+              vertical: 'bottom',
+              horizontal: 'left',
+            },
+            transformOrigin: {
+              vertical: 'top',
+              horizontal: 'left',
+            },
+            getContentAnchorEl: null,
+          }}
+          inputProps={{ name }}
+          variant={variant}
+          {...restProps}
+        >
+          {options.map(option => (
+            <MenuItem value={option.value}>{option.label}</MenuItem>
+          ))}
+        </MuiSelect>
+      )}
+      {error && (
+        <FormHelperText variant={variant} error={error}>
+          {error}
+        </FormHelperText>
+      )}
+    </FormControl>
+  );
+};
 
 Select.propTypes = {
   onChange: func,
   label: string.isRequired,
   name: string.isRequired,
   variant: oneOf(['filled']),
+  readonly: boolean,
   options: arrayOf(
     shape({
       label: string.isRequired,
@@ -62,6 +88,7 @@ Select.propTypes = {
 
 Select.defaultProps = {
   variant: 'filled',
+  readonly: false,
   onChange: undefined,
 };
 
