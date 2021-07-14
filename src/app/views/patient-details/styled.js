fda0a4915219ac9f3edffb4b65c363c28a975d6d
Nitin Gujral <nitin@dock.health>
2021-07-14 13:07:46 -0400

    updated styles for main tabs in Patient Details

--- src/app/views/patient-details/styled.js
+++ src/app/views/patient-details/styled.js
@@ -2,6 +2,8 @@
 import styled from 'styled-components';
 import palette from 'styles/palette';
 import spacing from 'styles/spacing';
+import { fontWeights, fontSizes } from 'styles/font';
+import { Tab } from '@material-ui/core';
 
 export const PatientDetailsTabsContainer = styled.div`
   padding: 0 ${spacing.huge};
@@ -16,4 +18,11 @@ export const PatientDetailsContainer = styled.div`
 export const SearchWrapper = styled.div`
   width: ${({ fullWidth }) => (fullWidth ? 374 : 115)}px;
   transition: width 0.25s ease-out;
+`;
+
+export const MainTab = styled(Tab)`
+  .MuiTab-wrapper {
+    font-weight: ${fontWeights.bold};
+    font-size: ${fontSizes.regularPlus};
+  }
 `;
