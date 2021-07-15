6c2796c375489ea51deb3177c14e59d415785710
Nitin Gujral <nitin@dock.health>
2021-07-14 23:18:45 -0400

    fixed modal for customer type config

--- src/app/modal/components/InterruptEditModal/InterruptEditModal.js
+++ src/app/modal/components/InterruptEditModal/InterruptEditModal.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Typography } from '@material-ui/core';
 import Button from 'components/common/Button/Button';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import Spacing from 'components/common/Spacing';
+import { getCustomerTypeLabel } from 'helpers/customer-type-helper';
 import DownArrow from 'img/modals/down-arrow';
 import { redTheme } from '../../themes/red-theme';
 
@@ -17,13 +19,19 @@ import {
 } from '../styled';
 
 const InterruptEditModal = ({ closeModal, confirm }) => {
+  const { currentUser } = useSelector(store => ({
+    currentUser: store.userState.userProfile,
+  }));
+  const customerTypeLabel = getCustomerTypeLabel(currentUser);
+
   return (
     <MuiThemeProvider theme={redTheme}>
       <ModalWrapper>
         <ModalIconContainer>
           <ModalMainIcon src={DownArrow} alt="Task" />
           <Typography variant="body1">
-            YOU HAVE UNSAVED CHANGES TO THE PATIENT’S PROFILE
+            YOU HAVE UNSAVED CHANGES TO THE {customerTypeLabel.toUpperCase()}
+            &apos;S PROFILE
           </Typography>
         </ModalIconContainer>
         <ModalDescriptionContainer />
