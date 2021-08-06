f59a3e627cbb5ccf6df9083a9e451dcf2c83895b
Maciej Laufer <maciek.laufer@gmail.com>
2021-08-06 16:44:18 +0200

    Fix shadow for popover

--- src/app/components/common/SelectorPopover/styled.js
+++ src/app/components/common/SelectorPopover/styled.js
@@ -10,14 +10,3 @@ export const ItemsList = styled.div`
   padding: ${props =>
     props.withPadding ? `${spacing.large} ${spacing.largePlus}` : ''};
 `;
-
-export const ShadowWrapper = styled.div`
-  /* border: 2px solid black; */
-  box-shadow: 4px 2px 2px 0px rgba(0, 0, 0, 0.1),
-    4px 2px 3px 4px rgba(0, 0, 0, 0.07), 4px 2px 6px 4px rgba(0, 0, 0, 0.06);
-  /* box-shadow: 5px 5px 5px 5px black; */
-  margin-top: 2px;
-  margin-left: 2px;
-  margin-right: 8px;
-  margin-bottom: 8px;
-`;
