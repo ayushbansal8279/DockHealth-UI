86e09088c0ff589ab2b951185ae40d6436fd60cf
Nitin Gujral <nitin@dock.health>
2021-08-06 00:14:39 -0400

    temporary fix for list invite by admin / owner

--- src/app/components/members/InviteMemberToListForm/ExternalInviteForm/OrganizationOwnerForm/OrganizationOwnerForm.js
+++ src/app/components/members/InviteMemberToListForm/ExternalInviteForm/OrganizationOwnerForm/OrganizationOwnerForm.js
@@ -1,21 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { Grid } from '@material-ui/core';
 import { FormContext, useForm } from 'react-hook-form';
-import { object, string } from 'yup';
+// import { object, string } from 'yup';
 import Spacing from 'components/common/Spacing';
 import UserDetailsStep from './UserDetailsStep';
 import UserRoleStep from './UserRoleStep';
 import { FormWrapper, Step } from './styled';
 
-const REQUIRED_FIELD = 'This field is required';
+// const REQUIRED_FIELD = 'This field is required';
 
-const validationSchema = object().shape({
-  firstName: string().required(REQUIRED_FIELD),
-  lastName: string().required(REQUIRED_FIELD),
-  email: string()
-    .required(REQUIRED_FIELD)
-    .email('Please enter a valid email address'),
-});
+// const validationSchema = object().shape({
+//   firstName: string().required(REQUIRED_FIELD),
+//   lastName: string().required(REQUIRED_FIELD),
+//   email: string()
+//     .required(REQUIRED_FIELD)
+//     .email('Please enter a valid email address'),
+// });
 
 const FormStep = {
   USER_DETAILS: 'USER_DETAILS',
@@ -29,34 +29,55 @@ const OrganizationOwnerForm = ({
   closeInviteForm,
 }) => {
   const [currentFormStep, setCurrentFormStep] = useState(FormStep.USER_DETAILS);
+  const [firstName, setFirstName] = useState(null);
+  const [lastName, setLastName] = useState(null);
+  const [email, setEmail] = useState(null);
 
   const formContext = useForm({
-    validationSchema,
+    // validationSchema,
     defaultValues: { ...initialValues, userRole: 'GUEST' },
   });
 
   const { handleSubmit, register, unregister } = formContext;
 
   useEffect(() => {
-    register({ name: 'firstName' });
-    register({ name: 'lastName' });
-    register({ name: 'email' });
-    register({ name: 'userRole' });
+    if (currentFormStep === FormStep.USER_DETAILS) {
+      register({ name: 'firstName' });
+      register({ name: 'lastName' });
+      register({ name: 'email' });
+    } else {
+      register({ name: 'userRole' });
+    }
 
     return () => {
-      unregister('firstName');
-      unregister('lastName');
-      unregister('email');
-      unregister('userRole');
+      if (currentFormStep === FormStep.USER_DETAILS) {
+        unregister('firstName');
+        unregister('lastName');
+        unregister('email');
+      } else {
+        unregister('userRole');
+      }
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [currentFormStep]);
 
   const ownerFormSubmit = data => {
     if (currentFormStep === FormStep.USER_DETAILS) {
       setCurrentFormStep(FormStep.USER_ROLE);
+      setFirstName(data.firstName);
+      setLastName(data.lastName);
+      setEmail(data.email);
+      formContext.defaultValues = {
+        firstName: data.firstName,
+        lastName: data.lastName,
+        email: data.email,
+      };
     } else {
-      onSubmit(data);
+      const inviteData = data;
+      inviteData.firstName = firstName;
+      inviteData.lastName = lastName;
+      inviteData.email = email;
+      onSubmit(inviteData);
     }
   };
 
