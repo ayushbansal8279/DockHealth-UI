a9d38a0e90ea9f818e22e6415e3efbccc947d817
Maciej Laufer <maciek.laufer@gmail.com>
2021-09-01 15:16:10 +0200

    Teplate name api bugfix

--- src/app/api/task-template-api.js
+++ src/app/api/task-template-api.js
@@ -72,10 +72,13 @@ export function getTasksForTemplate(taskTemplateIdentifier) {
     });
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function getTemplate(identifier) {
-  return axios.get(`task/template/${identifier}`).then(({ data }) => {
-    return data;
-  });
+  // TODO: uncomment api request after endpoint creation
+  return Promise.resolve({});
+  // return axios.get(`task/template/${identifier}`).then(({ data }) => {
+  //   return data;
+  // });
 }
 
 export function addTemplate(newTemplate, parentTaskTemplateIdentifier) {
