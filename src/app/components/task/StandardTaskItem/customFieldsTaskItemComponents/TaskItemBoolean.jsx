ff6b0951775877601f6de184e59de7ee5bd719b1
Nitin Gujral <nitin@dock.health>
2021-10-31 11:20:25 -0400

    capitalize display of boolean values

--- src/app/components/task/StandardTaskItem/customFieldsTaskItemComponents/TaskItemBoolean.jsx
+++ src/app/components/task/StandardTaskItem/customFieldsTaskItemComponents/TaskItemBoolean.jsx
@@ -2,13 +2,14 @@
 import React from 'react';
 import { CustomFieldWidthConfig, FieldType } from 'helpers/field-type-helpers';
 import { Typography } from '@material-ui/core';
+import { capitalize } from 'helpers/capitalize';
 import { DecisionBox, StandardTaskItemCell } from './styled';
 
 const TaskItemBoolean = ({ value }) => {
   return (
     <StandardTaskItemCell width={CustomFieldWidthConfig[FieldType.BOOL]}>
       <DecisionBox>
-        <Typography>{value}</Typography>
+        <Typography>{value ? capitalize(value) : ''}</Typography>
       </DecisionBox>
     </StandardTaskItemCell>
   );
