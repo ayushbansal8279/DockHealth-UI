10bde984283a6a8d040063bb69d59add8d4d465f
TomaszNowakowskiFrontEnd <77610574+TomaszNowakowskiFrontEnd@users.noreply.github.com>
2021-11-04 16:02:50 +0100

    error resolved (#1408)

--- src/app/views/custom-fields/CustomFieldsView.jsx
+++ src/app/views/custom-fields/CustomFieldsView.jsx
@@ -9,6 +9,7 @@ import {
 } from 'selectors/user-selectors';
 import { setHeader } from 'actions/template-actions';
 import GenericHeader from 'components/template/GenericHeader/GenericHeader';
+import { ColumnsConfigProvider } from 'context-api/ColumnsConfigContext';
 import TaskCustomFieldsView from './TaskCustomFieldsView';
 import PatientCustomFieldsView from './PatientCustomFieldsView';
 import { ViewContainer } from './styled';
@@ -57,25 +58,27 @@ const CustomFieldsView = () => {
   }, []);
 
   return (
-    <ViewContainer>
-      <Tabs
-        value={selectedTab}
-        onChange={(event, index) => setSelectedTab(index)}
-        indicatorColor="secondary"
-        textColor="inherit"
-        variant="fullWidth"
-      >
-        {patientCustomFieldsAvailable && (
-          <Tab label="Patient Custom Fields" {...applyProps(0)} />
-        )}
-        {taskCustomFieldsAvailable && (
-          <Tab label="Task Custom Fields" {...applyProps(1)} />
-        )}
-      </Tabs>
-      <Box p={1} />
-      {selectedTab === 0 && <PatientCustomFieldsView />}
-      {selectedTab === 1 && <TaskCustomFieldsView editable />}
-    </ViewContainer>
+    <ColumnsConfigProvider>
+      <ViewContainer>
+        <Tabs
+          value={selectedTab}
+          onChange={(event, index) => setSelectedTab(index)}
+          indicatorColor="secondary"
+          textColor="inherit"
+          variant="fullWidth"
+        >
+          {patientCustomFieldsAvailable && (
+            <Tab label="Patient Custom Fields" {...applyProps(0)} />
+          )}
+          {taskCustomFieldsAvailable && (
+            <Tab label="Task Custom Fields" {...applyProps(1)} />
+          )}
+        </Tabs>
+        <Box p={1} />
+        {selectedTab === 0 && <PatientCustomFieldsView />}
+        {selectedTab === 1 && <TaskCustomFieldsView editable />}
+      </ViewContainer>
+    </ColumnsConfigProvider>
   );
 };
 
