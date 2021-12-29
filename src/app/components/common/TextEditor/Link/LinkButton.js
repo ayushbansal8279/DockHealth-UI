316498fe35d64f275183da54a86e829bca457eab
TomaszNowakowskiFrontEnd <77610574+TomaszNowakowskiFrontEnd@users.noreply.github.com>
2021-12-29 12:06:16 +0100

    Dhwp 1409 do named links editable (#1462)

--- src/app/components/common/TextEditor/Link/LinkButton.js
+++ src/app/components/common/TextEditor/Link/LinkButton.js
@@ -1,18 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable import/extensions */
-import React, { useCallback, useState, useRef } from 'react';
+import React, { useCallback, useState, useRef, useMemo } from 'react';
 import LinkIcon from '@material-ui/icons/Link';
+import { Entity, SelectionState, EditorState } from 'draft-js';
 import { LinkButtonContainer, LinkButton } from './styled';
 import LinkPopover from './LinkPopover';
-import {
-  createLinkAtSelection,
-  removeLinkAtSelection,
-  hasEntity,
-} from './helpers';
+import { createLinkAtSelection, hasEntity } from './helpers';
 
 const Link = ({ getEditorState, setEditorState }) => {
   const buttonReference = useRef();
-  // const textInputReference = useRef();
   const [popoverOpen, setPopoverOpen] = useState(false);
 
   const handleConfirm = useCallback(
@@ -25,8 +21,35 @@ const Link = ({ getEditorState, setEditorState }) => {
   const handleOpen = useCallback(
     flag => {
       if (hasEntity(getEditorState(), 'LINK')) {
-        const nextEditorState = removeLinkAtSelection(getEditorState());
-        setEditorState(nextEditorState);
+        const editorState = getEditorState();
+        const selection = editorState?.getSelection();
+        if (selection.isCollapsed()) {
+          const content = editorState.getCurrentContent();
+          const startKey = selection.getStartKey();
+          const startOffset = selection.getStartOffset();
+          const block = content.getBlockForKey(startKey);
+          const entity = block.getEntityAt(startOffset);
+
+          block.findEntityRanges(
+            character => character.getEntity() === entity,
+            (start, end) => {
+              const newSelection = selection.merge({
+                anchorOffset: start,
+                focusOffset: end,
+              });
+              const newEditorState = EditorState.acceptSelection(
+                editorState,
+                newSelection,
+              );
+              const state = EditorState.forceSelection(
+                newEditorState,
+                newEditorState.getSelection(),
+              );
+              setEditorState(state);
+            },
+          );
+        }
+        setPopoverOpen(flag);
       } else {
         setPopoverOpen(flag);
       }
@@ -34,7 +57,7 @@ const Link = ({ getEditorState, setEditorState }) => {
     [getEditorState, setEditorState],
   );
 
-  const getInitText = useCallback(() => {
+  const initText = useMemo(() => {
     const selection = getEditorState().getSelection();
     const anchorKey = selection.getAnchorKey();
     const currentContent = getEditorState().getCurrentContent();
@@ -42,6 +65,24 @@ const Link = ({ getEditorState, setEditorState }) => {
     const start = selection.getStartOffset();
     const end = selection.getEndOffset();
     return currentBlock.getText().slice(start, end);
+  }, [getEditorState]);
+
+  const initLink = useMemo(() => {
+    try {
+      const editorState = getEditorState();
+      const selection = editorState.getSelection();
+      const content = editorState.getCurrentContent();
+      const startKey = selection.getStartKey();
+      const startOffset = selection.getStartOffset();
+      const block = content.getBlockForKey(startKey);
+      const linkKey = block.getEntityAt(startOffset);
+      const linkInstance = Entity.get(linkKey);
+      const { url } = linkInstance.getData();
+
+      return url || '';
+    } catch {
+      return '';
+    }
   }, [getEditorState]);
 
   return (
@@ -57,7 +98,8 @@ const Link = ({ getEditorState, setEditorState }) => {
           <LinkIcon />
         </LinkButton>
         <LinkPopover
-          initText={getInitText()}
+          initText={initText}
+          initLink={initLink}
           anchorElement={buttonReference}
           isPopoverOpen={popoverOpen}
           close={() => handleOpen(false)}
