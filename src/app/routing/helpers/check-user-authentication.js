a983c3cf096c3713a3175847757ed769a96d8469
Nitin Gujral <nitin@dock.health>
2022-03-21 22:57:01 -0400

    fix navigate to patient view based on token response for Athena SSO

--- src/app/routing/helpers/check-user-authentication.js
+++ src/app/routing/helpers/check-user-authentication.js
@@ -34,12 +34,10 @@ const checkUserAuthentication = async ({ history, isRequiredLogin }) => {
             patientIdentifier !== '' &&
             patientIdentifier !== 'null'
           ) {
-            return {
-              redirectPath: `/#/core/patient/${patientIdentifier}`,
-              user: null,
-            };
+            window.location.href = `/#/core/patient/${patientIdentifier}`;
+          } else {
+            window.location.href = `/#/core/home`;
           }
-          return { redirectPath: '/core/home', user: null };
         })
         .catch(error => {
           showAlert({ status: 'error', title: 'Error', text: error.message });
