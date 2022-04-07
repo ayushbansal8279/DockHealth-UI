df498c90a32fa8117cd97d24dc08b82b120db56e
Nitin Gujral <nitin@dock.health>
2022-04-07 00:36:21 -0400

    fixed a small bug when adding first task to a new list

--- src/app/sagas/list-details-saga.js
+++ src/app/sagas/list-details-saga.js
@@ -556,7 +556,7 @@ function* doCreateTask(payload) {
         }
       } else {
         const fetchedTasksGroups = yield select(groupTasksSelector);
-        const taskGroup = fetchedTasksGroups.find(
+        const taskGroup = fetchedTasksGroups?.find(
           ({ groupIdentifier }) => groupIdentifier === taskGroupIdentifier,
         );
 
