d750d27e69da70d40658b68b7fad3e7dbc74075d
pawelkeska00@gmail.com <Pawelwoel0912>
2022-04-12 10:41:29 +0200

    Code cleanup.

--- src/app/components/activity-alerts/ActivityAlerts.js
+++ src/app/components/activity-alerts/ActivityAlerts.js
@@ -7,7 +7,6 @@ import NewBlueBellIcon from 'img/notifications/new-blue-bell';
 import WhiteBellIcon from 'img/notifications/white-bell';
 import NewWhiteBellIcon from 'img/notifications/new-white-bell';
 import CrossedBellIcon from 'img/notifications/crossed-bell';
-// import SettingsIcon from 'img/settings-icon';
 import SettingsIcon from 'img/navigation/SettingsIcon';
 import { onActivityAlertOpened } from 'helpers/ga-event-helper';
 import * as ActivityAlertsApi from 'api/activity-alerts-api';
