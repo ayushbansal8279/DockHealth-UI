ef9b24d62d4a390bf4590209c7c459cae1588528
Zachary Shaked <zshaked@dock.health>
2022-05-25 18:13:39 -0400

    Update styled.js
    Patient Card Notes: show pinned notes at the top and reverse chronological

--- src/app/components/patients/PatientCard/styled.js
+++ src/app/components/patients/PatientCard/styled.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { fontSizes, fontWeights } from 'styles/font';
 import palette from 'styles/palette';
 import spacing from 'styles/spacing';
+import PinIcon from 'img/pin';
 
 export const PatientCellWrapper = styled.span``;
 
@@ -51,6 +52,25 @@ export const PatientNotesSection = styled.div`
 
 export const PatientNote = styled.div`
   width: 100%;
+`;
+export const PatientNotesWrapper = styled.div`
+  width: 100%;
+  max-width: 800px;
+  background: ${palette.white};
+  padding: 12px;
+`;
+
+export const PinnedNotesWrapper = styled.div`
+  // position: relative;
+  background-color: ${palette.coolGrey4};
+  border: 1px solid ${palette.coolGrey3};
+
+  &:before {
+    content: url(${PinIcon});
+    position: absolute;
+    top: 9px;
+    left: 6px;
+  }
 `;
 
 export const NoteDescription = styled.p`
