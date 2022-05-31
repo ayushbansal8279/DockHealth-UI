d5ad806553e7f2cc7fafbb28753ba4b98ed159ef
Zachary Shaked <zshaked@dock.health>
2022-05-31 12:14:48 -0400

    Birthday and Age on PatientCard and PatientView

--- src/app/components/patients/PatientCard/PatientCard.js
+++ src/app/components/patients/PatientCard/PatientCard.js
@@ -87,9 +87,6 @@ const renderPatientNotes = (
       </PatientNote>
     );
   };
-
-
-
   return notes?.length > 0 ? (
     <>
       <Divider />
@@ -219,9 +216,10 @@ const PatientCard = ({
                   <>
                     <Spacing vertical={2} />
                     <PatientInfo>
-                      {(age || gender) && (
+                      {(age || gender || dob) && (
                         <InfoItem>
-                          {age && `${age} `}
+                          {dob && `${moment(dob).format('MMM D, YYYY')} | `}
+                          {age && `${age} | `}
                           {gender && `${gender?.charAt(0)?.toUpperCase()}`}
                         </InfoItem>
                       )}
