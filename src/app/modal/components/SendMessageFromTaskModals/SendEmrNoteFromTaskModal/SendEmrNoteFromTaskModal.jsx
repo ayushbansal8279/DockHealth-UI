dfa9e0ac06c8cc29a757a1915a67b71574b1affe
piotr-dziubek <piotr.dziubek@htdevelopers.com>
2022-06-23 15:27:05 +0200

    DHWP-1106-send-emr-from-task-wip

---
+++ src/app/modal/components/SendMessageFromTaskModals/SendEmrNoteFromTaskModal/SendEmrNoteFromTaskModal.jsx
@@ -0,0 +1,151 @@
+import React, { useCallback, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectedTaskSelector } from 'selectors/task-drawer-selectors';
+import { EditorState } from 'draft-js';
+import { convertFromEditorStateToOutput } from 'components/common/TextEditor/helpers';
+import { createMentionEntities } from 'components/common/TextEditor/create-mention-entities';
+import { IconButton } from '@material-ui/core';
+import { Replay } from '@material-ui/icons';
+import palette from 'styles/palette';
+import Button from 'components/common/Button/Button';
+import { closeModal } from 'modal/actions';
+import CustomTextEditor from 'components/task-drawer/CustomTextEditor/CustomTextEditor';
+import TextEditor from 'components/common/TextEditor/TextEditor';
+import Select from 'components/common/Select/Select';
+import {
+  IncludeContainerStyled,
+  InfoHeaderTextStyled,
+  InputContainerStyled,
+  TextEditorContainerStyled,
+} from '../styled';
+import {
+  CloseIcon,
+  CloseIconButton,
+  ModalDescription,
+  ModalDescriptionContainer,
+  ModalFooterStyled,
+  ModalHeader,
+  ModalHeaderContainerStyled,
+  ModalWrapper,
+} from '../../styled';
+import TaskCheckBoxes from '../TaskCheckBoxes';
+
+function SendEmrNoteFromTaskModal() {
+  const selectedTask = useSelector(selectedTaskSelector);
+  const { taskMentions } = selectedTask;
+  const dispatch = useDispatch();
+  const [noteType, setNoteType] = useState(null);
+  const [isTaskDescriptionIncluded, setIsTaskDescriptionIncluded] = useState(
+    false,
+  );
+  const [isTaskDetailsIncluded, setIsTaskDetailsIncluded] = useState(false);
+
+  const [isTaskCommentsIncluded, setIsTaskCommentsIncluded] = useState(false);
+
+  const [detailsState, setDetailsState] = useState(() =>
+    EditorState.createEmpty(),
+  );
+
+  const insertTextFromTask = useCallback(
+    meta => {
+      const currentState = convertFromEditorStateToOutput(detailsState, true);
+      const newState = createMentionEntities(
+        `${currentState.meta}\n ${meta.meta}`,
+        `${currentState.tokenizedText}\n${meta.tokenized}`,
+        [...currentState.mentions, taskMentions],
+        true,
+      );
+
+      setDetailsState(EditorState.push(detailsState, newState));
+    },
+    [detailsState, taskMentions],
+  );
+
+  const handleReset = useCallback(() => {
+    setIsTaskCommentsIncluded(false);
+    setIsTaskDescriptionIncluded(false);
+    setIsTaskDetailsIncluded(false);
+    setDetailsState(EditorState.createEmpty());
+  }, []);
+
+  const handleOptionChange = event => {
+    const { value } = event.target;
+    setNoteType(value);
+  };
+
+  return (
+    <ModalWrapper width="600px">
+      <CloseIconButton size="small" onClick={() => dispatch(closeModal())}>
+        <CloseIcon htmlColor="#C1CCDA" />
+      </CloseIconButton>
+      <ModalHeaderContainerStyled>
+        <ModalHeader>post note to emr</ModalHeader>
+        <ModalDescription>
+          Post a summary to the patient’s medical record
+        </ModalDescription>
+      </ModalHeaderContainerStyled>
+      <ModalDescriptionContainer>
+        <InputContainerStyled>
+          <Select
+            options={[{ label: '1', value: '13' }]}
+            value={noteType}
+            onChange={handleOptionChange}
+          />
+        </InputContainerStyled>
+        <IncludeContainerStyled>
+          <InfoHeaderTextStyled>Include the following</InfoHeaderTextStyled>
+          <IconButton onClick={handleReset}>
+            <Replay htmlColor={palette.coolGrey9} />
+          </IconButton>
+        </IncludeContainerStyled>
+        <TaskCheckBoxes
+          isTaskDescriptionIncluded={isTaskDescriptionIncluded}
+          setIsTaskDescriptionIncluded={setIsTaskDescriptionIncluded}
+          isTaskDetailsIncluded={isTaskDetailsIncluded}
+          setIsTaskDetailsIncluded={setIsTaskDetailsIncluded}
+          isTaskCommentsIncluded={isTaskCommentsIncluded}
+          setIsTaskCommentsIncluded={setIsTaskCommentsIncluded}
+          insertTextFromTask={insertTextFromTask}
+          selectedTask={selectedTask}
+        />
+        <TextEditorContainerStyled>
+          <CustomTextEditor label="Emr note">
+            <TextEditor
+              readOnly={false}
+              minHeight={100}
+              disableMentions
+              showToolbar
+              state={detailsState}
+              onChange={setDetailsState}
+            />
+          </CustomTextEditor>
+        </TextEditorContainerStyled>
+      </ModalDescriptionContainer>
+      <ModalFooterStyled>
+        <Button
+          uppercase
+          width="150px"
+          variant="primary"
+          disabled={!detailsState.getCurrentContent().hasText()}
+          onClick={() => {
+            // dispatch(
+            //   sendEMRForTask({
+            //     message: subject,
+            //     details: convertFromEditorStateToOutput(detailsState, true)
+            //       .tokenizedText,
+            //     recipientContact: email,
+            //     taskAttachmentIdentifiers: attachmentsToSend,
+            //     taskIdentifier: identifier,
+            //   }),
+            // );
+            dispatch(closeModal());
+          }}
+        >
+          post
+        </Button>
+      </ModalFooterStyled>
+    </ModalWrapper>
+  );
+}
+
+export default SendEmrNoteFromTaskModal;
--- src/app/modal/components/SendMessageFromTaskModals/styled.js
+++ src/app/modal/components/SendMessageFromTaskModals/styled.js
@@ -34,3 +34,33 @@ export const InputContainerStyled = styled.div`
   gap: 0.5rem;
   flex-direction: column;
 `;
+
+export const CheckboxContainerStyled = styled.div`
+  display: flex;
+  align-content: center;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+  gap: 0.5rem;
+`;
+
+export const IncludeContainerStyled = styled.div`
+  padding-top: 1.5rem;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  align-items: center;
+`;
+
+export const TextEditorContainerStyled = styled.div`
+  margin-top: 1.5rem;
+  width: 100%;
+  background: #f7fafb;
+`;
+
+export const InfoHeaderTextStyled = styled.p`
+  color: ${palette.coolGrey9};
+  font-family: 'Montserrat';
+  font-weight: 600;
+  margin: 0;
+`;
