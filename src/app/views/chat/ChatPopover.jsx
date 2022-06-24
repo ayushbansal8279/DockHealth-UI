58065196b2393f80ab46a9158fe6080a16a50769
Zachary Shaked <zshaked@dock.health>
2022-06-24 16:51:52 -0400

    -added draggable area, leave button, fixed sizing

--- src/app/views/chat/ChatPopover.jsx
+++ src/app/views/chat/ChatPopover.jsx
@@ -1,18 +1,28 @@
 import React, { useCallback, useState } from 'react';
 import { IconButton, Popover, Box } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 import ChatIcon from '@material-ui/icons/Chat';
 import CloseIcon from '@material-ui/icons/Close';
+import OpenInNewIcon from '@material-ui/icons/OpenInNew';
 import Draggable from 'react-draggable';
 import { useBoolean } from 'hooks/useBoolean';
+import { CHAT_PATH } from 'routing/helpers/paths';
 import { App as SendbirdApp } from 'sendbird-uikit';
 import { userProfileSelector } from 'selectors/user-selectors';
 import { useSelector } from 'react-redux';
-import { Container, ColorSet, StyledIconButton } from './styled';
+import {
+  Container,
+  ColorSet,
+  StyledIconButton,
+  HeaderContainer,
+  ChatContainer,
+} from './styled';
 
 const ChatPopover = () => {
   const { name, identifier } = useSelector(userProfileSelector);
   const [open, setOpen, unsetOpen] = useBoolean(false);
   const [anchorElement, setAnchorElement] = useState(null);
+  const history = useHistory();
 
   const appId =
     process.env.SENDBIRD_APP_ID ?? 'D11A4B11-21AD-4025-9D8C-2BCF693C814C';
@@ -26,6 +36,12 @@ const ChatPopover = () => {
     unsetOpen();
     setAnchorElement(null);
   }, [unsetOpen]);
+
+  const handleLeaveIconClick = useCallback(() => {
+    unsetOpen();
+    setAnchorElement(null);
+    history.push(CHAT_PATH);
+  }, [history, unsetOpen]);
 
   const id = open ? 'simple-popover' : undefined;
 
@@ -51,24 +67,32 @@ const ChatPopover = () => {
             vertical: 'bottom',
             horizontal: 'left',
           }}
+          PaperProps={{ style: { height: '100vh' } }}
           onClose={handleClose}
         >
-          <Container className="handle">
-            <Box display="flex">
+          <Container>
+            <HeaderContainer className="handle">
+              {/* <Box display="flex"> */}
+              <StyledIconButton onClick={handleLeaveIconClick}>
+                <OpenInNewIcon />
+              </StyledIconButton>
               <Box mx={0.5} />
               <StyledIconButton onClick={handleClose}>
                 <CloseIcon />
               </StyledIconButton>
-            </Box>
-            <SendbirdApp
-              appId={appId}
-              userId={identifier}
-              nickname={name}
-              colorSet={ColorSet}
-              useReaction
-              useMessageGrouping
-              userListQuery
-            />
+              {/* </Box> */}
+            </HeaderContainer>
+            <ChatContainer>
+              <SendbirdApp
+                appId={appId}
+                userId={identifier}
+                nickname={name}
+                colorSet={ColorSet}
+                useReaction
+                useMessageGrouping
+                userListQuery
+              />
+            </ChatContainer>
           </Container>
         </Popover>
       </Draggable>
--- src/app/views/chat/styled.js
+++ src/app/views/chat/styled.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
-// import palette from 'styles/palette';
 import { withStyles } from '@material-ui/core/styles';
 import { IconButton } from '@material-ui/core';
+import palette from 'styles/palette';
 import styled from 'styled-components';
 
 export const StyledIconButton = withStyles({
@@ -13,9 +13,9 @@ export const StyledIconButton = withStyles({
 export const Container = styled.div`
   max-width: 1280px;
   width: 100%;
-  height: 100vh;
+  height: 100%;
   margin: 0 auto;
-  padding: 16px 12px;
+  // padding: 16px 12px;
   zindex: 0;
 `;
 
@@ -26,3 +26,19 @@ export const ColorSet = {
   '--sendbird-light-primary-200': '#0496ff',
   '--sendbird-light-primary-100': '#027bc5',
 };
+
+export const HeaderContainer = styled.div`
+  height: 10%;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  background-color: ${palette.darkBlue};
+  color: ${palette.white};
+`;
+
+export const ChatContainer = styled.div`
+  height: 90%;
+  width: 100%;
+`;
