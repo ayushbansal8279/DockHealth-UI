9a94ec45cca4e94964a132cfd124a612bee756ae
Zachary Shaked <zshaked@dock.health>
2022-07-21 23:07:27 -0400

    -added in supprt for unread messages

--- src/app/components/navigation/NavigationTemplate/NavigationTemplate.jsx
+++ src/app/components/navigation/NavigationTemplate/NavigationTemplate.jsx
@@ -5,11 +5,13 @@ import { userProfileSelector } from 'selectors/user-selectors';
 import Intercom from 'react-intercom';
 import GlobalAlertChip from 'alert/GlobalAlertChip';
 import NavigationSidebar from 'components/navigation/NavigationSidebar/NavigationSidebar';
+import SendbirdProvider from '@sendbird/uikit-react/SendbirdProvider';
 import {
   DrawerContainer,
   MainContainer,
   useDrawerClasses,
   MaterialDrawer,
+  SendBirdThemeColorSet,
 } from './styled';
 
 const { INTERCOM_APP_CODE } = process.env;
@@ -17,6 +19,10 @@ const { INTERCOM_APP_CODE } = process.env;
 const NavigationTemplate = ({ children }) => {
   const currentUser = useSelector(userProfileSelector);
   const isNavbarVisible = useSelector(isNavbarVisibleSelector);
+
+  const appId =
+    process.env.SENDBIRD_APP_ID ?? 'D11A4B11-21AD-4025-9D8C-2BCF693C814C';
+  const { identifier, name } = currentUser;
 
   const drawerClasses = useDrawerClasses({
     isNavbarVisible,
@@ -31,25 +37,32 @@ const NavigationTemplate = ({ children }) => {
       : undefined;
 
   return (
-    <DrawerContainer>
-      <MaterialDrawer
-        classes={{
-          root: drawerClasses.drawer,
-          paper: drawerClasses.drawerPaper,
-        }}
-        variant="permanent"
-        anchor="left"
-      >
-        <NavigationSidebar />
-      </MaterialDrawer>
-      <MainContainer>
-        <GlobalAlertChip />
-        {children}
-        {intercomUser && intercomUser.name && (
-          <Intercom appID={INTERCOM_APP_CODE} {...intercomUser} />
-        )}
-      </MainContainer>
-    </DrawerContainer>
+    <SendbirdProvider
+      appId={appId}
+      userId={identifier}
+      nickname={name}
+      colorSet={SendBirdThemeColorSet}
+    >
+      <DrawerContainer>
+        <MaterialDrawer
+          classes={{
+            root: drawerClasses.drawer,
+            paper: drawerClasses.drawerPaper,
+          }}
+          variant="permanent"
+          anchor="left"
+        >
+          <NavigationSidebar />
+        </MaterialDrawer>
+        <MainContainer>
+          <GlobalAlertChip />
+          {children}
+          {intercomUser && intercomUser.name && (
+            <Intercom appID={INTERCOM_APP_CODE} {...intercomUser} />
+          )}
+        </MainContainer>
+      </DrawerContainer>
+    </SendbirdProvider>
   );
 };
 
