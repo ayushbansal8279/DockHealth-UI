978ba32820cb9d4e5d237edd5670e2cca1fd4ebe
Zachary Shaked <zshaked@dock.health>
2022-08-03 15:43:39 -0400

    -fixed fullscreen to use the same sendbirdprovider

--- src/app/views/chat/ChatView.js
+++ src/app/views/chat/ChatView.js
@@ -1,24 +1,16 @@
 import React, { useState } from 'react';
 import ViewLayout from 'components/template/ViewLayout/ViewLayout';
 import BasicLayoutHeader from 'components/template/BasicLayoutHeader/BasicLayoutHeader';
-import { userProfileSelector } from 'selectors/user-selectors';
-import { useSelector } from 'react-redux';
-import SBProvider from '@sendbird/uikit-react/SendbirdProvider';
 import withSendBird from '@sendbird/uikit-react/withSendBird';
 import { ChannelListProvider } from '@sendbird/uikit-react/ChannelList/context';
 import ChatConversation from './channel/ChatConversation';
 import ChatChannelList from './ChatChannelList';
 import SelectedChannelContext from './SelectedChannelContext';
 import ShowSettingsContext from './ShowSettingsContext';
-import { Container, ColorSet } from './styled';
+import { Container } from './styled';
 import './sendbird-styles.css';
 
 const ChatView = () => {
-  const { name, identifier } = useSelector(userProfileSelector);
-  const APP_ID =
-    process.env.REACT_APP_SENDBIRD_APP_ID ??
-    `D11A4B11-21AD-4025-9D8C-2BCF693C814C`;
-
   const [selectedChannel, setSelectedChannel] = useState(null);
   const value = { selectedChannel, setSelectedChannel };
 
@@ -31,21 +23,14 @@ const ChatView = () => {
         <div className="full-view-chat sendbird-app__wrap">
           <SelectedChannelContext.Provider value={value}>
             <ShowSettingsContext.Provider value={showSettingsValue}>
-              <SBProvider
-                appId={APP_ID}
-                userId={identifier}
-                nickname={name}
-                colorSet={ColorSet}
-              >
-                <ChannelListProvider>
-                  <ChatChannelList
-                    className="full-view-chat"
-                    setSelectedChannel={setSelectedChannel}
-                    isFullView
-                  />
-                </ChannelListProvider>
-                <ChatConversation currentChannelUrl={selectedChannel?.url} />
-              </SBProvider>
+              <ChannelListProvider>
+                <ChatChannelList
+                  className="full-view-chat"
+                  setSelectedChannel={setSelectedChannel}
+                  isFullView
+                />
+              </ChannelListProvider>
+              <ChatConversation currentChannelUrl={selectedChannel?.url} />
             </ShowSettingsContext.Provider>
           </SelectedChannelContext.Provider>
         </div>
