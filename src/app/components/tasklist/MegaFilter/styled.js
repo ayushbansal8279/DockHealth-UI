a8c26528e3986827748b3a16713f3a45b6bad561
Nitin Gujral <nitin.gujral@conns.com>
2022-09-11 23:39:05 -0400

    donot show border when mega filter options are selected

--- src/app/components/tasklist/MegaFilter/styled.js
+++ src/app/components/tasklist/MegaFilter/styled.js
@@ -11,7 +11,8 @@ export const MegaFilterNoResultsLabel = styled.p`
   margin-bottom: 0;
 `;
 export const MegaFilterContainer = styled.div`
-  border: 1px solid #c1ccda;
+  border: ${props =>
+    props.isFilterApplied === true ? 'none' : `1px solid #c1ccda`};
   padding: 0 8px;
   height: 36px;
   display: flex;
