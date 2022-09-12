ed4de2409032eb4214fe3585f2e32f7ded0a0f64
Nitin Gujral <nitin.gujral@conns.com>
2022-09-11 23:00:46 -0400

    minor cleanup

--- src/app/views/chat/ChatChannelList.jsx
+++ src/app/views/chat/ChatChannelList.jsx
@@ -6,7 +6,7 @@ import { userProfileSelector } from 'selectors/user-selectors';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectedChatChannelSelector } from 'selectors/sendbird-selectors';
 import { CircularProgress } from '@material-ui/core';
-import { Title } from 'views/TaskTour/styled';
+// import { Title } from 'views/TaskTour/styled';
 import { selectChannel } from 'actions/sendbird-actions';
 import CustomAddChannel from './add-channel/CustomAddChannel';
 import ChatChannelPreview from './ChatChannelPreview';
