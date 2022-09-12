758e3b2969433900802a0a636ba068b396f0dc4f
Nitin Gujral <nitin.gujral@conns.com>
2022-09-11 23:38:34 -0400

    disabled google drive for now

--- src/app/views/patient-details/PatientAttachments/PatientAttachments.jsx
+++ src/app/views/patient-details/PatientAttachments/PatientAttachments.jsx
@@ -3,8 +3,8 @@ import React, { useCallback, useEffect, useState } from 'react';
 import palette from 'styles/palette';
 import { useSelector } from 'react-redux';
 import localStorageHelper from 'helpers/local-storage-helper';
-import compose from 'ramda/src/compose';
-import { showGlobalErrorAlert } from 'alert/actions';
+// import compose from 'ramda/src/compose';
+// import { showGlobalErrorAlert } from 'alert/actions';
 import { Box, IconButton } from '@material-ui/core';
 import ViewModuleIcon from '@material-ui/icons/ViewModule';
 import ViewHeadlineIcon from '@material-ui/icons/ViewHeadline';
@@ -22,15 +22,15 @@ import FileListItemProgressBar from 'components/attachments/FileListItemProgress
 import FileGridItemProgressBar from 'components/attachments/FileGridItemProgressBar/FileGridItemProgressBar';
 import FileGridItemLoader from 'components/attachments/FileGridItemLoader/FileGridItemLoader';
 import FileListItemLoader from 'components/attachments/FileListItemLoader/FileListItemLoader';
-import GoogleDriveIcon from 'img/google-drive-icon';
-import GooglePicker from 'react-google-picker';
+// import GoogleDriveIcon from 'img/google-drive-icon';
+// import GooglePicker from 'react-google-picker';
 import {
   PatientAttachmentsWrapper,
   AttachmentFileInput,
   DropzoneInfoText,
   DropzoneContainer,
   EmptyListText,
-  DriveIcon,
+  // DriveIcon,
   DownloadAllLink,
 } from './styled';
 import initializeAttachmentsSectionHooks from './hooks';
@@ -58,7 +58,7 @@ const PatientAttachments = () => {
   }, [activeViewType]);
 
   const {
-    dispatch,
+    // dispatch,
     handleCreateFolderClick,
     attachmentsSources,
     currentPatientAttachments,
@@ -76,7 +76,7 @@ const PatientAttachments = () => {
     openFolderInNewTab,
     renameAttachment,
     moveFileOrFolder,
-    handleGooglePickerChange,
+    // handleGooglePickerChange,
     handleAttachmentClick,
     dropzone: { getRootProps, getInputProps, isDragActive },
     downloadAllFiles,
@@ -144,7 +144,7 @@ const PatientAttachments = () => {
           <AttachmentsBreadcrumbs />
         </Box>
         <Box display="flex" alignItems="center" flex="0 0 auto">
-          {process.env.GOOGLE_DRIVE_API_CLIENT_ID &&
+          {/* {process.env.GOOGLE_DRIVE_API_CLIENT_ID &&
             process.env.GOOGLE_DRIVE_API_KEY && (
               <GooglePicker
                 clientId={process.env.GOOGLE_DRIVE_API_CLIENT_ID}
@@ -159,7 +159,7 @@ const PatientAttachments = () => {
                   <DriveIcon src={GoogleDriveIcon} alt="Google Drive" />
                 </IconButton>
               </GooglePicker>
-            )}
+            )} */}
           <AddButton onClick={handleCreateFolderClick}>Create Folder</AddButton>
           <AddButton
             onClick={() => attachmentFileInputReference.current.click()}
