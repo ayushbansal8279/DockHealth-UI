c2a2fc7d9fb1ec96877651ab5dfeb3d7f1a6d7f7
Mateusz Pietrzak <mateusz.pietrzak@htdevelopers.com>
2022-09-29 13:23:09 +0200

    DHWP-2110: add option to maximize and minimize charts

--- src/app/views/analytics/ChartTail/ChartTail.jsx
+++ src/app/views/analytics/ChartTail/ChartTail.jsx
@@ -1,14 +1,33 @@
 import { Box } from '@material-ui/core';
-import React from 'react';
+import { MoreVert } from '@material-ui/icons';
+import React, { useState } from 'react';
+import OptionsMenu from 'components/common/OptionsMenu/OptionsMenu';
 import { Container, Title } from './styled';
 
 const ChartTail = props => {
   const { name, children } = props;
+  const [maximized, setMaximized] = useState(false);
 
   return (
-    <Container>
+    <Container maximized={maximized}>
       <Box display="flex" flexDirection="column" height="100%">
-        <Title>{name}</Title>
+        <Title>
+          {name}
+          <Box display="inline-block" margin="-16px -40px 0px 0px">
+            <OptionsMenu
+              options={[
+                {
+                  name: maximized ? 'Minimize' : 'Maximize',
+                  onClick: () => {
+                    setMaximized(!maximized);
+                  },
+                },
+              ]}
+            >
+              <MoreVert color="primary" />
+            </OptionsMenu>
+          </Box>
+        </Title>
         <Box flex={1}>{children}</Box>
       </Box>
     </Container>
