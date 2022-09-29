088681987f8da41829f1207c6bc30d8b33123d47
tomasz.nowakowski <tomasz.nowakowski@htdevelopers.com>
2022-09-29 12:38:01 +0200

    done

--- src/app/components/common/TextEditor/create-mention-entities.js
+++ src/app/components/common/TextEditor/create-mention-entities.js
@@ -56,6 +56,7 @@ export const createMentionEntitiesFromRawText = (
             inline: ['ins', 'links'],
           },
         },
+        preserveNewlines: true,
       })
     : convertToRaw(ContentState.createFromText(text));
 
