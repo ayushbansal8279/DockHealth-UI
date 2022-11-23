61ee891a3f7911e63af0ec9eab51ca6a123f984a
Nitin Gujral <nitin.gujral@conns.com>
2022-11-23 09:43:13 -0500

    support custom theme for tasks header

--- src/app/views/task-template/TaskTemplateView.jsx
+++ src/app/views/task-template/TaskTemplateView.jsx
@@ -90,6 +90,8 @@ const TaskTemplateView = () => {
   const isFetchingTaskTemplates = useSelector(isFetchingTaskTemplatesSelector);
   const taskTemplates = useSelector(taskTemplatesSelector);
   const userProfile = useSelector(userProfileSelector);
+  const currentOrganization = useSelector(selectedUserOrganizationSelector);
+
   const folders = useMemo(
     () =>
       taskTemplates?.filter(template => template.templateType === 'FOLDER') ||
@@ -112,6 +114,16 @@ const TaskTemplateView = () => {
     return differenceWith(eqBy(prop('identifier')), current, previous)?.[0]
       ?.identifier;
   }, [folders, previousFolders, previousTemplates, templates]);
+
+  const tasksHeaderTextTransformItem =
+    currentOrganization?.themeSettings?.find(
+      ({ name }) => name === 'tasks.header.textTransform',
+    ) || {};
+
+  const tasksHeaderTextColorItem =
+    currentOrganization?.themeSettings?.find(
+      ({ name }) => name === 'tasks.header.textColor',
+    ) || {};
 
   useEffect(() => {
     if (userProfile?.orgUserRole === 'GUEST') {
@@ -198,7 +210,7 @@ const TaskTemplateView = () => {
     <ColumnsConfigProvider
       initialColumns={{
         [TaskItemColumn.DESCRIPTION]: true,
-        [TaskItemColumn.SUBTASKS_COUNT]: true,
+        // [TaskItemColumn.SUBTASKS_COUNT]: true,
         [TaskItemColumn.WORKFLOW_STATUS]: true,
         [TaskItemColumn.ACTIVITY]: true,
         [TaskItemColumn.ASSIGNED]: true,
@@ -266,7 +278,12 @@ const TaskTemplateView = () => {
               </Grid>
             </SearchAndFilterContainer>
             <Spacing vertical={4} />
-            <TasksTemplatesHeader sort={sort} onSortChange={handleSortChange} />
+            <TasksTemplatesHeader
+              sort={sort}
+              onSortChange={handleSortChange}
+              tasksHeaderTextTransform={tasksHeaderTextTransformItem?.value}
+              tasksHeaderTextColor={tasksHeaderTextColorItem?.value}
+            />
             {!isFetchingTaskTemplates ? (
               <>
                 {currentSortMethodWithOrder(folders).map(template => (
