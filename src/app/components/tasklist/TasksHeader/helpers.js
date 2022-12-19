19d400429b7b30fb40f674524a2ef01fd6d44f52
Nitin Gujral <nitin.gujral@conns.com>
2022-12-18 21:13:00 -0500

    minor fix

--- src/app/components/tasklist/TasksHeader/helpers.js
+++ src/app/components/tasklist/TasksHeader/helpers.js
@@ -132,7 +132,7 @@ export const getTaskHeaderOptions = (
   ];
 
   const foundHeaderColumn = predefinedHeaderColumns.find(
-    c => c.identifier === column.identifier,
+    c => c.identifier === column?.identifier,
   );
 
   return { ...foundHeaderColumn, ...column };
