8cd28c0f4efc07cee4c5e8910442b9706f0fc236
Nitin Gujral <nitin.gujral@conns.com>
2022-12-18 21:22:13 -0500

    sort drop down options selectively

--- src/app/components/common/DropdownInput/helpers.js
+++ src/app/components/common/DropdownInput/helpers.js
@@ -2,17 +2,30 @@
 import React from 'react';
 import { SelectOption } from './styled';
 
-export function generateSelectOptions(options) {
-  return options
-    .sort((a, b) => a.localeCompare(b))
-    .map(({ value, label }) => {
-      return {
-        key: value,
-        label: isHovered => (
-          <SelectOption isActive={isHovered}>{label}</SelectOption>
-        ),
-        value,
-        displayLabel: label,
-      };
-    });
+export function generateSelectOptions(options, sortEnabled) {
+  if (sortEnabled) {
+    return options
+      .sort((a, b) => a.label?.localeCompare(b.label))
+      .map(({ value, label }) => {
+        return {
+          key: value,
+          label: isHovered => (
+            <SelectOption isActive={isHovered}>{label}</SelectOption>
+          ),
+          value,
+          displayLabel: label,
+        };
+      });
+  }
+  // eslint-disable-next-line sonarjs/no-identical-functions
+  return options.map(({ value, label }) => {
+    return {
+      key: value,
+      label: isHovered => (
+        <SelectOption isActive={isHovered}>{label}</SelectOption>
+      ),
+      value,
+      displayLabel: label,
+    };
+  });
 }
