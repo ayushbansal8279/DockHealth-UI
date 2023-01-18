7222981400f5d32a0d63b8c5349cb64fcbbd24b3
Nitin Gujral <nitin.gujral@conns.com>
2023-01-18 12:32:33 -0500

    minor cleanup

--- src/app/views/chat/alerts/ChatActivityAlertsToast.jsx
+++ src/app/views/chat/alerts/ChatActivityAlertsToast.jsx
@@ -42,11 +42,6 @@ const ChatActivityAlertsToast = ({
           isCleared={isCleared}
           topSpacing={topSpacing}
         >
-          {/* <ActivityAlertsItem
-            onClearAlert={() => setIsCleared(true)}
-            itemAlert={itemAlert}
-            withCrossIcon
-          /> */}
           <ChatActivityAlertsItem
             // onClearAlert={() => setIsCleared(true)}
             closeAlerts={() => setIsCleared(true)}
