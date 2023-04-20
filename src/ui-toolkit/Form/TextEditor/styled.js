d455b8fb0c719897bd8a72c9f271063c8a604c41
Mateusz Pietrzak <mateusz.pietrzak@htdevelopers.com>
2023-04-20 15:50:00 +0200

    DHWV-33: add mentions avatars

--- src/ui-toolkit/Form/TextEditor/styled.js
+++ src/ui-toolkit/Form/TextEditor/styled.js
@@ -33,3 +33,25 @@ export const Placeholder = styled(Box)`
   font: inherit;
   color: rgb(132, 146, 164);
 `
+
+export const Avatar = styled(Box)`
+  font-family: 'Roboto Condensed', sans-serif;
+  font-size: 12px;
+  font-weight: bold;
+  text-align: center;
+  line-height: 30px;
+  max-width: 32px;
+  max-height: 32px;
+  min-width: 32px;
+  min-height: 32px;
+  margin: 4px;
+  color: white;
+  background-color: ${({ $color }) => $color};
+  border: 1px solid white;
+  outline: 1px solid ${({ $color }) => $color};
+  border-radius: 100%;
+  
+  img {
+    border-radius: 100%
+  }
+`
