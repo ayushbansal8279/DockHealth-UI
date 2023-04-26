7640c3ce31ed3d9920446b61e58866516ebc170c
Nitin Gujral <nitin.gujral>
2023-04-26 09:12:40 -0400

    ensure logs are printed in build version

--- src/app/helpers/log.js
+++ src/app/helpers/log.js
@@ -1,6 +1,6 @@
 export const log = (message, ...rest) => {
-  if (import.meta.env.NODE_ENV !== 'production') {
-    // eslint-disable-next-line no-console
-    console.log(message, ...rest);
-  }
+  // if (import.meta.env.NODE_ENV !== 'production') {
+  // eslint-disable-next-line no-console
+  console.log(message, ...rest);
+  // }
 };
