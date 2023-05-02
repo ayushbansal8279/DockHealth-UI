2d199033402b384d1699d47baf5ae99f45344c23
zachdock <105669414+zachdock@users.noreply.github.com>
2023-05-01 23:30:18 -0400

    -added toolbar to search view

---
+++ src/app/views/global-search/GlobalSearchToolBar/GlobalSearchToolBar.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { Box } from '@material-ui/core';
+import InboxTips from 'components/tasklist/InboxTips/InboxTips';
+import { ViewType, getViewTypeFromQueryString } from 'helpers/view-type-helper';
+import { useLocation } from 'react-router-dom';
+import CustomizeToolbarButton from 'components/tasklist/CustomizeToolbarButton/CustomizeToolbarButton';
+import { checkIfUserIsOrganizationAdmin } from 'helpers/user-helper';
+import {
+  userProfileSelector,
+  selectedUserOrganizationSelector,
+} from 'selectors/user-selectors';
+import { useSelector } from 'react-redux';
+import { currentTaskListSelector } from 'selectors/task-list-selectors';
+import { isMemberAdmin } from 'helpers/list-members-helper';
+import { ToolbarContainer } from './styled';
+import TaskCustomFieldsModal from '../../../modal/customModals/TaskCustomFieldsModal';
+
+const GlobalSearchToolbar = ({ additionalOptions, children }) => {
+  const { search } = useLocation();
+  const [customFieldsModalOpened, setCustomFieldsModalOpened] = useState(false);
+  const currentUser = useSelector(userProfileSelector);
+  const currentOrganization = useSelector(selectedUserOrganizationSelector);
+  const isOrganizationAdmin = checkIfUserIsOrganizationAdmin(currentUser);
+  const taskList = useSelector(currentTaskListSelector);
+  const { restrictCustomization } = taskList || {};
+  const currentUserMember = taskList?.listUsers.find(
+    u => u.identifier === currentUser?.identifier,
+  );
+  const isListAdmin = isMemberAdmin(currentUserMember);
+  const viewType = getViewTypeFromQueryString(search);
+  const restrictCustomizationFeatures = restrictCustomization && !isListAdmin;
+  const iconColorFilterActiveItem =
+    currentOrganization?.themeSettings?.find(
+      ({ name }) => name === 'icon.active.filter',
+    ) || {};
+
+  return (
+    <ToolbarContainer>
+      <Box display="flex" flex={1} justifyContent="flex-start">
+        {viewType === ViewType.LIST_VIEW && (
+          <>
+            <Box mx={0.5} />
+            <CustomizeToolbarButton
+              openCustomFieldModal={() => setCustomFieldsModalOpened(true)}
+              additionalOptions={additionalOptions}
+              disableButton={restrictCustomizationFeatures}
+              iconColorFilterActive={iconColorFilterActiveItem?.value}
+            />
+            <Box mx={0.5} />
+            <TaskCustomFieldsModal
+              opened={customFieldsModalOpened}
+              handleClose={() => setCustomFieldsModalOpened(false)}
+              taskListIdentifier={taskList?.taskListIdentifier}
+              isOrganizationAdmin={isOrganizationAdmin}
+              isListAdmin={isListAdmin}
+            />
+            {taskList?.listType === 'INBOX' && (
+              <>
+                <InboxTips />
+              </>
+            )}
+          </>
+        )}
+      </Box>
+      {children}
+    </ToolbarContainer>
+  );
+};
+
+export default GlobalSearchToolbar;
