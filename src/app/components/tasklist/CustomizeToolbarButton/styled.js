14f0a50a86003eb2f0cb950dcc8ace44de15defe
Nitin Gujral <nitin.gujral>
2023-05-02 23:52:59 -0400

    ensure the customize toolbar has consistent height and width

--- src/app/components/tasklist/CustomizeToolbarButton/styled.js
+++ src/app/components/tasklist/CustomizeToolbarButton/styled.js
@@ -12,12 +12,13 @@ export const CustomizeImg = styled.img`
 
 export const PopoverContainer = styled.div`
   background-color: ${palette.white};
-  min-width: 233px;
-  max-width: 500px;
+  min-width: 300px;
+  max-width: 300px;
   box-shadow: 0px 4px 11px grey;
-  max-height: 800px;
+  min-height: 600px;
+  max-height: 600px;
   overflow-x: hidden;
-  overflow-y: auto;
+  overflow-y: visible;
 `;
 
 export const PlusIcon = styled.div`
