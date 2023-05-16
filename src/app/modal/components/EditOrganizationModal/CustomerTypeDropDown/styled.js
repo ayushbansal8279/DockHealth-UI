cf8488f56e7a362b6a402975f2848388d09282ae
zachdock <105669414+zachdock@users.noreply.github.com>
2023-05-16 17:34:19 -0400

    created clienttype drop down organization modal

---
+++ src/app/modal/components/EditOrganizationModal/CustomerTypeDropDown/styled.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+import Select from 'components/common/Select/Select';
+import palette from 'styles/palette';
+
+export const DropdownBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  overflow: hidden;
+  position: relative;
+  cursor: pointer;
+  color: ${palette.paleBlue};
+`;
+
+export const DropdownSelect = styled(Select)`
+  & .MuiSelect-root {
+    padding: 0px;
+    background: white;
+  }
+  & .MuiSelect-filled.MuiSelect-filled {
+    padding-right: 0;
+  }
+  & .MuiSelect-root:before {
+    display: none;
+  }
+  & .MuiSelect-select {
+    padding: 0px;
+    background-color: cream
+  }
+`;
+
+export const PlaceholderContainer = styled.div`
+  top: -3px;
+  position: absolute;
+  left: 0px;
+  z-index: 9;
+  pointer-events: none;
+`;
+
+export const ColorIndicator = styled.div`
+  display: block;
+  width: 6px;
+  height: 100%;
+  background: ${(props) => props.color};
+  position: absolute;
+  left: 0px;
+`;
