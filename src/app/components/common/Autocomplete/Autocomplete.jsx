2df3ccaabd9a2e5a8f3b5f18062e2dbd8fa66dfe
Nitin Gujral <nitin.gujral>
2023-06-25 17:22:54 -0400

    address some react warnings

--- src/app/components/common/Autocomplete/Autocomplete.jsx
+++ src/app/components/common/Autocomplete/Autocomplete.jsx
@@ -85,7 +85,7 @@ const Autocomplete = ({
   value,
   InputProps,
   disableClearable,
-  getOptionSelected,
+  // getOptionSelected,
   limitTags,
 }) => {
   const textFieldReference = useRef(null);
@@ -165,7 +165,7 @@ const Autocomplete = ({
       renderOption={renderOption}
       renderTags={renderTags}
       value={value}
-      getOptionSelected={getOptionSelected}
+      // getOptionSelected={getOptionSelected}
       disableClearable={disableClearable}
       limitTags={limitTags}
     />
