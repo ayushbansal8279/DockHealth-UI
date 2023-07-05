7372d16a1e759ba831301fcf11be2070055c15e6
zachdock <105669414+zachdock@users.noreply.github.com>
2023-07-05 09:47:16 -0400

    updates to api and users, patients

--- src/app/views/custom-fields/profiles-and-custom-fields/ProfilesView.jsx
+++ src/app/views/custom-fields/profiles-and-custom-fields/ProfilesView.jsx
@@ -85,6 +85,10 @@ const ProfilesAndCustomFieldsView = () => {
 
   const onConfigureCustomFields = useCallback(
     ({ row: { name, identifier } }) => {
+      if (name.toLowerCase() === 'users' || name.toLowerCase() === 'patients') {
+        history.push(`/settings/custom-fields/${name.toLowerCase()}`);
+        return;
+      }
       history.push(
         `/settings/custom-fields/${name.toLowerCase()}/${identifier}`,
       );
