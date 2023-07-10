f12b450e5b5fd58784f487ef4a3597e9715b45b8
Nitin Gujral <nitin.gujral>
2023-07-10 17:39:11 -0400

    fix issue with last merge

--- src/app/components/patients/CustomProfilesList/CustomProfileList.jsx
+++ src/app/components/patients/CustomProfilesList/CustomProfileList.jsx
@@ -1,44 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import DataGrid, { Data } from 'ui-toolkit/Composite/DataGrid';
-import { useParams } from "react-router-dom";
-import { setCurrentUserGroup, unsetCurrentUserGroup } from "actions/user-groups-actions";
-import { Box, Stack } from "@mui/material";
-import { getAllProfiles } from "api/profile-api";
-import { getAllProfileFieldTypes } from "api/profile-type-field-api";
-import { showGlobalErrorAlert } from "alert/actions";
-import LayoutHeader from "components/template/LayoutHeader/LayoutHeader";
-import OptionsMenu from "components/common/OptionsMenu/OptionsMenu";
-import MoreVert from "@mui/icons-material/MoreVert";
-import ViewLayout from "components/template/ViewLayout/ViewLayout";
-import ProfileDrawer from "components/patients/CustomProfilesList/ProfileDrawer";
-import { Add as AddIcon } from "@mui/icons-material";
-import AdornedButton from "components/common/AdornedButton/AdornedButton";
-import { useDispatch, useSelector } from 'react-redux';
-import { getCurrentUserGroupDetailsSelector } from 'selectors/user-groups-selectors';
 import { useParams, useHistory, useLocation } from 'react-router-dom';
-import { getUserGroupIdentifierByUrlParameter } from 'helpers/user-groups-helper';
-import {
-  setCurrentUserGroup,
-  unsetCurrentUserGroup,
-} from 'actions/user-groups-actions';
-import Drawer from 'ui-toolkit/Navigation/Drawer/Drawer';
-import Input from 'ui-toolkit/Form_v2/Input';
-import { Box, IconButton, Stack } from '@mui/material';
-import * as CustomFieldsApi from 'api/custom-fields-api';
+import { Box, Stack } from '@mui/material';
+import { getAllProfiles } from 'api/profile-api';
+import { getAllProfileFieldTypes } from 'api/profile-type-field-api';
 import { showGlobalErrorAlert } from 'alert/actions';
 import LayoutHeader from 'components/template/LayoutHeader/LayoutHeader';
 import OptionsMenu from 'components/common/OptionsMenu/OptionsMenu';
 import MoreVert from '@mui/icons-material/MoreVert';
 import ViewLayout from 'components/template/ViewLayout/ViewLayout';
+import ProfileDrawer from 'components/patients/CustomProfilesList/ProfileDrawer';
+import { Add as AddIcon } from '@mui/icons-material';
+import AdornedButton from 'components/common/AdornedButton/AdornedButton';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserGroupIdentifierByUrlParameter } from 'helpers/user-groups-helper';
 import {
-  ContentWrapper,
-  MoreActinsWrapper,
-  StickyHeader,
-  TitleName,
-} from 'components/patients/PatientDrawer/styled';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import CloseIcon from '@mui/icons-material/Close';
-import Fieldset from 'ui-toolkit/Form_v2/Fieldset';
+  setCurrentUserGroup,
+  unsetCurrentUserGroup,
+} from 'actions/user-groups-actions';
 
 const CustomProfileList = () => {
   const dispatch = useDispatch();
@@ -54,10 +33,13 @@ const CustomProfileList = () => {
     };
   }, [dispatch, groupIdentifier]);
 
-  const { users } = useSelector(getCurrentUserGroupDetailsSelector) || {};
-
   const location = useLocation();
   const history = useHistory();
+
+  const [open, setOpen] = useState(null);
+
+  const [customFields, setCustomFields] = useState([]);
+  const [profiles, setProfiles] = useState([]);
 
   const handleRecordClick = (event, { id }) => {
     setOpen(id);
@@ -67,12 +49,7 @@ const CustomProfileList = () => {
   const handleClose = () => {
     setOpen(null);
   };
-
-  const [open, setOpen] = useState(null);
-
-  const [customFields, setCustomFields] = useState([]);
-  const [profiles, setProfiles] = useState([]);
-
+  
   const fetchUserCustomFields = () => {
     getAllProfileFieldTypes(profileIdentifier)
       .then((data) => {
@@ -129,25 +106,22 @@ const CustomProfileList = () => {
         />
         <Stack direction="row-reverse" sx={{ m: "16px 32px" }}>
           <Box display="flex" alignItems="center">
-              <Box m={1} />
-              <AdornedButton
-                adornment={<AddIcon />}
-                onClick={handleProfileAddClick}
-              >
-                ADD A PROVIDER
-              </AdornedButton>
+            <Box m={1} />
+            <AdornedButton
+              adornment={<AddIcon />}
+              onClick={handleProfileAddClick}
+            >
+              ADD A PROVIDER
+            </AdornedButton>
           </Box>
         </Stack>
-        <DataGrid
-          dataset={profiles}
-          onRecordClick={handleRecordClick}
-        >
-          {customFields.map(field => (
+        <DataGrid dataset={profiles} onRecordClick={handleRecordClick}>
+          {customFields.map((field) => (
             <Data
               name={field.name}
               value={(data) => {
-                const record = data.fields
-                  .find(({ profileTypeField }) =>
+                const record = data.fields.find(
+                  ({ profileTypeField }) =>
                     field.identifier === profileTypeField.identifier);
 
                 if (record) {
@@ -157,15 +131,14 @@ const CustomProfileList = () => {
                     case "PICK_LIST":
                       return record.values[0]?.customFieldOption.name;
                     default:
-                      return ""
+                      return '';
                   }
                 }
 
-                return "";
+                return '';
               }}
             />
           ))}
-
         </DataGrid>
       </ViewLayout>
     </>
