5a5d7af405efa7f47520f70c655e389dc9612080
Nitin Gujral <nitin.gujral>
2023-07-11 05:32:13 -0400

    minor style updates for popovers

--- src/app/components/task-drawer/StartDateSection/styled.js
+++ src/app/components/task-drawer/StartDateSection/styled.js
@@ -60,15 +60,13 @@ export const StyledButton = styled.button`
 `;
 
 export const StyledPopover = styled(Popover)`
-  &&& {
-    &.MuiPopover-paper {
-      border: none;
-      box-shadow: none;
-      width: ${({ width }) => width};
-      overflow: visible;
-    }
-    .MuiBackdrop-root {
-      opacity: 0 !important;
-    }
+  .MuiPopover-paper {
+    border: none;
+    box-shadow: none;
+    width: ${({ width }) => width};
+    overflow: visible;
+  }
+  .MuiBackdrop-root {
+    opacity: 0 !important;
   }
 `;
