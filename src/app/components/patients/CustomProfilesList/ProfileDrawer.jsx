2641ffb8609a8ef138531124df3043708bc38524
Mateusz Pietrzak <mateusz.pietrzak@htdevelopers.com>
2023-07-17 14:01:09 +0200

    When adding a custom profile, close the drawer and update the list with newly added profile Include a Search box similar to ones on the All Patients view On the profiles listing view update the header to reflect the specific Profile Type listing being viewed

--- src/app/components/patients/CustomProfilesList/ProfileDrawer.jsx
+++ src/app/components/patients/CustomProfilesList/ProfileDrawer.jsx
@@ -72,6 +72,7 @@ const ProfileDrawer = ({
         },
       });
     }
+    onClose();
   };
 
   const handleCollapse = () => {
--- src/app/ui-toolkit/Composite/DataGrid/index.js
+++ src/app/ui-toolkit/Composite/DataGrid/index.js
@@ -1,2 +1,4 @@
+export * from './utils';
+// eslint-disable-next-line no-restricted-exports
 export { default } from './DataGrid';
 export { default as Data } from './Data';
