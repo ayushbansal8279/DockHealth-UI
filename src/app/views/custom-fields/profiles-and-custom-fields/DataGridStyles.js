487af424c532ef80c31a5c99bb03a08fc3ca9247
Nitin Gujral <nitin.gujral>
2023-09-02 13:48:08 -0400

    updated header styles for data grid

--- src/app/views/custom-fields/profiles-and-custom-fields/DataGridStyles.js
+++ src/app/views/custom-fields/profiles-and-custom-fields/DataGridStyles.js
@@ -45,6 +45,10 @@ export const StyledDataGrid = styled(DataGrid)`
     margin-left: -15px;
   }
 
+  .MuiDataGrid-columnHeaderTitleContainerContent {
+    font-weight: ${fontWeights.regularPlus} !important;
+  }
+
   &.MuiDataGrid-columnHeader {
     &--sorted {
       color: ${palette.brightBlue};
@@ -52,12 +56,6 @@ export const StyledDataGrid = styled(DataGrid)`
     &:focus {
       outline: none !important;
     }
-  }
-
-  &.MuiDataGrid-colCellTitle {
-    color: #3d4858;
-    font-family: 'Roboto Condensed', sans-serif;
-    font-size: 0.875rem !important;
   }
 
   &.MuiDataGrid-columnHeaderWrapper {
