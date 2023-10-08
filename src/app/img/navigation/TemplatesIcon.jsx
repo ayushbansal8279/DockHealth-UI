4b83dc97a49f29fbf8615f9b66f788e7a157abb6
Nitin Gujral <nitin.gujral>
2023-10-08 17:34:11 -0400

    updated navigation icons

--- src/app/img/navigation/TemplatesIcon.jsx
+++ src/app/img/navigation/TemplatesIcon.jsx
@@ -4,12 +4,30 @@ export default ({ size = 25 }) => (
   <svg
     width={size}
     height={size * 0.75}
-    viewBox="0 0 25 19"
+    viewBox="0 0 20 20"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <path d="M23 12L12.5 7L2 12L12.5 17.5L23 12Z" stroke="currentColor" />
-    <path d="M23 9L12.5 4L2 9L12.5 14.5L23 9Z" stroke="currentColor" />
-    <path d="M23 6L12.5 1L2 6L12.5 11.5L23 6Z" stroke="currentColor" />
+    <path
+      d="M19 12.4288L10 8.14307L1 12.4288L10 17.1431L19 12.4288Z"
+      stroke="currentColor"
+      strokeWidth="1.28571"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M19 9.85603L10 5.57031L1 9.85603L10 14.5703L19 9.85603Z"
+      stroke="currentColor"
+      strokeWidth="1.28571"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M19 7.28571L10 3L1 7.28571L10 12L19 7.28571Z"
+      stroke="currentColor"
+      strokeWidth="1.28571"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
   </svg>
 );
