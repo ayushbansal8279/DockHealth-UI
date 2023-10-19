76d454a9c0eaf93ebd237a73a535502c277d6523
Mateusz Pietrzak <mateusz.pietrzak@htdevelopers.com>
2023-10-19 12:42:56 +0200

    DHWP-2568: Fix redirect when user is not logged in

--- src/app/routing/SecuredRoute.jsx
+++ src/app/routing/SecuredRoute.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import {
   isFetchingProfileSelector,
   userProfileSelector,
 } from 'selectors/user-selectors';
 import { Route, Redirect } from 'react-router-dom';
-import { HOME_PATH } from 'routing/helpers/paths';
+import { DEFAULT_REDIRECT_PATH, HOME_PATH } from 'routing/helpers/paths';
+import { isAuthenticated } from 'api/user-auth-api';
+
+function LazyRedirect() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    isAuthenticated().then((authenticated) =>
+      setIsLoggedIn(authenticated.isLoggedIn),
+    );
+  }, []);
+
+  return isLoggedIn ? (
+    <Redirect to={HOME_PATH} />
+  ) : (
+    <Redirect to={DEFAULT_REDIRECT_PATH} />
+  );
+}
 
 const SecureRoute = ({ required = [], ...props }) => {
   const { orgUserPermissions } = useSelector(userProfileSelector);
@@ -23,7 +40,11 @@ const SecureRoute = ({ required = [], ...props }) => {
     return <Route {...props} />;
   }
 
-  return <Redirect to={HOME_PATH} />;
+  return (
+    <Suspense fallback={null}>
+      <LazyRedirect />
+    </Suspense>
+  );
 };
 
 export default SecureRoute;
