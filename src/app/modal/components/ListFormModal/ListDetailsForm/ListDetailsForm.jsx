4456e9139b8f58e0096fdec971c6bb5180179090
Aditya Chaurasia <chaurasiaaaditya755.com>
2023-11-28 22:28:57 +0530

    added validation for existing List Name

--- src/app/modal/components/ListFormModal/ListDetailsForm/ListDetailsForm.jsx
+++ src/app/modal/components/ListFormModal/ListDetailsForm/ListDetailsForm.jsx
@@ -15,6 +15,7 @@ import { createTaskListPath } from 'routing/helpers/paths';
 import { Title, ButtonWrapper, Header } from '../styled';
 import messages from './messages';
 import { CheckboxContainer, CheckboxDescription, StyledForm } from './styled';
+import { taskListsSelector } from 'selectors/task-list-selectors';
 
 const validateListName = (value) => {
   if (!value || ![...value]?.filter((char) => char !== ' ').length > 0) {
@@ -104,6 +105,19 @@ const ListDetailsForm = ({
 
   const shareTaskAvailable = useSelector(userHasShareTaskFeatureSelector);
 
+  const taskLists = useSelector(taskListsSelector);
+  const validateExistingListName = (value) => {
+    const activeLists = [];
+    taskLists.forEach((task) => {
+      activeLists.push(task.listName);
+    });
+
+    if (activeLists.includes(value)) {
+      return 'List Name already exists';
+    }
+    return validateListName(value);
+  };
+
   return (
     <StyledForm
       onSubmit={(event) =>
@@ -135,7 +149,7 @@ const ListDetailsForm = ({
               name="listName"
               required
               placeholder="Add your list name here"
-              validate={validateListName}
+              validate={validateExistingListName}
             />
             <Spacing vertical={4} />
             <FormInput
