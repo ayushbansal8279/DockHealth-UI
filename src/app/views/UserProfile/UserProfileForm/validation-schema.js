dd62156b1ee56ed5bfd42a203f14656ffcf6ebd9
Nitin Gujral <nitin.gujral>
2023-12-07 09:09:37 -0500

    disabled phone no validation for additional number since it is being required currently

--- src/app/views/UserProfile/UserProfileForm/validation-schema.js
+++ src/app/views/UserProfile/UserProfileForm/validation-schema.js
@@ -22,7 +22,7 @@ export default object().shape({
       }
       return matchEmptyNumber(value);
     })
-    .phone(null, null, MASK_MESSAGE)
+    // .phone(null, false, MASK_MESSAGE)
     // .matches(PHONE_MASK, MASK_MESSAGE)
     .notRequired(),
 });
