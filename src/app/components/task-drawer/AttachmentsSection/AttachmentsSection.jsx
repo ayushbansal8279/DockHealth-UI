6a6bcf35e8c45a7335ba77c400cacb699f745910
Aditya Chaurasia <chaurasiaaaditya755.com>
2023-12-14 02:49:29 +0530

    updated task drawer for Attachments

--- src/app/components/task-drawer/AttachmentsSection/AttachmentsSection.jsx
+++ src/app/components/task-drawer/AttachmentsSection/AttachmentsSection.jsx
@@ -14,6 +14,7 @@ import {
   AttachmentsContainer,
   AttachmentFileInput,
   DownloadAllLink,
+  Title,
 } from './styled';
 
 const AttachmentsSection = ({ selectedTask, disabled = false }) => {
@@ -50,9 +51,7 @@ const AttachmentsSection = ({ selectedTask, disabled = false }) => {
       <Grid container>
         <Grid item xs={12}>
           <Spacing vertical={3} />
-          <RobotoTypography condensed variant="h5" color="inherit">
-            ATTACHMENTS
-          </RobotoTypography>
+          <Title>Files</Title>
         </Grid>
         <RobotoTypography condensed variant="h4" color="inherit">
           <Spacing vertical={2} />
