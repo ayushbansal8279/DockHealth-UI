4fac82ada577f191375bf5ca57101e5a22bc3252
Aditya Chaurasia <chaurasiaaaditya755.com>
2023-12-15 23:54:08 +0530

    Fixed Cursor issue in email Body which was going back to Initial Position

--- src/app/components/common/RichTextEditor/RichTextEditor.jsx
+++ src/app/components/common/RichTextEditor/RichTextEditor.jsx
@@ -192,7 +192,7 @@ const RichTextEditor = React.forwardRef(
       if (reset) {
         setEditorState(processMarkdownValue(initialValue || '', mentions));
       }
-    }, [reset, initialValue, mentions]);
+    }, [reset, mentions]);
 
     const [editor, setEditor] = useState(null);
     // const [initControls, setInitControls] = useState(null);
--- src/app/modal/components/SendMessageFromTaskModals/SendEmailFromTaskModal/SendEmailFromTaskModal.jsx
+++ src/app/modal/components/SendMessageFromTaskModals/SendEmailFromTaskModal/SendEmailFromTaskModal.jsx
@@ -100,7 +100,7 @@ const SendEmailFromTaskModal = () => {
     setIsTaskCommentsIncluded(false);
     setIsTaskDescriptionIncluded(false);
     setIsTaskDetailsIncluded(false);
-    setValueReset(true);
+    setValueReset(false);
     setDetailsState('');
   }, []);
 
