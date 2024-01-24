93479ba4433ef9cc5af0c90662f5332cc8a2c4b1
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2024-01-24 22:34:16 +0530

    removed unused console log

--- src/app/components/tasklist/TasksGroup/TasksGroup.jsx
+++ src/app/components/tasklist/TasksGroup/TasksGroup.jsx
@@ -285,7 +285,6 @@ const TasksGroup = ({
           : columns
               .filter((f) => f.isChecked)
               .reduce((accumulator, column) => {
-                console.log('columnWidth', column, column.columnWidth);
                 return accumulator + column.columnWidth;
               }, 0)
       }
