560de81e9ff1796636a4af10c35dc7206f405a05
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2024-01-31 22:18:02 +0530

    added tooltip for complete and incomplete circle

--- src/app/components/task/StandardTaskItem/TaskItem.jsx
+++ src/app/components/task/StandardTaskItem/TaskItem.jsx
@@ -117,6 +117,7 @@ import {
 import TaskItemText from './customFieldsTaskItemComponents/TaskItemText/TaskItemText';
 import TaskItemDropdown from './customFieldsTaskItemComponents/TaskItemDropdown/TaskItemDropdown';
 import TaskItemDate from './customFieldsTaskItemComponents/TaskItemDate';
+import Tooltip from 'components/common/Tooltip/Tooltip';
 
 const { DISABLED, READ_ONLY } = SINGLE_TASK_RESTRICTIONS_OPTIONS;
 
@@ -767,7 +768,11 @@ const TaskItem = React.memo(
                   />
                 )}
               <Box ml="10px" />
-              <CircleIcon
+              <Tooltip
+                placement="top"
+                title={isCompleted ? 'Mark incomplete'  : 'Complete task'}
+              >
+                <CircleIcon
                 src={isCompleted ? CircleCompleted : Circle}
                 isClickable={
                   !isTaskStatusTogglingDisabled &&
@@ -782,6 +787,7 @@ const TaskItem = React.memo(
                     : () => {}
                 }
               />
+              </Tooltip>
             </ActionIconsContainer>
             {content}
             <TaskScrollVericleLine>&nbsp;</TaskScrollVericleLine>
@@ -1062,7 +1068,11 @@ const TaskItem = React.memo(
                         options: [
                           { identifier: 'HIGH', name: 'High', color: 'red' },
                           { identifier: 'NONE', name: 'No Priority' },
-                          { identifier: 'MEDIUM', name: 'Medium', color: '#fd8914'},
+                          {
+                            identifier: 'MEDIUM',
+                            name: 'Medium',
+                            color: '#fd8914',
+                          },
                           { identifier: 'LOW', name: 'Low' },
                         ],
                         displayOptions: [],
@@ -1361,7 +1371,7 @@ const TaskItem = React.memo(
                           identifier === TaskItemColumn.START_DATE,
                       )?.columnWidth
                     }
-                    paddingLeft='10px'
+                    paddingLeft="10px"
                     paddingRight="tiny"
                     justify="flex-start"
                     onContextMenu={(event) => {
@@ -1369,7 +1379,7 @@ const TaskItem = React.memo(
                     }}
                     order={getColumnOrder(TaskItemColumn.START_DATE)}
                     onMouseEnter={() => setIsDateHover(true)}
-                      onMouseLeave={() => setIsDateHover(false)}
+                    onMouseLeave={() => setIsDateHover(false)}
                   >
                     <TaskItemStartDate
                       task={task}
@@ -1394,7 +1404,7 @@ const TaskItem = React.memo(
                             identifier === TaskItemColumn.DUE_DATE,
                         )?.columnWidth
                       }
-                      paddingLeft='10px'
+                      paddingLeft="10px"
                       paddingRight="tiny"
                       justify="flex-start"
                       onContextMenu={(event) => {
