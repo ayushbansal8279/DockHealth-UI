68e953dd7706f4aaf2abc48a2fe1d2dec1fe0d35
tejas <tejas.patil@rearckindia.com>
2024-02-09 22:39:01 +0530

    Fixed the opening of subtasks on Staging

--- src/app/components/task/StandardTaskItem/StandardTaskItem.jsx
+++ src/app/components/task/StandardTaskItem/StandardTaskItem.jsx
@@ -91,7 +91,7 @@ const Task = React.memo(
           dispatch(loadSubTasks(task));
         }
         setAreSubtasksOpen(areOpen);
-        collapse.set(taskIdentifier, areOpen);
+        collapse.set(taskIdentifier, !areOpen);
       },
       [
         subTasksCount,
