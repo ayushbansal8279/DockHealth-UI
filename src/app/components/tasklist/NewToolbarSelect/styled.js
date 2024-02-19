9612021db4dd5af62d5a1518a8951a5bde1a97a6
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2024-02-19 21:17:59 +0530

    added the design for Task visibility CTA

---
+++ src/app/components/tasklist/NewToolbarSelect/styled.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+import palette from 'styles/palette';
+import { Select as MuiSelect } from '@mui/material';
+
+export const Select = styled(MuiSelect)`
+  display: flex;
+  align-items: center;
+  background: ${(props) =>
+    props.isOpen ? palette.newBrightBlue : palette.newDarkBlue};
+  color: ${palette.white};
+  height: 40px;
+  width: auto;
+  border-radius: 4px;
+
+  &:hover {
+    background: ${(props) =>
+      props.isOpen ? palette.cornFlowerBlue : palette.purpleNavy};
+  }
+
+  & .switchIcon > path {
+    fill: ${(props) => props.iconcoloractive ?? palette.white};
+  }
+`;
+
+export const SelectWrapper = styled.div`
+  @media print {
+    display: none;
+  }
+`;
+
+export const SelectIcon = styled.span`
+  border-right: 2px solid ${palette.white};
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding-right: 10px;
+  filter: brightness(0) invert(1);
+`;
