202f01f73be51d273b84d8ea84b7ac09ff7774ff
tejas <tejas.patil@rearckindia.com>
2024-02-20 03:20:47 +0530

    fixed tasklist sticky column width issue

--- src/app/components/task-template/TaskTemplateGroupHeader/TaskTemplateGroupHeader.jsx
+++ src/app/components/task-template/TaskTemplateGroupHeader/TaskTemplateGroupHeader.jsx
@@ -658,7 +658,7 @@ const TaskTemplateGroupHeader = ({
     (content, order, width) => {
       if (order !== 0) return content;
       return (
-        <StickyColumnContainer>
+        <StickyColumnContainer customWidthExists={true}>
           <StickyMainTaskItemCell
             customWidthExists
             backgroundColor={pageBackground}
