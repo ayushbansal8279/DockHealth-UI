a4f113526ef7179efb3df58b6b8ed6da4f8a907d
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2024-03-06 19:46:35 +0530

    added quick add sub task

--- src/app/views/list-details/VirtualTaskList/VirtualSegment/VSubtask/VSubtask.tsx
+++ src/app/views/list-details/VirtualTaskList/VirtualSegment/VSubtask/VSubtask.tsx
@@ -1,13 +1,16 @@
-import React, { ForwardedRef, forwardRef } from 'react';
+import React, { ForwardedRef, forwardRef, useEffect, useState } from 'react';
 import { Segment } from 'views/list-details/modules/Virtualized';
 import StandardTaskItem from 'components/task/StandardTaskItem/StandardTaskItem';
 import { getSubtaskStylingLink } from 'components/task/StandardTaskItem/helpers';
+import QuickAddSubtask from '@/app/components/task/StandardTaskItem/QuickAddSubtask';
 import {
   Draggable,
   DraggableProvided,
   DraggableStateSnapshot,
 } from 'react-beautiful-dnd';
 import * as Sc from './styled';
+import { useSelector } from 'react-redux';
+import { taskLookupSelector } from '@/app/selectors/task-details-selectors';
 
 export interface Props extends Segment {
   task: any;
@@ -26,6 +29,23 @@ function VSubtask(
     }
     return false;
   };
+
+  const [subtaskQuickAddOpen, setSubtaskQuickAddOpen] = useState(false);
+
+  const pulledTask = useSelector((state) => {
+    // @ts-ignore
+    return taskLookupSelector(state, origin, metadata.parent.id);
+  });
+
+  const parentTask = pulledTask;
+
+  useEffect(() => {
+    setSubtaskQuickAddOpen(parentTask?.subtaskQuickAddOpen);
+  }, [parentTask]);
+
+  // @ts-ignore
+  const noOfSubtask = metadata.parent?.children.length - 1;
+
   return (
     <Draggable
       draggableId={metadata.id}
@@ -33,22 +53,36 @@ function VSubtask(
       key={metadata.id}
     >
       {(provided: DraggableProvided, snapshot: DraggableStateSnapshot) => (
-        <Sc.VSubtask
-          ref={ref}
-          {...register}
-          $subitem={metadata.level > 1}
-          isWorkflowSubtask={!!task}
-        >
-          {getSubtaskStylingLink(isLast())}
-          <StandardTaskItem
-            // @ts-ignore
-            taskIdentifier={metadata.id}
-            draggableProvided={provided}
-            isDraggable
-            isDragging={snapshot.isDragging}
+        <>
+          <Sc.VSubtask
+            ref={ref}
+            {...register}
+            $subitem={metadata.level > 1}
             isWorkflowSubtask={!!task}
-          />
-        </Sc.VSubtask>
+          >
+            {getSubtaskStylingLink(isLast())}
+            <StandardTaskItem
+              // @ts-ignore
+              taskIdentifier={metadata.id}
+              draggableProvided={provided}
+              isDraggable
+              isDragging={snapshot.isDragging}
+              isWorkflowSubtask={!!task}
+              isSubtask
+            />
+          </Sc.VSubtask>
+          {metadata.sameLevelIndex === noOfSubtask && subtaskQuickAddOpen && (
+            <Sc.QuickAddContainer>
+              <QuickAddSubtask
+                taskListIdentifier={parentTask.taskList.taskListIdentifier}
+                parentTaskIdentifier={parentTask.identifier}
+                isWorkflowSubtask={!!task}
+                // onFocus={handleQuickAddOnFocus}
+                // origin={origin}
+              />
+            </Sc.QuickAddContainer>
+          )}
+        </>
       )}
     </Draggable>
   );
--- src/app/views/list-details/VirtualTaskList/VirtualSegment/VSubtask/styled.ts
+++ src/app/views/list-details/VirtualTaskList/VirtualSegment/VSubtask/styled.ts
@@ -1,4 +1,5 @@
 import styled from '@mui/styled-engine';
+import palette, { featurePalette, typography } from 'styles/palette';
 
 export const VSubtask = styled('div')`
   position: relative;
@@ -11,4 +12,15 @@ export const VSubtask = styled('div')`
     isWorkflowSubtask ? '105px;' : '106px'};
   margin-bottom: -5px;
   margin-top: -1px;
+`;
+
+export const QuickAddContainer = styled('div')`
+  border-left: 1px solid ${palette.coolGrey3};
+  width: 70%;
+  margin-left: ${({ isWorkflowSubtask }) =>
+    isWorkflowSubtask ? '107px;' : '106px'};
+
+  &:hover {
+    border-left: 1px solid ${palette.coolGrey2};
+  }
 `;
