04313458c27cbedf29b637073b0d720a281c3b95
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2024-03-11 18:22:39 +0530

    added outline for chips in label

--- src/app/components/task-drawer/LabelsSection/LabelsSection.jsx
+++ src/app/components/task-drawer/LabelsSection/LabelsSection.jsx
@@ -198,7 +198,7 @@ const LabelsSection = ({ selectedTask, onTaskUpdate }) => {
     () =>
       selectedLabels.map((option) => (
         <Chip
-          // variant='outlined'
+          variant='outlined'
           key={option.labelIdentifier}
           onDelete={() => {
             setSelectedLabels((state) =>
