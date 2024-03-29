9b43d8085498abd073bb77ead06e3f2bb7f26e9b
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2024-03-29 15:31:36 +0530

    Added Tooltip on Task Drawer Header

--- src/app/components/task-drawer/TopSection/TopSection.jsx
+++ src/app/components/task-drawer/TopSection/TopSection.jsx
@@ -29,14 +29,10 @@ import EmailIconActive from 'img/email-new-icon-active.svg';
 import MobileIcon from 'img/mobile-new-icon.svg';
 import MobileIconActive from 'img/mobile-new-icon-active.svg';
 import Spacing from 'components/common/Spacing';
-import {
-  IconContainer,
-  Title,
-  CompletedByTitle,
-  CompleteAge,
-} from './styled';
+import { IconContainer, Title, CompletedByTitle, CompleteAge } from './styled';
 import initializeTaskDrawerTopSectionHooks from './hooks';
 import moment from 'moment';
+import Tooltip from 'components/common/Tooltip/Tooltip';
 
 const { DISABLED } = SINGLE_TASK_RESTRICTIONS_OPTIONS;
 
@@ -265,54 +261,60 @@ const TopSection = ({
         </Box>
         <Box display="flex">
           {handleCopyLink && (
-            <IconContainer
-              onClick={() => {
-                handleCopyLink();
-                setShareActive(true);
-                setTimeout(() => {
-                  setShareActive(false);
-                }, 2000);
-              }}
-            >
-              <img
-                src={!isShareActive ? ShareLinkIcon : ShareLinkIconActive}
-                fontSize="small"
-                color="primary"
-                alt="Copy the Task link"
-              />
-            </IconContainer>
+            <Tooltip placement="left" title={'Copy Task Link'}>
+              <IconContainer
+                onClick={() => {
+                  handleCopyLink();
+                  setShareActive(true);
+                  setTimeout(() => {
+                    setShareActive(false);
+                  }, 2000);
+                }}
+              >
+                <img
+                  src={!isShareActive ? ShareLinkIcon : ShareLinkIconActive}
+                  fontSize="small"
+                  color="primary"
+                  alt="Copy the Task link"
+                />
+              </IconContainer>
+            </Tooltip>
           )}
           <Spacing horizontal={5} />
           {sendEmailAvailable && (
-            <IconContainer
-              onClick={() => {
-                dispatch(openModal('SendEmailFromTask', modalProp));
-                setEmailActive(true);
-              }}
-            >
-              <img
-                src={!isEmailActive ? EmailIcon : EmailIconActive}
-                fontSize="small"
-                color="primary"
-                alt="Copy the Task link"
-              />
-            </IconContainer>
+            <Tooltip placement="bottom-end" title={'Email Task'}>
+              <IconContainer
+                onClick={() => {
+                  dispatch(openModal('SendEmailFromTask', modalProp));
+                  setEmailActive(true);
+                }}
+              >
+                <img
+                  src={!isEmailActive ? EmailIcon : EmailIconActive}
+                  fontSize="small"
+                  color="primary"
+                  alt="Copy the Task link"
+                />
+              </IconContainer>
+            </Tooltip>
           )}
           <Spacing horizontal={4} />
           {sendSmsAvailable && (
-            <IconContainer
-              onClick={() => {
-                dispatch(openModal('SendSmsFromTask', modalProp));
-                setSmsActive(true);
-              }}
-            >
-              <img
-                src={!isSmsActive ? MobileIcon : MobileIconActive}
-                fontSize="small"
-                color="primary"
-                alt="Copy the Task link"
-              />
-            </IconContainer>
+            <Tooltip placement="bottom-start" title={'Send SMS'}>
+              <IconContainer
+                onClick={() => {
+                  dispatch(openModal('SendSmsFromTask', modalProp));
+                  setSmsActive(true);
+                }}
+              >
+                <img
+                  src={!isSmsActive ? MobileIcon : MobileIconActive}
+                  fontSize="small"
+                  color="primary"
+                  alt="Copy the Task link"
+                />
+              </IconContainer>
+            </Tooltip>
           )}
           <Spacing horizontal={4} />
           {allowedOptions.length !== 0 &&
@@ -322,19 +324,25 @@ const TopSection = ({
                 options={allowedOptions}
                 customButtonComponent={IconButton}
               >
-                <MoreHoriz color={!isOptionActive ? 'primary' : 'secondary'} />
+                <Tooltip placement="bottom" title={'More'}>
+                  <MoreHoriz
+                    color={!isOptionActive ? 'primary' : 'secondary'}
+                  />
+                </Tooltip>
               </OptionsMenu>
             )}
           {!hideCloseIcon && (
-            <IconButton
-              onClick={() => {
-                closeTaskDrawer();
-              }}
-              size="small"
-              color="secondary"
-            >
-              <Close />
-            </IconButton>
+            <Tooltip placement="bottom-end" title={'Close Task Drawer'}>
+              <IconButton
+                onClick={() => {
+                  closeTaskDrawer();
+                }}
+                size="small"
+                color="secondary"
+              >
+                <Close />
+              </IconButton>
+            </Tooltip>
           )}
         </Box>
       </Box>
