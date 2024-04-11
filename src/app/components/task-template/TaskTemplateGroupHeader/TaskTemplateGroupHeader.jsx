a76477c8591d51f28edbbe776fab372799bdfbc6
tejas <tejas.patil@rearckindia.com>
2024-04-11 05:38:02 +0530

    fixed task list alignment issue

--- src/app/components/task-template/TaskTemplateGroupHeader/TaskTemplateGroupHeader.jsx
+++ src/app/components/task-template/TaskTemplateGroupHeader/TaskTemplateGroupHeader.jsx
@@ -708,7 +708,7 @@ const TaskTemplateGroupHeader = ({
           isSelected={isBundleSelected}
           isEditingDescription={isEditing}
           order={0}
-          width={+width + 25 + 54}
+          width={+width + 25 + 55}
           origin={origin}
         >
           {!groupDragAndDropDisabled &&
