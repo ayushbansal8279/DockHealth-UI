29a27870b52915442cccf63a080b9e1182d62966
Nikos Amofa <nikosbrightamofa@gmail.com>
2024-04-16 16:11:58 -0400

    practice of code

--- src/app/types/Task.ts
+++ src/app/types/Task.ts
@@ -1,6 +1,8 @@
 import { CommentDto } from './swagger/models/CommentDto';
 import { TaskDto } from './swagger/models/TaskDto';
 
+export type Comment = CommentDto;
+
 export interface Task extends TaskDto {
-  comments?: CommentDto[];
+  comments?: Comment[];
 }
