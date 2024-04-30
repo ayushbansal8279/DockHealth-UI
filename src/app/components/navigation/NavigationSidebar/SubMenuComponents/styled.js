734e9467cfed5e416a7e4468ad2cf071c9cc055a
tejas <tejas.patil@rearckindia.com>
2024-05-01 03:00:03 +0530

    removed commented code

--- src/app/components/navigation/NavigationSidebar/SubMenuComponents/styled.js
+++ src/app/components/navigation/NavigationSidebar/SubMenuComponents/styled.js
@@ -283,7 +283,6 @@ export const DrawerListsItem = styled.div`
     `
         &:hover {
           border: 1px solid ${palette.coolGrey2};
-          // background-color: ${palette.coolGrey3};
         }
 
         &:hover {
