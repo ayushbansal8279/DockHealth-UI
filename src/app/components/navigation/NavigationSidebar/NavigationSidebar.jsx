2c1a3fc58507b054b94cda79f354c58b7a8951c2
Nitin Gujral <nitin.gujral>
2024-05-06 20:58:30 -0400

    remove new label from Dock Chat

--- src/app/components/navigation/NavigationSidebar/NavigationSidebar.jsx
+++ src/app/components/navigation/NavigationSidebar/NavigationSidebar.jsx
@@ -329,7 +329,6 @@ const NavigationSidebar = () => {
                     path=""
                     onItemClick={handleDockChatClick}
                     navSelectedColor={navSelectedColorItem?.value}
-                    isNew
                   />
                 </NavigationIconContainer>
               </AccessRestrictor>
