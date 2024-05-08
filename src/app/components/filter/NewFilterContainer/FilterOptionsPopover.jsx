17daf9e120268f02fbb620f258eb1e883a500432
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2024-05-09 01:47:13 +0530

    Added search bar to filter dropdown

--- src/app/components/filter/NewFilterContainer/FilterOptionsPopover.jsx
+++ src/app/components/filter/NewFilterContainer/FilterOptionsPopover.jsx
@@ -1,5 +1,5 @@
-import React, { useMemo, useCallback, useEffect } from 'react';
-import { MenuItem, Popover } from '@mui/material';
+import React, { useState } from 'react';
+import { Input, MenuItem, Popover } from '@mui/material';
 import {
   FilterOptionsList,
   FilterOptionsListContainer,
@@ -16,9 +16,14 @@ const FilterOptionsPopover = ({
   onSearchChange,
   getTemplatesList,
 }) => {
-  useEffect(() => {
-    if (!open && searchPhrase !== '') onSearchChange('');
-  }, [onSearchChange, open, searchPhrase]);
+  const [filterOptionLists, setFilterOptionList] = useState(filterOptionsList);
+
+  const handleFilterListSearch = (event) => {
+    const filteredList = filterOptionsList.filter((item) =>
+      item.label.toLowerCase().includes(event.target.value.toLowerCase()),
+    );
+    setFilterOptionList(filteredList);
+  };
 
   return (
     <Popover
@@ -44,20 +49,16 @@ const FilterOptionsPopover = ({
       onEnter={getTemplatesList}
       onClose={onClose}
     >
-      {/* <SearchContainer isWorkFlowSearch={isWorkflowSearch}>
-        <Search
-          fullWidth
-          noBackground
-          value={searchPhrase}
-          onChange={(event) => onSearchChange(event?.target?.value)}
-          placeholder="Search"
-          isWorkFlowSearch={isWorkflowSearch}
-        />
-      </SearchContainer> */}
+      <Input
+        fullWidth
+        placeholder='Search'
+        sx={{ padding: '5px 10px 5px 15px' }}
+        onChange={handleFilterListSearch}
+      />
       <SelectOptionsContainer>
         <FilterOptionsListContainer>
           <FilterOptionsList>
-            {filterOptionsList?.map((option) => (
+            {filterOptionLists?.map((option) => (
               <MenuItem
                 onClick={() => onFilterSelect(option.id)}
                 value={option.id}
@@ -70,9 +71,6 @@ const FilterOptionsPopover = ({
                 {option.label}
               </MenuItem>
             ))}
-            {/* {!taskTemplatesIsLoading && taskTemplatesList?.length === 0 && (
-              <EmptyLabel>There are no workflows to select from</EmptyLabel>
-            )} */}
           </FilterOptionsList>
         </FilterOptionsListContainer>
       </SelectOptionsContainer>
