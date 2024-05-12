c6b78d27fc5b0fd55935b862d545a7ea3b117b5f
Nitin Gujral <nitin.gujral>
2024-05-12 14:12:49 -0400

    minor error handling

--- src/app/components/task/StandardTaskItem/TaskItem.jsx
+++ src/app/components/task/StandardTaskItem/TaskItem.jsx
@@ -861,11 +861,11 @@ const TaskItem = React.memo(
                         {`${task?.completedBy?.firstName} ${task?.completedBy?.lastName}`}
                       </TootipCompletedByName>
                       <TootipCompletedByDate>
-                        {`${new Date(task.completedDt).toLocaleString('en-US', {
+                        {`${new Date(task?.completedDt).toLocaleString('en-US', {
                           weekday: 'long',
-                        })}, ${moment(task.completedDt).format(
+                        })}, ${moment(task?.completedDt).format(
                           'MMM DD, YYYY',
-                        )} @${new Date(task.completedDt)
+                        )} @${new Date(task?.completedDt)
                           .toLocaleTimeString('en-US', {
                             hour: 'numeric',
                             minute: '2-digit',
@@ -942,7 +942,7 @@ const TaskItem = React.memo(
         task?.priority,
         task?.completedBy?.firstName,
         task?.completedBy?.lastName,
-        task.completedDt,
+        task?.completedDt,
         isLast,
         bulkEditEnabled,
         onClickBulkEdit,
