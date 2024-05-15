07c89aa7f3936bf67b655745b0b14f603438c62e
Nitin Gujral <nitin.gujral>
2024-05-14 22:53:04 -0400

    updated display of priority column for task - show color for High Priority

--- src/app/components/task/StandardTaskItem/TaskItem.jsx
+++ src/app/components/task/StandardTaskItem/TaskItem.jsx
@@ -1223,7 +1223,12 @@ const TaskItem = React.memo(
                       onChange={handlePriorityChange}
                       field={{
                         options: [
-                          { identifier: 'HIGH', name: 'High', tag: 'High' },
+                          {
+                            identifier: 'HIGH',
+                            name: 'High',
+                            tag: 'High',
+                            color: getPriorityColor('HIGH'),
+                          },
                           { identifier: 'LOW', name: 'No Priority', tag: '' },
                         ],
                         displayOptions: [],
