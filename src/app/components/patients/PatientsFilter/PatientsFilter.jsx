51c42cbc6b2dd4bdb88ac14d1c93b96a3b63d642
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2024-05-17 18:49:02 +0530

    optimised patient list filter

--- src/app/components/patients/PatientsFilter/PatientsFilter.jsx
+++ src/app/components/patients/PatientsFilter/PatientsFilter.jsx
@@ -7,9 +7,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import isEmpty from 'ramda/src/isEmpty';
 import * as PatientsActions from 'actions/patients-actions';
 import { organizationSelector } from 'selectors/organization-selectors';
-import FilterHeader from 'components/filter/FilterHeader/FilterHeader';
-import { Box } from '@mui/material';
-import FilterTable from 'components/filter/FilterTable/FilterTable';
 import {
   getQuickFilters,
   createQuickFilter,
@@ -29,15 +26,24 @@ import {
 import NewFilterContainer from '../../filter/NewFilterContainer/NewFilterContainer';
 import SaveFilterPopup from '../../filter/SaveFilterPopup/SaveFilterPopup';
 
-const PatientsFilter = ({ closeFilter, filterButtonReference }) => {
-  const [menuOptions, setMenuOption] = useState([]);
-  const [finalFilter, setFinalFilter] = useState({});
-  const [filteredData, setFilteredData] = useState({});
-  const [isSavePopupOpen, setSavePopupOpen] = useState(false);
-  const [editIdentifier, setEditIdentifier] = useState('');
-  const [customFinalFilter, setCustomFinalFilter] = useState({});
-  const [selectedCustomFilter, setSelectedCustomFilter] = useState({});
-  const [customFilteredData, setCustomFilteredData] = useState({});
+const PatientsFilter = ({
+  closeFilter,
+  filterButtonReference,
+  finalFilter,
+  setFinalFilter,
+  filteredData,
+  setFilteredData,
+  isSavePopupOpen,
+  setSavePopupOpen,
+  editIdentifier,
+  setEditIdentifier,
+  customFinalFilter,
+  setCustomFinalFilter,
+  selectedCustomFilter,
+  setSelectedCustomFilter,
+  customFilteredData,
+  setCustomFilteredData,
+}) => {
   const dispatch = useDispatch();
   const filterOptions = useSelector(filterOptionsSelector);
   const selectedFilters = useSelector(patientsSelectedFiltersSelector);
@@ -48,12 +54,6 @@ const PatientsFilter = ({ closeFilter, filterButtonReference }) => {
   const selectedQuickFilter = useSelector(selectedQuickFilterSelector);
 
   const { organizationIdentifier } = useSelector(organizationSelector);
-
-  useEffect(() => {
-    setMenuOption(
-      filterOptions?.map((item) => ({ label: item.label, id: item.id })),
-    );
-  }, [filterOptions, finalFilter]);
 
   useEffect(() => {
     if (!filterOptions || filterOptions?.length === 0) {
@@ -68,15 +68,51 @@ const PatientsFilter = ({ closeFilter, filterButtonReference }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  console.log(selectedQuickFilter);
+
+  useEffect(() => {
+    const data = {};
+    if (selectedQuickFilter === null && selectedFilters && filterOptions) {
+      for (const key in selectedFilters) {
+        if (key !== '') {
+          const users = filterOptions
+            .flatMap((item) => item.id === key && item.options)
+            .filter((item) => typeof item !== 'boolean');
+
+          let options = selectedFilters[key].options.map((item) => {
+            if (item.includes('DATE_RANGE')) {
+              let aa = users.find((user) => user.key === item);
+              aa = {
+                ...aa,
+                dateStart: selectedFilters[key].dateStart,
+                dateEnd: selectedFilters[key].dateEnd,
+              };
+              return aa;
+            } else {
+              return users.find((user) => user.key === item);
+            }
+          });
+          data[key] = options;
+        }
+      }
+      setFinalFilter(data);
+    }
+  }, [selectedFilters, filterOptions, selectedQuickFilter]);
+
   const handleSelectedFiltersChange = () => {
     dispatch(PatientsActions.setPatientsSelectedFilters(filteredData));
     closeFilter();
   };
 
   const handleClear = () => {
+    console.log("heyyyyyyyyyy");
     dispatch(PatientsActions.clearPatientsFilters());
     dispatch(selectQuickFilter(null));
   };
+
+  // setTimeout(() => {
+  //   handleClear()
+  // }, 10000);
 
   const handleSaveAsQuickFilter = useCallback(
     () => dispatch(showAddQuickFilterOption()),
@@ -108,7 +144,7 @@ const PatientsFilter = ({ closeFilter, filterButtonReference }) => {
   );
 
   const handleQuickFilterCreate = useCallback(
-    (name,filteredData) =>
+    (name, filteredData) =>
       dispatch(
         createQuickFilter(
           name,
@@ -167,24 +203,15 @@ const PatientsFilter = ({ closeFilter, filterButtonReference }) => {
         setEditIdentifier={setEditIdentifier}
         editIdentifier={editIdentifier}
         filters={filterOptions}
-        // onSelectedFiltersChange={onSelectFilters}
         setFinalFilter={setFinalFilter}
-        setMenuOption={setMenuOption}
-        menuOptions={menuOptions}
-        // openPopover={openPopover}
         handleSaveQuickFilter={handleSaveQuickFilter}
-        // setFilteredData={setFilteredData}
         filteredData={filteredData}
         customFinalFilter={customFinalFilter}
         selectedCustomFilter={selectedCustomFilter}
         setCustomFinalFilter={setCustomFinalFilter}
-        // selectedQuickFilter={selectedQuickFilter}
-        // setSelectedQuickFilter={setSelectedQuickFilter}
         customFilteredData={customFilteredData}
         setCustomFilteredData={setCustomFilteredData}
-        // selectQuickFilter={selectQuickFilter}
         isPatientListPage
-        
       ></SaveFilterPopup>
       <CustomFilters
         quickFiltersList={quickFiltersList}
@@ -192,80 +219,35 @@ const PatientsFilter = ({ closeFilter, filterButtonReference }) => {
         selectedQuickFilter={selectedQuickFilter}
         selectQuickFilter={handleSelectQuickFilter}
         editModeEnabled={false}
-        // onCreate={handleQuickFilterCreate}
         onUpdate={handleQuickFilterUpdate}
         onDelete={handleQuickFilterDelete}
         setSavePopupOpen={setSavePopupOpen}
-        // selectQuickFilter={selectQuickFilter}
-        // onUpdate={onQuickFilterUpdate}
-        // editModeEnabled={wasChangedFilters}
         onQuickFilterCreate={handleQuickFilterCreate}
-        // onDelete={onQuickFilterDelete}
-        // setMenuOption={setMenuOption}
-        // setFinalFilter={setFinalFilter}
         filters={filterOptions}
         setEditIdentifier={setEditIdentifier}
         editIdentifier={editIdentifier}
         customFinalFilter={customFinalFilter}
         setCustomFinalFilter={setCustomFinalFilter}
-        // openPopover={openPopover}
-        // selectedQuickFilter={selectedQuickFilter}
-        // setSelectedQuickFilter={setSelectedQuickFilter}
-        // clearFilters={clearFilters}
         setSelectedCustomFilter={setSelectedCustomFilter}
-        handleQuickFilterDuplicateForPatientList={handleQuickFilterDuplicateForPatientList}
+        handleQuickFilterDuplicateForPatientList={
+          handleQuickFilterDuplicateForPatientList
+        }
+        
+        clearFilters={handleClear}
         isPatientListPage
       />
       <NewFilterContainer
         filters={filterOptions}
-        // onSelectedFiltersChange={onSelectFilters}
         setFinalFilter={setFinalFilter}
         finalFilter={finalFilter}
-        setMenuOption={setMenuOption}
-        menuOptions={menuOptions}
-        // openPopover={openPopover}
         quickFiltersList={quickFiltersList}
         setSavePopupOpen={setSavePopupOpen}
-        // onQuickFilterCreate={onQuickFilterCreate}
-        // handleSaveQuickFilter={handleSaveQuickFilter}
         setFilteredData={setFilteredData}
-        // filteredData={filteredData}
         customFinalFilter={customFinalFilter}
         setCustomFinalFilter={setCustomFinalFilter}
-        // setSelectedQuickFilter={setSelectedQuickFilter}
         handleSelectedFiltersChange={handleSelectedFiltersChange}
         isPatientListPage
       ></NewFilterContainer>
-      {/* <FilterHeader
-        title="Filter patients"
-        filterActive={isFilterApplied}
-        selectedFilters={selectedFilters}
-        searchValue={searchValue}
-        onSearchValueChange={setSearchValue}
-        onClear={handleClear}
-        onSave={handleSaveQuickFilter}
-        onSaveAsNew={handleSaveAsQuickFilter}
-        selectedQuickFilter={selectedQuickFilter}
-        editModeEnabled={wasChangedFilters}
-      />
-      <Box p={2} />
-      <FilterTable
-        searchValue={searchValue}
-        filters={filterOptions}
-        selectedFilters={selectedFilters}
-        onSelectedFiltersChange={handleSelectedFiltersChange}
-      >
-        <CustomFilters
-          quickFiltersList={quickFiltersList}
-          addQuickFilterOption={addQuickFilterOption}
-          selectedQuickFilter={selectedQuickFilter}
-          selectQuickFilter={handleSelectQuickFilter}
-          editModeEnabled={false}
-          onCreate={handleQuickFilterCreate}
-          onUpdate={handleQuickFilterUpdate}
-          onDelete={handleQuickFilterDelete}
-        />
-      </FilterTable> */}
     </>
   );
 };
