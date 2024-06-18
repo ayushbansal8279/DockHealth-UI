5d30dc33d25c32b67daf4cb7e8787c201ead9fa0
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2024-06-18 18:39:35 +0530

    Added AddTaskDrawerContent in TaskDrawer

--- src/app/components/task-drawer/TaskDrawer/TaskDrawer.jsx
+++ src/app/components/task-drawer/TaskDrawer/TaskDrawer.jsx
@@ -3,23 +3,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { useSelector } from 'react-redux';
 import { taskDrawerOpenSelector } from 'selectors/task-drawer-selectors';
-// import { AnimatePresence } from 'framer-motion';
 import TaskDrawerContent from 'components/task-drawer/TaskDrawerContent/TaskDrawerContent';
+import AddTaskDrawerContent from '@/app/components/task-drawer/TaskDrawerContent/AddTaskDrawerContent';
 import { AnimatedContainer } from './styled';
 
 const TaskDrawer = (props) => {
+  const { isAddTaskDrawer } = props;
   const taskDrawerOpen = useSelector(taskDrawerOpenSelector);
 
   return ReactDOM.createPortal(
-    // <AnimatePresence initial={false}>
     <AnimatedContainer
       style={{
         transform: taskDrawerOpen ? 'translateX(0%)' : 'translateX(100%)',
       }}
     >
-      <TaskDrawerContent {...props} stickyHeader origin={props.origin} />
+      {isAddTaskDrawer ? (
+        <AddTaskDrawerContent {...props} />
+      ) : (
+        <TaskDrawerContent {...props} stickyHeader origin={props.origin} />
+      )}
     </AnimatedContainer>,
-    // </AnimatePresence>,
     document.body,
   );
 };
