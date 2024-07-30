f59c770ec0d7f32272c697cd6e118e9ddb55c9f6
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2024-07-30 16:52:35 +0530

    added patient AI summary modal and respective styled

---
+++ src/app/modal/components/PatientAISummaryModal/PatientAISummaryModal.jsx
@@ -0,0 +1,79 @@
+import React, { useEffect } from 'react';
+import { Grid } from '@mui/material';
+import Spacing from 'components/common/Spacing';
+import {
+  PatientAISummaryModalWrapper,
+  Header,
+  PatientName,
+  SubHeader,
+  IconWrapper,
+  GeneratedTime,
+  PatientInfo,
+} from './styled';
+import LuminaStar from 'img/AI/LuminaStar';
+import palette from '@/app/styles/palette';
+import Copy from 'img/AI/Copy.svg';
+import Close from 'img/AI/XClose.svg';
+
+const PatientAISummaryModal = ({ closeModal, patient }) => {
+  useEffect(() => {
+    if (!patient) {
+      closeModal();
+    }
+  }, [closeModal, patient]);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText('Aditya Chaurasia');
+  };
+
+  return (
+    <PatientAISummaryModalWrapper>
+      <Header>
+        <SubHeader>
+          <LuminaStar color={palette.newBrightBlue} />
+          <PatientName>
+            {patient?.lastName}, {patient?.firstName}
+          </PatientName>
+        </SubHeader>
+        <div>
+          <IconWrapper onClick={handleCopy} src={Copy} alt="copy" />
+          <IconWrapper onClick={closeModal} src={Close} alt="close" />
+        </div>
+      </Header>
+      <Spacing vertical={3} />
+      <Grid container direction="column" item wrap="nowrap">
+        <GeneratedTime>Generated 2 m ago</GeneratedTime>
+        <Spacing vertical={4} />
+        <PatientInfo>
+          Patient info: <br />
+          34 year old male patient named Bojan Ilioski El with date of birth
+          1989-11-08, patient identifier 6ea1e12d-1a54-40db-ae63-7cd70b6fe4f3,
+          created by Stefan Kochev on 2024-01-15, last updated on 2024-02-02,
+          currently has active status. <br />
+          <Spacing vertical={4} />
+          Notes summary: <br />
+          Notes indicate the patient's appetite is improving after follow-up. A
+          Flomax prescription was sent to the pharmacy for the patient. The
+          patient also reported no longer having nausea in a previous note.
+          <Spacing vertical={4} />
+          Workflows summary: <br />
+          The first workflow involved calling the patient Bojan Ilioski to share
+          lab results. It was conducted by Stefan Kochev on January 16, 2024.
+          The lab results were received and analyzed, then the patient was
+          called to discuss the findings. The call was documented in the
+          patient's chart and follow up reminders were created. <br />
+          <br />
+          The second workflow was for onboarding patient Bojan Ilioski El. It
+          involved contacting the patient, checking their medication and
+          insurance information, submitting refill requests, and recording
+          encounters. It was executed by Stefan Kochev from January 21-22, 2024.
+          Initial contact was unsuccessful so alternative methods were used. A
+          medication coverage issue was identified and resolved. All encounters
+          were recorded in the EMR.
+        </PatientInfo>
+      </Grid>
+    </PatientAISummaryModalWrapper>
+  );
+};
+
+export default PatientAISummaryModal;
---
+++ src/app/modal/components/PatientAISummaryModal/styled.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+import spacing from 'styles/spacing';
+import { fontSizes, fontWeights } from 'styles/font';
+import palette from 'styles/palette';
+import { ModalWrapper } from '../styled';
+
+export const PatientAISummaryModalWrapper = styled(ModalWrapper)`
+  width: 600px;
+  border-radius: 10px;
+  padding: ${spacing.regularPlus} ${spacing.largePlus} ${spacing.largePlus};
+`;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: space-between;
+  width: 100%;
+`;
+
+export const SubHeader = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+
+export const PatientName = styled.div`
+  color: ${palette.gunmetal};
+  font-size: 21px;
+  font-weight: ${fontWeights.bold};
+`;
+
+export const IconWrapper = styled.img`
+  margin-left: 10px;
+  cursor: pointer;
+`;
+
+export const GeneratedTime = styled.div`
+  color: ${palette.gunmetal};
+  font-size: 12px;
+  font-weight: ${fontWeights.light};
+  line-height: 150%;
+`;
+export const PatientInfo = styled.div`
+  color: ${palette.gunmetal};
+  font-size: 14px;
+  font-style: normal;
+  font-weight: ${fontWeights.light};
+  line-height: normal;
+`;
