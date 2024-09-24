0bbeebb588eef0c62c98491484f10f84bd2b9994
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2024-09-24 18:44:19 +0530

    Created Profile Builder main container

---
+++ src/app/components/profile-builder/styled.ts
@@ -0,0 +1,27 @@
+import palette from '@/app/styles/palette';
+import spacing from '@/app/styles/spacing';
+import styled from 'styled-components';
+
+export const BuilderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 100%;
+  background: ${palette.white};
+`;
+
+export const CategoryWrapper = styled.div`
+  width: 30%;
+  height: 100%;
+  border-left: 1px solid ${palette.iron};
+  border-top: 1px solid ${palette.iron};
+`;
+
+export const PlayGroungWrapper = styled.div`
+  width: 70%;
+  height: 100%;
+  border-top: 1px solid ${palette.iron};
+  padding: ${spacing.huge}
+`;
+
+
+
