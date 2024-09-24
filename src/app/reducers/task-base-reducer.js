9f51f1e01bc9648ae0086f21036626671c431e49
tejas <tejas.patil@rearckindia.com>
2024-09-25 02:20:08 +0530

    fixed deleting subtask issue on list page

--- src/app/reducers/task-base-reducer.js
+++ src/app/reducers/task-base-reducer.js
@@ -133,25 +133,6 @@ const TaskBaseReducer = (state, action, updateStateCallback) => {
           return null;
         }
 
-        if (task?.parentTaskIdentifier) {
-          const updateTaskFromAction = (tasksMap) => {
-            const parentTask = tasksMap[task?.parentTaskIdentifier];
-            if (!parentTask) {
-              return null;
-            }
-
-            return {
-              ...parentTask,
-              subtasks: (parentTask.subtasks || []).filter(
-                (subtask) => subtask?.identifier !== task?.identifier,
-              ),
-              subTasksCount: parentTask?.subTasksCount - 1,
-            };
-          };
-
-          return updateStateCallback(state, updateTaskFromAction);
-        }
-
         if (task.identifier === taskIdentifier) {
           // return null to remove the task
           return null;
@@ -540,6 +521,27 @@ const TaskBaseReducer = (state, action, updateStateCallback) => {
           ...task,
           subtasks: [...(task.subtasks || []), subtask],
           subTasksCount: task?.subTasksCount + 1,
+        };
+      };
+
+      return updateStateCallback(state, updateTaskFromAction);
+    }
+
+    case ActionTypes.DELETE_SUBTASK: {
+      const { subTask, parentTaskIdentifier } = action;
+
+      const updateTaskFromAction = (tasksMap) => {
+        const parentTask = tasksMap[parentTaskIdentifier];
+        if (!parentTask) {
+          return null;
+        }
+
+        return {
+          ...parentTask,
+          subtasks: (parentTask?.subtasks || []).filter(
+            (subtask) => subtask?.identifier !== subTask?.identifier,
+          ),
+          subTasksCount: parentTask?.subTasksCount - 1,
         };
       };
 
