c587c680ddf8abd7cd013ffd272a256abe460197
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2024-10-25 00:07:20 +0530

    updated date formatter

--- src/app/helpers/date-formatter.ts
+++ src/app/helpers/date-formatter.ts
@@ -1,17 +1,17 @@
-import { format } from 'date-fns';
-import { utcToZonedTime } from 'date-fns-tz';
+import moment from 'moment';
 
 type InputValue = Date | string | number | null | undefined;
 
-/**
- * https://date-fns.org/v3.6.0/docs/format
- * @param date '2023-04-13'
- * @param newFormat 'MM/dd/yyyy, HH:mm'
- * @returns 04/13/2024, 00:00, regardless of local timezone
- */
-export function dateFormatter(
-  date: InputValue,
-  newFormat: string = 'MM/dd/yyyy',
-) {
-  return date ? format(utcToZonedTime(new Date(date), 'UTC'), newFormat) : '';
+export function dateFormatter(date: InputValue) {
+  if (!date) return '';
+
+  const standardDateFormat = 'MMM DD, YYYY';
+  const standardDateTimeFormat = 'MMM DD, YYYY @ hh:mm a';
+  
+  const dateMoment = moment(date);
+  const isTimeAvailable = dateMoment.hour() || dateMoment.minute();
+
+  return dateMoment.format(
+    isTimeAvailable ? standardDateTimeFormat : standardDateFormat
+  );
 }
