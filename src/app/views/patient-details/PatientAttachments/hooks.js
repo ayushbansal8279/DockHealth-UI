ce1c5929144e69017954b067bfebc6282889ced1
mohit <mohit.kumawat@conns.com>
2024-11-26 23:25:31 +0530

    added a download option in the context menu for attachment

--- src/app/views/patient-details/PatientAttachments/hooks.js
+++ src/app/views/patient-details/PatientAttachments/hooks.js
@@ -157,6 +157,24 @@ const useInitializeAttachmentsSectionHooks = () => {
     [dispatch, patientIdentifier],
   );
 
+  const downloadAttachment = async (attachment) => {
+    const { attachmentIdentifier, fileName, contentType } = attachment;
+    try {
+        const {data} = await getMemoPatientAttachment(attachmentIdentifier);
+        const blob = new Blob([data], { type: contentType });
+        const url = window.URL.createObjectURL(blob);
+        const anchor = document.createElement('a');
+        anchor.href = url;
+        anchor.download = fileName;
+        document.body.appendChild(anchor);
+        anchor.click();
+        document.body.removeChild(anchor);
+        window.URL.revokeObjectURL(url);
+    } catch (error) {
+        console.error('Error downloading attachment:', error);
+    } 
+};
+
   const openAttachmentPreview = useCallback(
     (attachment) => {
       setPreviewedAttachment(attachment);
@@ -319,6 +337,7 @@ const useInitializeAttachmentsSectionHooks = () => {
       isDragActive,
     },
     downloadAllFiles,
+    downloadAttachment,
     getMemoPatientAttachment,
   };
 };
