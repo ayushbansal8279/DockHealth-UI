09754d8594c07df62957914586474fd8b4133b2f
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2024-12-23 18:37:34 +0530

    updated metering api

--- src/app/api/metering-api.js
+++ src/app/api/metering-api.js
@@ -1,12 +1,12 @@
 import { log } from 'helpers/log';
 import axios from './axios-heydoc';
 
-export function getMeteringEvents(payload) {
-  return axios
-    .post(`/metering/event/search`, payload)
-    .then((response) => response.data)
-    .catch((error) => {
-      log(error);
-      throw new Error(error?.response?.data?.errorMessage);
-    });
+export async function getMeteringEvents(payload) {
+  try {
+    const response = await axios.post(`/metering/event/search`, payload);
+    return response.data;
+  } catch (error) {
+    log(error);
+    throw new Error(error?.response?.data?.errorMessage);
+  }
 }
