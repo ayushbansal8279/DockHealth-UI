16b2ab3179faa5b2b64e03a88d5e650e2dc09cda
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2024-12-30 22:14:59 +0530

    added getMeteringFilterOptions in api

--- src/app/api/metering-api.js
+++ src/app/api/metering-api.js
@@ -1,10 +1,21 @@
 import { log } from 'helpers/log';
 import axios from './axios-heydoc';
+import { mapFilterOptions } from '../helpers/filter-options-helpers';
 
 export async function getMeteringEvents(payload) {
   try {
     const response = await axios.post(`/metering/event/search`, payload);
     return response.data;
+  } catch (error) {
+    log(error);
+    throw new Error(error?.response?.data?.errorMessage);
+  }
+}
+
+export async function getMeteringFilterOptions() {
+  try {
+    const response = await axios.get('/metering/filterOptions');
+    return mapFilterOptions(response.data);
   } catch (error) {
     log(error);
     throw new Error(error?.response?.data?.errorMessage);
