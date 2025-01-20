5c5b67cca67b6f1dd2ecb6c540fe9c0bc6485f06
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2025-01-21 04:02:22 +0530

    updated filter components back for multiple option support

--- src/app/components/filter/FilterSelect/FilterSelect.jsx
+++ src/app/components/filter/FilterSelect/FilterSelect.jsx
@@ -33,7 +33,6 @@ const FilterSelect = ({
   setFinalFilter,
   filter,
   filters,
-  multiSelectEnabled = true,
 }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [optionName, setOptionName] = useState('');
@@ -169,8 +168,8 @@ const FilterSelect = ({
         {!isDateRange ? (
           <div style={{ display: 'flex' }}>
             <Autocomplete
-              multiple={multiSelectEnabled}
-              options={multiSelectEnabled ? options : filterOptions}
+              multiple
+              options={options}
               disableCloseOnSelect
               getOptionLabel={(option) => option?.displayValue}
               renderOption={(props, option) => (
@@ -206,11 +205,7 @@ const FilterSelect = ({
                 </>
               )}
               style={{ width: '517' }}
-              value={
-                multiSelectEnabled
-                  ? finalFilter[filter]
-                  : finalFilter[filter][finalFilter[filter].length - 1]
-              }
+              value={finalFilter[filter]}
               onChange={(event, newValue, action, option) => {
                 if (action === 'selectOption') {
                   handleSelectOption(option.option);
