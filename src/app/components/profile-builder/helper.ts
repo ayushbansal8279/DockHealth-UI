1b916ef326b3ebfdac8e5f13a61a38a8c7df76c7
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2025-02-01 00:36:41 +0530

    updated profile builder

--- src/app/components/profile-builder/helper.ts
+++ src/app/components/profile-builder/helper.ts
@@ -1,13 +1,16 @@
 import ShortText from 'img/profile-builder/shortText.svg';
 import RichText from 'img/profile-builder/RichText.svg';
-import Hash from 'img/profile-builder/Hash.svg';
 import Calender from 'img/profile-builder/Calender.svg';
-import Phone from 'img/profile-builder/Phone.svg';
-import Email from 'img/profile-builder/Email.svg';
+import Hash from 'img/profile-builder/Hash.svg';
+import Boolean from 'img/profile-builder/Boolean.svg';
+import Link from 'img/profile-builder/Link.svg';
+import DropDown from 'img/profile-builder/DropDown.svg';
+import Profile from 'img/profile-builder/Profile.svg';
 
 export interface Field {
   name: string;
   dragId: string;
+  fieldType: string;
   img: string;
   index: number;
 }
@@ -18,42 +21,81 @@ export interface Category {
   fields: Field[];
 }
 
+export const FieldType = {
+  TEXT: 'TEXT',
+  LONG_TEXT: 'LONG_TEXT',
+  DATE: 'DATE',
+  NUMBER: 'NUMBER',
+  BOOL: 'BOOLEAN',
+  HYPERLINK: 'HYPERLINK',
+  DROPDOWN: 'PICK_LIST',
+  DROPDOWN_MULTI: 'MULTI_SELECT',
+  RELATIONSHIP: 'RELATIONSHIP',
+};
+
 export const dummyFields: Field[] = [
   {
     name: 'Short Filed',
-    dragId: 'short-field',
+    dragId: FieldType.TEXT,
+    fieldType: FieldType.TEXT,
     img: ShortText,
     index: 1,
   },
   {
     name: 'Rich Text Paragraph',
-    dragId: 'rich-text-field',
+    dragId: FieldType.LONG_TEXT,
+    fieldType: FieldType.LONG_TEXT,
     img: RichText,
     index: 2,
   },
   {
     name: 'Date',
-    dragId: 'date-field',
+    dragId: FieldType.DATE,
+    fieldType: FieldType.DATE,
     img: Calender,
     index: 3,
   },
   {
     name: 'Numeric Input',
-    dragId: 'number-field',
+    dragId: FieldType.NUMBER,
+    fieldType: FieldType.NUMBER,
     img: Hash,
     index: 4,
   },
   {
-    name: 'Phone Field',
-    dragId: 'phone-field',
-    img: Phone,
+    name: 'Boolean',
+    dragId: FieldType.BOOL,
+    fieldType: FieldType.BOOL,
+    img: Boolean,
     index: 5,
   },
   {
-    name: 'Email Field',
-    dragId: 'email-field',
-    img: Email,
+    name: 'Link Field',
+    dragId: FieldType.HYPERLINK,
+    fieldType: FieldType.HYPERLINK,
+    img: Link,
     index: 6,
+  },
+  {
+    name: 'Single Select',
+    dragId: FieldType.DROPDOWN,
+    fieldType: FieldType.DROPDOWN,
+    img: DropDown,
+    index: 7,
+  },
+  {
+    name: 'Multi-Selection',
+    dragId: FieldType.DROPDOWN_MULTI,
+    fieldType: FieldType.DROPDOWN_MULTI,
+    img: DropDown,
+    index: 8,
+  },
+  {
+    name: 'Link Profile',
+    dragId: FieldType.RELATIONSHIP,
+    fieldType: FieldType.RELATIONSHIP,
+    img: Profile,
+    index: 9,
   },
 ];
 
@@ -70,3 +112,36 @@ export const DROPTYPE = {
   AddCategory: 'add-category-area-source',
   AddField: 'add-fields-area-source',
 };
+
+// active: true;
+// contextType: 'CUSTOM';
+// createdDateTime: '2023-12-04T22:48:37.000+00:00';
+// fieldCategoryType: 'PROFILE';
+// fieldType: 'LONG_TEXT';
+// identifier: '83a07aaa-3630-4fd1-840a-6530afdcdb2b';
+// name: 'Working Hours';
+// targetType: 'PROFILE';
+// updatedDateTime: '2023-12-11T21:00:51.000+00:00';
+
+// active: true;
+// contextType: 'CUSTOM';
+// createdDateTime: '2025-01-31T08:14:35.246+00:00';
+// fieldCategoryType: 'PROFILE';
+// fieldType: 'TEXT';
+// identifier: '101c9da1-9b2b-4bce-95e5-f5858487455e';
+// name: 'Testing';
+// targetType: 'PROFILE';
+// updatedDateTime: '2025-01-31T08:14:35.246+00:00';
+
+contextType: 'CUSTOM';
+displayOptions: [];
+fieldCategoryType: 'PROFILE';
+fieldType: 'TEXT';
+name: 'Testing';
+options: null;
+profileType: {
+  identifier: '3de8dc3e-75d9-4e52-9e2f-f1ff6d975b76';
+}
+relatedProfileType: {
+}
+targetType: 'PROFILE';
