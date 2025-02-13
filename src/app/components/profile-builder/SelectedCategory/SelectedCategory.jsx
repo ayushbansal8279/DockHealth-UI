6cc8f468ba609991a7fb76718eb39847bea57a45
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2025-02-13 18:30:52 +0530

    updated selected category for builder

---
+++ src/app/components/profile-builder/SelectedCategory/SelectedCategory.jsx
@@ -0,0 +1,73 @@
+import React, { useContext } from 'react';
+import {
+  CategoryHeader,
+  FieldDropper,
+  MainContainer,
+  HeaderWrapper,
+  DeletIcon,
+} from './styled';
+import { DragIndicator } from '@mui/icons-material';
+import TextField from '../TextField';
+import { Droppable } from 'react-beautiful-dnd';
+import SelectedField from '../SelectedField/SelectedField';
+import * as ProfileTypeFieldApi from 'api/profile-type-field-api';
+import { ProfileBuilderContext } from '../ProfileBuilder';
+
+const SelectedCategory = ({ category }) => {
+  const { isAddFieldDrop, setSelectedCategories } = useContext(
+    ProfileBuilderContext,
+  );
+
+  const handleDeleteGroup = () => {
+    ProfileTypeFieldApi.deleteCustomFiledGroup(category.identifier).then(() => {
+      setSelectedCategories((prevCategories) =>
+        prevCategories.filter(
+          (category1) => category1.identifier !== category.identifier,
+        ),
+      );
+    });
+  };
+
+  const handleCategoryUpdate = (value) => {
+    const payload = {
+      name: value,
+    };
+    ProfileTypeFieldApi.updateCustomFiledGroup(category.identifier, payload);
+  };
+
+  return (
+    <MainContainer>
+      <CategoryHeader>
+        <HeaderWrapper>
+          <DragIndicator />
+          <TextField
+            value={category.name}
+            fontSize="18px"
+            fontWeight="600"
+            size="small"
+            placeholder="Add Category Name "
+            width="50%"
+            onEnter={handleCategoryUpdate}
+          />
+        </HeaderWrapper>
+        <DeletIcon onClick={handleDeleteGroup} />
+      </CategoryHeader>
+      {category?.fields?.map((field) => {
+        return <SelectedField category={category} field={field} />;
+      })}
+      <Droppable droppableId={category.identifier}>
+        {(provided) => (
+          <FieldDropper
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            isAddFieldDrop={isAddFieldDrop}
+          >
+            Drag and drop field items
+          </FieldDropper>
+        )}
+      </Droppable>
+    </MainContainer>
+  );
+};
+
+export default SelectedCategory;
