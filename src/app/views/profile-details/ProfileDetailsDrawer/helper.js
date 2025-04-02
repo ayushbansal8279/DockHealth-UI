22f7cac23464cda60c07e0e7b9f2fa41715b437d
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2025-04-02 21:56:45 +0530

    moved most of the Profile details Drawer functions to helper

--- src/app/views/profile-details/ProfileDetailsDrawer/helper.js
+++ src/app/views/profile-details/ProfileDetailsDrawer/helper.js
@@ -1,3 +1,5 @@
+import { GENDER_OPTIONS_BIRTH } from '@/app/types/gender';
+
 export function mapFieldsFromIdentifiers(mappings, data) {
   const mappedFields = {};
   const unmappedFields = {};
@@ -42,4 +44,80 @@ export function processCustomFields(unmappedFields, allCustomFields) {
       }
     })
     .filter(Boolean);
+}
+
+export function formatPatientName(patient) {
+  return `${patient?.lastName}, ${patient?.firstName} ${
+    patient?.middleName ?? ''
+  }`;
+}
+
+export function formatProfileTitle(profileName) {
+  return [
+    `${profileName?.[1] ? profileName?.[1] + ',' : ''}`,
+    profileName?.[0],
+    profileName?.[2],
+  ].join(' ');
+}
+
+export function enrichCategoryGroups(customGroups, customFields) {
+  return customGroups.map((category) => {
+    if (!category.fields) {
+      return category;
+    }
+
+    const enrichedFields = category.fields.map((field) => {
+      const matchingField = customFields?.find(
+        (customField) => customField.identifier === field.fieldReferenceId,
+      );
+
+      return matchingField ? { ...field, ...matchingField } : field;
+    });
+
+    return { ...category, fields: enrichedFields };
+  });
+}
+
+function getGenderOptions() {
+  return GENDER_OPTIONS_BIRTH?.map((item) => {
+    return {
+      identifier: item?.value,
+      name: item?.label,
+    };
+  });
+}
+
+function getGenderIdentityOptions(genderIdentity) {
+  return genderIdentity?.map((item) => {
+    return {
+      identifier: item?.genderIdentityType,
+      name: item?.description,
+    };
+  });
+}
+
+export function addFieldOptionsInDefaultCategory(processedGroups, GENDER_OPTIONS) {
+  return processedGroups.map((category) => {
+    if (category.name === 'Default Group') {
+      const newFields = category?.fields?.map((field) => {
+        let updatedField = { ...field };
+
+        switch (field.name) {
+          case 'Gender Identity':
+            updatedField.options = getGenderIdentityOptions(GENDER_OPTIONS);
+            break;
+          case 'Sex at Birth':
+            updatedField.options = getGenderOptions();
+            break;
+          default:
+            break;
+        }
+
+        return updatedField;
+      });
+
+      return { ...category, fields: newFields };
+    }
+    return category;
+  });
 }
