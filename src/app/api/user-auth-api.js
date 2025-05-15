6af1b102799d9937ebdd559203d947e30de9cdeb
Nitin Gujral <nitin.gujral>
2025-05-15 13:28:34 -0400

    store DOCK access token from FHIR launch

--- src/app/api/user-auth-api.js
+++ src/app/api/user-auth-api.js
@@ -599,6 +599,7 @@ export const getFHIREnterpriseAccessTokensByAuthCode = (
         sessionStorage.setItem('SSO_USEREMAIL', email);
         sessionStorage.setItem('userIdentifier', userIdentifier);
         sessionStorage.setItem('patientIdentifier', patientIdentifier);
+        sessionStorage.setItem('accessToken', userAccessToken);
         resolve('success');
       });
     } catch (error) {
@@ -628,6 +629,7 @@ export const getFHIREnterpriseAccessTokensByRefreshToken = (
         sessionStorage.setItem('SSO_USEREMAIL', email);
         sessionStorage.setItem('userIdentifier', userIdentifier);
         sessionStorage.setItem('patientIdentifier', patientIdentifier);
+        sessionStorage.setItem('accessToken', userAccessToken);
         resolve('success');
       });
     } catch (error) {
