638c0b9da34b4227398f456b03c29b6951764424
Nitin Gujral <nitin.gujral>
2025-07-07 06:28:54 -0400

    resolved last merge

--- src/app/components/navigation/NavigationSidebar/SubMenuComponents/SettingsSubmenu.jsx
+++ src/app/components/navigation/NavigationSidebar/SubMenuComponents/SettingsSubmenu.jsx
@@ -85,15 +85,12 @@ const SettingsSubmenu = () => {
           <SubMenuLink to={DEVELOPERS_PATH}>Developers</SubMenuLink>
         </AccessRestrictor>
       )}
-      {true && (
-        <AccessRestrictor allowedToRoles={[ADMIN, OWNER]}>
-          <SubMenuLink to="/settings/workspaces">Workspaces</SubMenuLink>
-        </AccessRestrictor>
-      )}
+      <AccessRestrictor allowedToRoles={[ADMIN, OWNER]}>
+        <SubMenuLink to="/settings/workspaces">Workspaces</SubMenuLink>
+      </AccessRestrictor>
       <AccessRestrictor allowedToRoles={[ADMIN, OWNER]}>
         <SubMenuLink to={INTEGRATIONS_PATH}>Integrations</SubMenuLink>
       </AccessRestrictor>
-
     </Box>
   );
 };
