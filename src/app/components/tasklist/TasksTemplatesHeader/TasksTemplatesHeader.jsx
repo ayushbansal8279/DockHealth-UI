d40faf9cb8dd7f7faed452b34f489cbdd79f15b0
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2025-08-11 18:48:06 +0530

    fixed width issue for Workflow library header

--- src/app/components/tasklist/TasksTemplatesHeader/TasksTemplatesHeader.jsx
+++ src/app/components/tasklist/TasksTemplatesHeader/TasksTemplatesHeader.jsx
@@ -1,10 +1,14 @@
 import React, { useMemo } from 'react';
 import ColumnSortHeader from 'components/tasklist/ColumnSortHeader/ColumnSortHeader';
-import { SortHeaderRow } from 'components/tasklist/ColumnSortHeader/styled';
 import {
   TaskTemplateItemColumn,
   TASK_TEMPLATE_ITEM_BASE_COLUMN_CONFIG,
 } from 'helpers/workflow-helpers';
+import {
+  TaskTemplateContainer,
+  TaskTemplateHeader,
+  HeaderChildrenContainer,
+} from './styled';
 
 const TasksTemplatesHeader = ({
   sort,
@@ -28,75 +32,79 @@ const TasksTemplatesHeader = ({
   }
 
   return (
-    <SortHeaderRow>
-      <ColumnSortHeader
-        width={30}
-        tasksHeaderTextTransform={tasksHeaderTextTransform}
-        tasksHeaderTextColor={tasksHeaderTextColor}
-      />
-      {mergedConfig[TaskTemplateItemColumn.NAME] && (
+    <TaskTemplateContainer>
+      <TaskTemplateHeader>
         <ColumnSortHeader
-          flex={1}
-          id={TaskTemplateItemColumn.NAME}
-          label="Name"
-          sort={sort}
-          onSortChange={onSortChange}
+          width={30}
           tasksHeaderTextTransform={tasksHeaderTextTransform}
           tasksHeaderTextColor={tasksHeaderTextColor}
         />
-      )}
-      {mergedConfig[TaskTemplateItemColumn.SHARED_BY] && (
-        <ColumnSortHeader
-          id={TaskTemplateItemColumn.SHARED_BY}
-          label="Shared by"
-          width={150}
-          tasksHeaderTextTransform={tasksHeaderTextTransform}
-          tasksHeaderTextColor={tasksHeaderTextColor}
-        />
-      )}
-      {mergedConfig[TaskTemplateItemColumn.SHARED_WITH] && (
-        <ColumnSortHeader
-          id={TaskTemplateItemColumn.SHARED_WITH}
-          label="Shared with"
-          width={150}
-          tasksHeaderTextTransform={tasksHeaderTextTransform}
-          tasksHeaderTextColor={tasksHeaderTextColor}
-        />
-      )}
-      {mergedConfig[TaskTemplateItemColumn.CREATED_BY] && (
-        <ColumnSortHeader
-          id={TaskTemplateItemColumn.CREATED_BY}
-          label="Created by"
-          width={150}
-          sort={sort}
-          onSortChange={onSortChange}
-          tasksHeaderTextTransform={tasksHeaderTextTransform}
-          tasksHeaderTextColor={tasksHeaderTextColor}
-        />
-      )}
-      {mergedConfig[TaskTemplateItemColumn.CREATED] && (
-        <ColumnSortHeader
-          id={TaskTemplateItemColumn.CREATED}
-          label="Created"
-          width={150}
-          sort={sort}
-          onSortChange={onSortChange}
-          tasksHeaderTextTransform={tasksHeaderTextTransform}
-          tasksHeaderTextColor={tasksHeaderTextColor}
-        />
-      )}
-      {mergedConfig[TaskTemplateItemColumn.PERMISSIONS] && (
-        <ColumnSortHeader
-          id={TaskTemplateItemColumn.PERMISSIONS}
-          label="Permissions"
-          width={200}
-          sort={sort}
-          onSortChange={onSortChange}
-          tasksHeaderTextTransform={tasksHeaderTextTransform}
-          tasksHeaderTextColor={tasksHeaderTextColor}
-        />
-      )}
-    </SortHeaderRow>
+        {mergedConfig[TaskTemplateItemColumn.NAME] && (
+          <ColumnSortHeader
+            flex={1}
+            id={TaskTemplateItemColumn.NAME}
+            label="Name"
+            sort={sort}
+            onSortChange={onSortChange}
+            tasksHeaderTextTransform={tasksHeaderTextTransform}
+            tasksHeaderTextColor={tasksHeaderTextColor}
+          />
+        )}
+        <HeaderChildrenContainer>
+          {mergedConfig[TaskTemplateItemColumn.SHARED_BY] && (
+            <ColumnSortHeader
+              id={TaskTemplateItemColumn.SHARED_BY}
+              label="Shared by"
+              width={150}
+              tasksHeaderTextTransform={tasksHeaderTextTransform}
+              tasksHeaderTextColor={tasksHeaderTextColor}
+            />
+          )}
+          {mergedConfig[TaskTemplateItemColumn.SHARED_WITH] && (
+            <ColumnSortHeader
+              id={TaskTemplateItemColumn.SHARED_WITH}
+              label="Shared with"
+              width={150}
+              tasksHeaderTextTransform={tasksHeaderTextTransform}
+              tasksHeaderTextColor={tasksHeaderTextColor}
+            />
+          )}
+          {mergedConfig[TaskTemplateItemColumn.CREATED_BY] && (
+            <ColumnSortHeader
+              id={TaskTemplateItemColumn.CREATED_BY}
+              label="Created by"
+              width={150}
+              sort={sort}
+              onSortChange={onSortChange}
+              tasksHeaderTextTransform={tasksHeaderTextTransform}
+              tasksHeaderTextColor={tasksHeaderTextColor}
+            />
+          )}
+          {mergedConfig[TaskTemplateItemColumn.CREATED] && (
+            <ColumnSortHeader
+              id={TaskTemplateItemColumn.CREATED}
+              label="Created"
+              width={150}
+              sort={sort}
+              onSortChange={onSortChange}
+              tasksHeaderTextTransform={tasksHeaderTextTransform}
+              tasksHeaderTextColor={tasksHeaderTextColor}
+            />
+          )}
+          {mergedConfig[TaskTemplateItemColumn.PERMISSIONS] && (
+            <ColumnSortHeader
+              id={TaskTemplateItemColumn.PERMISSIONS}
+              label="Permissions"
+              width={200}
+              sort={sort}
+              onSortChange={onSortChange}
+              tasksHeaderTextTransform={tasksHeaderTextTransform}
+              tasksHeaderTextColor={tasksHeaderTextColor}
+            />
+          )}
+        </HeaderChildrenContainer>
+      </TaskTemplateHeader>
+    </TaskTemplateContainer>
   );
 };
 
