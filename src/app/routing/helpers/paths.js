d40f490e840b20c1caec47ef3c7a51c0db271764
Nitin Gujral <nitin.gujral>
2025-08-18 22:14:54 -0400

    configure Workspaces path constant

--- src/app/routing/helpers/paths.js
+++ src/app/routing/helpers/paths.js
@@ -39,7 +39,7 @@ export const PATIENTS_LIST_WITH_TASKS = '/core/patients/list/active';
 export const PATIENTS_LIST_ARCHIVED = '/core/patients/list/archived';
 export const PATIENTS_LIST = '/core/patients/list';
 export const PATIENTS_LIST_CUSTOM = '/core/patients/list/custom';
-export const WORKSPACE_PATH = '/core/workspace';
+export const WORKSPACE_PATH = '/core/workspaces';
 
 export const CHAT_PATH = '/core/chat';
 
