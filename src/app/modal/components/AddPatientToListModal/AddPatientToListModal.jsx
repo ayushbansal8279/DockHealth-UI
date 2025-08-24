ae0e30d8d5402c6d2e986f988148c6981d361447
Nitin Gujral <nitin.gujral>
2025-08-24 18:26:21 -0400

    using pluralize for patient label display

--- src/app/modal/components/AddPatientToListModal/AddPatientToListModal.jsx
+++ src/app/modal/components/AddPatientToListModal/AddPatientToListModal.jsx
@@ -15,6 +15,7 @@ import { getCustomerTypeLabel } from 'helpers/customer-type-helper';
 import { capitalize } from 'helpers/capitalize';
 import { userProfileSelector } from 'selectors/user-selectors';
 import { createPatientListPath } from 'routing/helpers/paths';
+import pluralize from 'pluralize';
 import { CloseIconButton, CloseIcon } from '../styled';
 import {
   AddPatientModalWrapper,
@@ -151,13 +152,13 @@ const AddPatientToListModal = ({
         <Spacing vertical={5} />
         <PatientsSection>
           <Column width={320}>
-            <Header>Add {customerTypeLabel}s</Header>
+            <Header>Add {pluralize(customerTypeLabel)}</Header>
             <Spacing vertical={4} />
             <PatientList onSelect={handleSelectPatient} workspaceIdentifier={workspaceIdentifier} />
           </Column>
           <Spacing horizontal={4} />
           <Column maxHeight={325}>
-            <Header>Included {customerTypeLabel}s in list</Header>
+            <Header>Included {pluralize(customerTypeLabel)} in list</Header>
             <Spacing vertical={4} />
             <SelectedPatientsWrapper>
               {!fetchingPatients ? (
--- src/app/views/patient-details/PatientDetailsView.jsx
+++ src/app/views/patient-details/PatientDetailsView.jsx
@@ -55,6 +55,7 @@ const PatientDetailsView = () => {
   const { userIdentifier: currentUserIdentifier } = currentUser || {};
   const pusher = useRef(initializePusher());
   const customerTypeLabel = getCustomerTypeLabel(currentUser);
+  const customerTypeLabelCapitalized = capitalize(customerTypeLabel);
 
   const currentOrganization = useSelector(selectedUserOrganizationSelector);
   const patientOrgIdentifier = useSelector(patientOrgIdSelector);
@@ -85,7 +86,7 @@ const PatientDetailsView = () => {
     embeddedMode,
     isWorkspaceScoped: isWorkspacePatient,
     workspace,
-    titleText: customerTypeLabel,
+    titleText: customerTypeLabelCapitalized,
   });
 
   useEffect(() => {
