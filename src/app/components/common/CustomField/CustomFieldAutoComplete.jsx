4899ec20404074a44a3ce39abd7c4c8ff92b9de2
Harsh Tomar <harsh.tomar@rearckindia.com>
2025-08-25 18:04:38 +0530

    fix: single select in patient form

--- src/app/components/common/CustomField/CustomFieldAutoComplete.jsx
+++ src/app/components/common/CustomField/CustomFieldAutoComplete.jsx
@@ -103,7 +103,7 @@ const CustomFieldAutoComplete = ({
         );
       } else {
         selectedOptions = event ? [event] : [];
-        selectedValues = event?.profile?.identifier || null;
+        selectedValues = [event?.profile?.identifier] || null;
       }
 
       setValue(name, selectedValues, {
