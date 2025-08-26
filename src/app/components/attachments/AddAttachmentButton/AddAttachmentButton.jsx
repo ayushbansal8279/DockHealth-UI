827d5ff9c947f247c30a081dd0ba06910d2edaae
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2025-08-26 18:53:11 +0530

    rollback the changes for subtask patient file refrence

--- src/app/components/attachments/AddAttachmentButton/AddAttachmentButton.jsx
+++ src/app/components/attachments/AddAttachmentButton/AddAttachmentButton.jsx
@@ -1,99 +1,53 @@
-import React, { useRef, useState } from 'react';
-import {
-  Paper,
-  Popper,
-  MenuItem,
-  ClickAwayListener,
-  ListItemText,
-  Box,
-  MenuList,
-} from '@mui/material';
+import React, { useState } from 'react';
 import { OutfitTypography } from 'styles/theme';
-import Tooltip from 'components/common/Tooltip/Tooltip';
-import zIndex from 'styles/z-index';
-import palette from 'styles/palette';
 import { Container } from './styled';
+import { Menu, MenuItem } from '@mui/material';
 
 const AddAttachmentButton = ({ attachmentOptions = [] }) => {
-  const buttonRef = useRef(null);
-  const inputRef = useRef(null);
-  const [isOpen, setIsOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
 
-  const handleToggle = (e) => {
-    e.stopPropagation();
-    if (attachmentOptions.length === 0) {
-      inputRef.current?.click();
-    } else {
-      setIsOpen((prev) => !prev);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+    if (attachmentOptions.length > 0) {
+      event.stopPropagation();
     }
   };
 
-  const handleClickAway = (event) => {
-    if (buttonRef.current && !buttonRef.current.contains(event.target)) {
-      setIsOpen(false);
-    }
-  };
-
-  const handleOptionClick = (option) => {
-    if (option.label === 'Upload Local File') {
-      inputRef.current?.click();
-    } else {
-      option.onClick?.();
-    }
-  };
-
-  const handleFileChange = (event) => {
-    const file = event.target.files?.[0];
-    if (file) {
-      console.log('Selected file:', file);
-    }
-    event.target.value = '';
+  const handleClose = () => {
+    setAnchorEl(null);
   };
 
   return (
     <div>
-      <Container ref={buttonRef} onClick={handleToggle}>
-        <OutfitTypography condensed variant="h4" color="inherit">
-          +
-        </OutfitTypography>
+      <Container onClick={handleClick}>
+        <div>
+          <OutfitTypography condensed variant="h4" color="inherit">
+            +
+          </OutfitTypography>
+        </div>
+        {attachmentOptions.length > 0 && (
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            {attachmentOptions.map((option, index) => (
+              <MenuItem
+                key={index}
+                onClick={(event) => {
+                  handleClose();
+                  option.onClick?.(event);
+                  event.stopPropagation();
+                }}
+                {...(option.getRootProps ? option.getRootProps() : {})}
+                disabled={option.disabled}
+              >
+                {option.label}
+              </MenuItem>
+            ))}
+          </Menu>
+        )}
       </Container>
-      <Popper
-        anchorEl={buttonRef.current}
-        open={isOpen}
-        placement="bottom-end"
-        disablePortal={false}
-        style={{ zIndex: zIndex.optionsMenu }}
-      >
-        <ClickAwayListener onClickAway={handleClickAway}>
-          <Paper>
-            <MenuList>
-              {attachmentOptions.map((option) => (
-                <Tooltip key={option.name} title={option.tooltipText || ''}>
-                  <div>
-                    <MenuItem
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        setIsOpen(false);
-                        handleOptionClick(option);
-                      }}
-                      disabled={option.disabled}
-                    >
-                      <ListItemText primary={option.label} />
-                    </MenuItem>
-                  </div>
-                </Tooltip>
-              ))}
-              <Box borderBottom={`1px solid ${palette.coolGrey3}`} mb={1} />
-            </MenuList>
-          </Paper>
-        </ClickAwayListener>
-      </Popper>
-      <input
-        ref={inputRef}
-        type="file"
-        style={{ display: 'none' }}
-        onChange={handleFileChange}
-      />
     </div>
   );
 };
--- src/app/components/task-drawer/AttachmentsSection/AttachmentsSection.jsx
+++ src/app/components/task-drawer/AttachmentsSection/AttachmentsSection.jsx
@@ -16,11 +16,7 @@ import {
   Title,
   AttachmentStatusMessage,
 } from './styled';
-import {
-  ScanStatus,
-  ScanStatusText,
-  UNSUPPORTED_WARNING_MESSAGE,
-} from './helpers';
+import { ScanStatus, ScanStatusText, UNSUPPORTED_WARNING_MESSAGE } from './helpers';
 import { DrawerFieldEnum } from '@/app/helpers/task-drawer-helpers';
 import { useDispatch } from 'react-redux';
 import { openModal } from '@/app/modal/actions';
@@ -55,14 +51,11 @@ const AttachmentsSection = ({
   const downloadDisabled = currentTaskAttachments?.some(
     ({ scanStatus }) =>
       scanStatus &&
-      !(
-        scanStatus === ScanStatus.CLEAN || scanStatus === ScanStatus.UNSUPPORTED
-      ),
+      !(scanStatus === ScanStatus.CLEAN ||
+        scanStatus === ScanStatus.UNSUPPORTED),
   );
-  const isSubTask = Boolean(selectedTask?.parentTask);
-  const patientIdentifier = isSubTask
-    ? selectedTask?.parentTask?.patient?.patientIdentifier
-    : selectedTask?.patient?.patientIdentifier;
+  const patientIdentifier = selectedTask?.patient?.patientIdentifier;
+
   useEffect(() => {
     if (
       attachmentRef.current &&
@@ -75,7 +68,7 @@ const AttachmentsSection = ({
   const referenceAttachment = async () => {
     try {
       const patientAttachments = await getPatientAttachments(patientIdentifier);
-
+  
       dispatch(
         openModal('PatientAttachmentReference', {
           attachmentList: patientAttachments,
@@ -97,9 +90,7 @@ const AttachmentsSection = ({
         {
           label: 'Reference File from Patient',
           onClick: referenceAttachment,
-          disabled: isSubTask
-            ? !selectedTask?.parentTask?.patient
-            : !selectedTask?.patient,
+          disabled: !selectedTask?.patient,
         },
       ]
     : [];
@@ -145,9 +136,7 @@ const AttachmentsSection = ({
             xs={12}
             container
             alignContent="center"
-            {...getRootProps({ style: { outline: 'none' },
-              onClick : (e) => e.stopPropagation(),
-            })}
+            {...getRootProps({ style: { outline: 'none' } })}
           >
             {attachmentsLoading ? (
               <>
@@ -209,7 +198,7 @@ const AttachmentsSection = ({
                 onClick={() => {
                   if (
                     attachment?.scanStatus === null ||
-                    attachment?.scanStatus === ScanStatus.CLEAN ||
+                    attachment?.scanStatus === ScanStatus.CLEAN||
                     attachment?.scanStatus === ScanStatus.UNSUPPORTED
                   )
                     openAttachmentPreview(attachment);
