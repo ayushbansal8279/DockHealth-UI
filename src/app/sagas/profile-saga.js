852affd1bf6fbdb8cf04546e108dac251882738b
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2025-10-09 02:38:05 +0530

    integrated user preference updates in profile saga to handle selected filters

--- src/app/sagas/profile-saga.js
+++ src/app/sagas/profile-saga.js
@@ -19,6 +19,9 @@ import { showGlobalAlert, showGlobalErrorAlert } from '../alert/actions';
 import AlertMessages from 'alert/AlertMessages';
 import { closeModal } from 'modal/actions';
 import { ProfileStatus, ProfileQueryType } from 'helpers/profile-helpers';
+import { cleanedSelectedFilters } from '../helpers/mega-filter-helper';
+import { UserPreferenceContextType } from '../helpers/user-prefrence-helper';
+import * as UserPreferenceApi from '../api/user-preference-api';
 
 function* getCurrentProfileFilterOptions() {
   try {
@@ -48,9 +51,27 @@ function* selectedProfileFilter({ payload }) {
     currentProfileTypeIdentifierSelector,
   );
   const status = '';
+  const selectedFilters = cleanedSelectedFilters(filters);
+  const partialDetails = {
+    selectedFilters,
+    selectedQuickFilter,
+  };
+
+  const preferences = yield call(
+    UserPreferenceApi.updateUserPreference,
+    UserPreferenceContextType.CUSTOM_OBJECT_LIST,
+    profileTypeIdentifier,
+    partialDetails,
+  );
+
+  yield put({
+    type: ActionTypes.UPDATE_USER_PREFERENCES_SUCCESS,
+    preferences,
+  });
+
   yield put(
     MegaFilterActions.selectFiltersForMegaFilter(
-      filters,
+      selectedFilters,
       profileTypeIdentifier,
       status,
       selectedQuickFilter,
@@ -519,7 +540,10 @@ export default function* watchProfileDetail() {
   );
   yield takeEvery(ActionTypes.PROFILE_BULK_ARCHIVE, profileBulkArchive);
   yield takeEvery(ActionTypes.PROFILE_BULK_DELETE, profileBulkDelete);
-  yield takeEvery(ActionTypes.PROFILE_BULK_EDIT_CUSTOM_FIELDS, profileBulkEditCustomFields);
+  yield takeEvery(
+    ActionTypes.PROFILE_BULK_EDIT_CUSTOM_FIELDS,
+    profileBulkEditCustomFields,
+  );
   yield takeEvery(ActionTypes.PROFILE_BULK_UNARCHIVE, profileBulkUnarchive);
   yield takeEvery(ActionTypes.PROFILE_BULK_RECOVER, profileBulkRecover);
   yield takeEvery(ActionTypes.GET_PROFILES, getProfiles);
