0e22ee928c2223a36771fac599252bc4dabc1e47
Nitin Gujral <nitin.gujral>
2025-10-29 01:25:34 -0400

    support metering filter by single type

--- src/app/views/self-serve/metered-billing/MeterBillingView.jsx
+++ src/app/views/self-serve/metered-billing/MeterBillingView.jsx
@@ -78,6 +78,7 @@ const MeterBillingView = () => {
           end: endDate.toISOString(),
           eventIdentifier: null,
           duration: days > 1 || months >= 1 ? 'DAILY' : 'HOURLY',
+          type: filterOptions?.eventTypes?.options?.[0] || 'AGGREGATION',
           ts: null
         },
       };
