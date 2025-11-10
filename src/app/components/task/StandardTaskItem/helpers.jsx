23c0aa8909a8b38a8ae3295393c59d0318ec3ff8
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2025-11-10 20:41:02 +0530

    add PatientFieldTooltip component to enhance patient data fields with contextual information

--- src/app/components/task/StandardTaskItem/helpers.jsx
+++ src/app/components/task/StandardTaskItem/helpers.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import { Link, useLocation } from 'react-router-dom';
+import Tooltip from 'components/common/Tooltip/Tooltip';
 import {
   SubtaskStylingLastLink,
   SubtaskStylingLinkContainer,
   SubtaskStylingVerticalPart,
   SubtaskStylingHorizontalPart,
 } from '../styled';
+import palette from '@/app/styles/palette';
 
 export const getMatchedComments = (comments, matchingCommentIdentifiers) =>
   matchingCommentIdentifiers?.length > 0
@@ -50,5 +52,63 @@ export const getSubtaskStylingLink = (isLast, origin) => {
       <SubtaskStylingVerticalPart />
       <SubtaskStylingHorizontalPart />
     </SubtaskStylingLinkContainer>
+  );
+};
+
+const getPatientName = (patient) => {
+  if (!patient) return '';
+  return patient?.middleName
+    ? `${patient?.lastName}, ${patient?.firstName} ${patient?.middleName?.slice(
+        0,
+        1,
+      )}.`
+    : `${patient?.lastName}, ${patient?.firstName}`;
+};
+
+const getPatientFieldTooltipContent = (patient, pathname, search) => {
+  if (!patient?.patientIdentifier) {
+    return 'This is a patient field and can only be edited on the patient page';
+  }
+  const patientName = getPatientName(patient);
+  return (
+    <>
+      This is a patient field and can only be edited on the{' '}
+      <Link
+        to={{
+          pathname: `/core/patient/${patient.patientIdentifier}`,
+          state: {
+            from: search ? pathname + search : pathname,
+          },
+        }}
+        onClick={(e) => {
+          e.stopPropagation();
+          sessionStorage.setItem(
+            'navigation-from',
+            search ? pathname + search : pathname,
+          );
+        }}
+        style={{
+          color: palette.brightBlue,
+          textDecoration: 'underline',
+          fontWeight: 'bold',
+        }}
+      >
+        {patientName}
+      </Link>{' '}
+      patient page
+    </>
+  );
+};
+
+export const PatientFieldTooltip = ({ patient, children }) => {
+  const { pathname, search } = useLocation();
+
+  return (
+    <Tooltip
+      placement="top"
+      title={getPatientFieldTooltipContent(patient, pathname, search)}
+    >
+      {children}
+    </Tooltip>
   );
 };
