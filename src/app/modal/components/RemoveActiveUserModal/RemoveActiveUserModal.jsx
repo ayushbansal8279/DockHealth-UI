df30524fde3732996b08f559710769ea97850348
tejas <tejas.patil@rearckindia.com>
2025-11-11 10:15:09 +0530

    fixed: When off boarding / removing a user - system should support to pick another person from the org that should be assigned the tasks and workflows

--- src/app/modal/components/RemoveActiveUserModal/RemoveActiveUserModal.jsx
+++ src/app/modal/components/RemoveActiveUserModal/RemoveActiveUserModal.jsx
@@ -15,9 +15,12 @@ import {
 } from '../styled';
 import { FirstDescription, SecondDescription } from './styled';
 import { CancelButton, ConfirmButton } from '../ModalButton/ModalButtons';
+import SingleUserPicker from '@/app/components/user/SingleUserPicker/SingleUserPicker';
+import { reassignTask } from '@/app/api/task-api';
 
 const RemoveActiveUserModal = ({ closeModal, confirm, userIdentifier }) => {
   const [taskCount, setTaskCount] = useState(0);
+  const [selectedUserIdentifier, setSelectedUserIdentifier] = useState(null);
 
   useEffect(() => {
     getUserActiveTasksCount(userIdentifier).then(({ data }) =>
@@ -35,28 +38,56 @@ const RemoveActiveUserModal = ({ closeModal, confirm, userIdentifier }) => {
         <ModalDescriptionContainer>
           <FirstDescription>
             {taskCount > 0
-              ? `This user has ${taskCount} tasks assigned to them. These tasks will
-            become unassigned and you can reassign in the list(s).`
+              ? `This user has ${taskCount} tasks assigned to them. 
+              Removing the user without reassigning their tasks will unassign those tasks`
               : `Are you sure you want to remove this user?`}
           </FirstDescription>
           <SecondDescription>
-            If removed, you will not be charged for this user starting in the
-            next billing cycle.
+            {taskCount > 0 && (
+              <>
+                Select an alternative existing user to reassign the current
+                pending tasks and workflows
+              </>
+            )}
           </SecondDescription>
         </ModalDescriptionContainer>
+        {taskCount > 0 && (
+          <div
+            style={{
+              marginTop: '16px',
+              marginBottom: '16px',
+              width: '100%',
+              padding: '0 24px',
+              boxSizing: 'border-box',
+            }}
+          >
+            <SingleUserPicker
+              value={selectedUserIdentifier}
+              onChange={setSelectedUserIdentifier}
+              placeholder="Search user to reassign tasks"
+              removedUserIdentifier={userIdentifier}
+            />
+          </div>
+        )}
         <ButtonsContainer>
           <CancelButton style={{ width: '180px' }} onClick={closeModal}>
             Do Not Remove
           </CancelButton>
           <Spacing horizontal={4} />
           <ConfirmButton
-            style={{ width: '180px' }}
-            onClick={() => {
+            style={{ width: '250px' }}
+            onClick={async () => {
               closeModal();
+              if (selectedUserIdentifier) {
+                await reassignTask({
+                  fromUserIdentifier: userIdentifier,
+                  toUserIdentifier: selectedUserIdentifier,
+                });
+              }
               confirm();
             }}
           >
-            Remove
+            Remove and {selectedUserIdentifier ? 'Assign' : 'Unassign'}
           </ConfirmButton>
         </ButtonsContainer>
       </ModalWrapper>
