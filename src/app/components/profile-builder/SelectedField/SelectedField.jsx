c95428128a57adf3febefd7852fc94874a21d6cb
Harsh Tomar <harsh.tomar@rearckindia.com>
2025-11-26 00:24:44 +0530

    feat(object-builder): delete field from object when removing it from group

--- src/app/components/profile-builder/SelectedField/SelectedField.jsx
+++ src/app/components/profile-builder/SelectedField/SelectedField.jsx
@@ -18,6 +18,7 @@ import { ProfileBuilderContext } from '../ProfileBuilder';
 import {
   createProfileFieldType,
   editProfileFieldType,
+  deleteProfileFieldType,
 } from '@/app/api/profile-type-field-api';
 import { showAlert } from 'helpers/utility-functions';
 import { showGlobalAlert } from 'alert/actions';
@@ -247,13 +248,20 @@ const SelectedField = ({ field, category, id }) => {
 
       CustomFieldApi.updateCustomFiledGroup(category.identifier, {
         fields: updatedSavedRefrenceFields,
-      }).then((updatedGroup) => {
-        setCustomGroups((prev) =>
-          prev.map((cg) =>
-            cg.identifier === category.identifier ? updatedGroup : cg,
-          ),
-        );
-      });
+      })
+        .then((updatedGroup) => {
+          setCustomGroups((prev) =>
+            prev.map((cg) =>
+              cg.identifier === category.identifier ? updatedGroup : cg,
+            ),
+          );
+        })
+        .then(() => {
+          return deleteProfileFieldType(field.identifier);
+        })
+        .catch((error) => {
+          console.error('Error removing field from group:', error);
+        });
 
       const allRemainingFields = [
         ...updatedSavedActualFields,
