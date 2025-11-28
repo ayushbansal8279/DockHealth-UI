b277463014526aca1195ed4871e0509ded01cecd
tejas <tejas.patil@rearckindia.com>
2025-11-28 23:44:39 +0530

    fixed: Tasks are not showing on the List page issue

--- src/app/sagas/list-details-saga.js
+++ src/app/sagas/list-details-saga.js
@@ -48,9 +48,7 @@ import {
   currentTaskListSelector,
   currentTaskListIdentifierSelector,
 } from 'selectors/task-list-selectors';
-import {
-  cleanedSelectedFilters,
-} from 'helpers/mega-filter-helper';
+import { cleanedSelectedFilters } from 'helpers/mega-filter-helper';
 import { calendarDateRangeSelector } from 'selectors/calendar-tasks-selectors';
 import { showGlobalAlert, showGlobalErrorAlert } from 'alert/actions';
 import AlertMessages from 'alert/AlertMessages';
@@ -165,7 +163,12 @@ function* getCurrentListTasks() {
       );
       groups = action.groups;
     }
-    const groupsWithTasks = compose(filter((g) => g.metricValue >= 0))(groups);
+
+    const groupsWithTasks = compose(
+      filter(
+        (g) => g.metrics?.some((metric) => metric.metricValue >= 0) ?? false,
+      ),
+    )(groups);
     const groupsToGet = groupsWithTasks;
 
     yield all(
@@ -485,7 +488,6 @@ function* initializeListDetailsTableState() {
     const sort = yield select(userPreferenceSortSelector);
 
     if (taskListIdentifier) {
-
       if (sort) {
         yield put(
           ListDetailsActions.setListDetailsTasksSort(sort.key, sort.order),
